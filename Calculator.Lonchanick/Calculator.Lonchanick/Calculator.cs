5e5c3d7e6388f3e8b4e42c14220fc22330227f83
Diego Arroyo <diego.arroyo2462050@gmail.com>
2023-01-01 20:10:33 -0300

    v2.0 organizando en clases separadas

---
+++ Calculator.Lonchanick/Calculator.Lonchanick/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Calculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+        // Use a switch statement to do the math.
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                while (num2 == 0)
+                {
+                    Console.WriteLine("Enter a non-zero divisor: ");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                break;
+        }
+        return result;
+    }
+}
--- Calculator.Lonchanick/Calculator.Lonchanick/Program.cs
+++ Calculator.Lonchanick/Calculator.Lonchanick/Program.cs
@@ -1,80 +1,64 @@
-﻿static double GetInputDouble()
+﻿using System;
+
+public class Program
 {
-    double aux;
-    string answer = Console.ReadLine();
-    while (!double.TryParse(answer, out aux))
+
+    static double GetInputDouble()
     {
-        Console.WriteLine("This is not an integer");
-        answer = Console.ReadLine();
-    }
-    return aux;
-}
-
-static string GetValidOption()
-{
-    //this mean option can't be more than one char and an option that is not allowed
-    string ops = "asmd";
-    string op = "";
-    bool wh = true;
-    while (wh)
-    {
-        op = Console.ReadLine();
-        if (ops.IndexOf(op[0]) == -1 | op.Length > 1)
-            Console.WriteLine("No es una opcion valida try again: ");
-        else
-            wh = false;
-    }
-    return op;
-}
-
-// Declare variables and then initialize to zero.
-double num1 = 0; double num2 = 0;
-
-// Display title as the C# console calculator app.
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-// Ask the user to type the first number.
-Console.WriteLine("Type a number, and then press Enter");
-num1 = GetInputDouble();
-
-// Ask the user to type the second number.
-Console.WriteLine("Type another number, and then press Enter");
-num2 = GetInputDouble();
-
-// Ask the user to choose an option.
-Console.WriteLine("Choose an option from the following list:");
-Console.WriteLine("\ta - Add");
-Console.WriteLine("\ts - Subtract");
-Console.WriteLine("\tm - Multiply");
-Console.WriteLine("\td - Divide");
-Console.Write("Your option? ");
-
-//pedimos los valores por teclado
-string op = GetValidOption();
-
-// Use a switch statement to do the math.
-switch (op)
-{
-    case "a":
-        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-        break;
-    case "s":
-        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-        break;
-    case "m":
-        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-        break;
-    case "d":
-        while (num2 == 0)
+        double aux;
+        string answer = Console.ReadLine();
+        while (!double.TryParse(answer, out aux))
         {
-            Console.WriteLine("Enter a non-zero divisor: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Error, Type again:");
+            answer = Console.ReadLine();
         }
-        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-        break;
-}
-// Wait for the user to respond before closing.
-Console.Write("Press any key to close the Calculator console app...");
-Console.ReadKey();
+        return aux;
+    }
 
+    static string GetValidOption()
+    {
+        //this mean option can't be more than one char and an option that is not allowed
+        string ops = "asmd";
+        string op = "";
+        bool wh = true;
+        while (wh)
+        {
+            op = Console.ReadLine();
+            if (ops.IndexOf(op[0]) == -1 | op.Length > 1)
+                Console.WriteLine("No es una opcion valida try again: ");
+            else
+                wh = false;
+        }
+        return op;
+    }
+    static void Main(string[] args)
+    {
+
+        // Declare variables and then initialize to zero.
+        double num1 = 0; double num2 = 0;
+
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        // Ask the user to type the first number.
+        Console.WriteLine("Type a number, and then press Enter");
+        num1 = GetInputDouble();
+
+        // Ask the user to type the second number.
+        Console.WriteLine("Type another number, and then press Enter");
+        num2 = GetInputDouble();
+
+        // Ask the user to choose an option.
+        Console.WriteLine("Choose an option from the following list:");
+        Console.WriteLine("\ta - Add");
+        Console.WriteLine("\ts - Subtract");
+        Console.WriteLine("\tm - Multiply");
+        Console.WriteLine("\td - Divide");
+        Console.Write("Your option? ");
+
+        //pedimos los valores por teclado
+        string op = GetValidOption();
+        Calculator.DoOperation(num1,num2,op);
+    }
+}
