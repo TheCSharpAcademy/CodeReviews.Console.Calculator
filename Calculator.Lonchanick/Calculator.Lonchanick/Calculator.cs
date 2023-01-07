48e397b5849396f1ebcea3037d6aa7503e8d1945
Diego Arroyo <diego.arroyo2462050@gmail.com>
2023-01-07 14:56:11 -0300

    removing unusing using

--- Calculator.Lonchanick/Calculator.Lonchanick/Calculator.cs
+++ Calculator.Lonchanick/Calculator.Lonchanick/Calculator.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using Newtonsoft.Json;
-
+﻿
 namespace Calculator.Lonchanick
 {
     public class Calculator
