44992a0bbd05a397bcf2c6fb1df9b66cc695f82e
kraven88 <k.rowecki@gmail.com>
2023-01-19 13:03:35 +0100

    Finished part 2 of the MS tutorial

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace CalculatorLibrary;
+
+public class Calculator
+{
+    JsonWriter writer;
+
+    public Calculator()
+    {
+        var logFile = File.CreateText("calculatorLog.json");
+        logFile.AutoFlush= true;
+        writer = new JsonTextWriter(logFile);
+        writer.Formatting = Formatting.Indented;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operations");
+        writer.WriteStartArray();
+    }
+    public double DoOperation(double num1, double num2, string operation)
+    {
+        var result = double.NaN;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operand1");
+        writer.WriteValue(num1);
+        writer.WritePropertyName("Operand2");
+        writer.WriteValue(num2);
+        writer.WritePropertyName("Operation");
+
+        // Use switch statement to do the math;
+        switch (operation)
+        {
+            case "a":
+                result = num1 + num2;
+                writer.WriteValue("Add");
+                break;
+            case "s":
+                result = num1 - num2;
+                writer.WriteValue("Subtract");
+                break;
+            case "m":
+                result = num1 * num2;
+                writer.WriteValue("Multiply");
+                break;
+            case "d":
+                if (num2 != 0)
+                    result = num1 / num2;
+                    writer.WriteValue("Divide");
+                break;
+            default:
+                break;
+        }
+        writer.WritePropertyName("Result");
+        writer.WriteValue(result);
+        writer.WriteEndObject();
+
+        return result;
+    }
+
+    public void Finish()
+    {
+        writer.WriteEndArray();
+        writer.WriteEndObject();
+        writer.Close();
+    }
+}
--- ConsoleCalculator.kraven88/Program.cs
+++ ConsoleCalculator.kraven88/Program.cs
@@ -1,70 +1,84 @@
-﻿using ConsoleCalculator.kraven88;
+﻿using CalculatorLibrary;
 
-var endApp = false;
+namespace CalculatorProgram;
 
-// Display title as the C# console calculator app;
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-while (true)
+internal class Program
 {
-
-    // Declare variables and initialize with zero;
-    var numText1 = "";
-    var numText2 = "";
-    var result = 0.0;
-
-    // Ask the user to type the first number;
-    Console.Write("Type a number, then press Enter: ");
-    numText1 = Console.ReadLine().Trim();
-
-    var num1 = 0.0;
-    while (double.TryParse(numText1, out num1) == false)
+    private static void Main(string[] args)
     {
-        Console.Write("Invalid number. Please enter a numeric value: ");
-        numText1 = Console.ReadLine().Trim();
+        var endApp = false;
+        var calculator = new Calculator();
+
+        // Display title as the C# console calculator app;
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        while (endApp == false)
+        {
+
+            // Declare variables and initialize with zero;
+            var numText1 = "";
+            var numText2 = "";
+            var result = 0.0;
+
+            // Ask the user to type the first number;
+            Console.Write("Type a number, then press Enter: ");
+            numText1 = Console.ReadLine()!.Trim();
+
+            var num1 = 0.0;
+            while (double.TryParse(numText1, out num1) == false)
+            {
+                Console.Write("Invalid number. Please enter a numeric value: ");
+                numText1 = Console.ReadLine()!.Trim();
+            }
+
+            // Ask the user to type the second number;
+            Console.Write("Type another number, then press Enter: ");
+            numText2 = Console.ReadLine()!.Trim();
+
+            var num2 = 0.0;
+            while (double.TryParse(numText2, out num2) == false)
+            {
+                Console.Write("Invalid number. Please enter a numeric value: ");
+                numText2 = Console.ReadLine()!.Trim();
+            }
+
+            // Ask the user to choose an option;
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your choice: ");
+
+            var operation = Console.ReadLine()!.Trim();
+
+            try
+            {
+                result = calculator.DoOperation(num1, num2, operation);
+
+                if (double.IsNaN(result))
+                    Console.WriteLine("This operation will result in mathematical error.\n");
+                else Console.WriteLine($"Your result: {result:0.##}\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Oh no! An exception occured while doing the math. \nDetails: {ex.Message}");
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing the app;
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n")
+                endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+
+        // Add call to close the JSON writer before return
+        calculator.Finish();
+
+        return;
     }
-
-    // Ask the user to type the second number;
-    Console.Write("Type another number, then press Enter: ");
-    numText2 = Console.ReadLine().Trim();
-
-    var num2 = 0.0;
-    while (double.TryParse(numText2, out num2) == false)
-    {
-        Console.Write("Invalid number. Please enter a numeric value: ");
-        numText2 = Console.ReadLine().Trim();
-    }
-
-    // Ask the user to choose an option;
-    Console.WriteLine("Choose an option from the following list:");
-    Console.WriteLine("\ta - Add");
-    Console.WriteLine("\ts - Subtract");
-    Console.WriteLine("\tm - Multiply");
-    Console.WriteLine("\td - Divide");
-    Console.Write("Your choice: ");
-
-    var operation = Console.ReadLine().Trim();
-
-    try
-    {
-        result = Calculator.DoOperation(num1, num2, operation);
-
-        if (double.IsNaN(result))
-            Console.WriteLine("This operation will result in mathematical error.\n");
-        else Console.WriteLine($"Your result: {result:0.##}\n");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Oh no! An exception occured while doing the math. \nDetails: {ex.Message}");
-    }
-
-    Console.WriteLine("------------------------\n");
-
-    // Wait for the user to respond before closing the app;
-    Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-    if (Console.ReadLine() == "n") 
-        endApp = true;
-
-    Console.WriteLine("\n"); // Friendly linespacing.
 }
