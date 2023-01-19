fd92dd82c4b5f9794b53a54b9aaea7c3180bc584
yashsachdev <yashsachdev5677@gmail.com>
2023-01-20 01:41:18 +0530

    yashsachdev

--- Calculator.yashsachdev/Calculator.cs
+++ Calculator.yashsachdev/Calculator.cs
@@ -1,24 +1,21 @@
 ﻿namespace Calculator.yashsachdev;
+
 class Calculator
 {
-
     private List<double> _storeValue = new List<double>();
-    public List<double> store // allowing access to the field to read-only. 
+    public List<double> store
     {
         get { return _storeValue; }
     }
-
     private int _count = 0;
-
     public int cnt
     {
         get { return _count; }
     }
+    double result = double.NaN;
 
     public double DoOperation(double num1, double num2, string op)
     {
-        double result = double.NaN;
-
         switch (op)
         {
             case "a":
@@ -34,62 +31,61 @@ class Calculator
                 _storeValue.Add(result);
                 break;
             case "d":
-
                 if (num2 != 0)
                 {
                     result = num1 / num2;
                     _storeValue.Add(result);
                 }
                 break;
-            //challenge 4:Add extra calculations: Square Root, Taking the Power, 10x, Trigonometry functions.
-            case "^":
-                Console.WriteLine("Enter number you want to use for power:");
-                double Num = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine($"enter the number you wnat {Num} raised to ? ");
-                double powerNum = Convert.ToDouble(Console.ReadLine());
-                result = Math.Pow(Num, powerNum);
-                _storeValue.Add(result);
-                break;
-            case "sqrt":
-                Console.WriteLine("Enter number you want to use for square root:");
-                double sqrtNum = Convert.ToDouble(Console.ReadLine());
-                result = Math.Sqrt(sqrtNum);
-                _storeValue.Add(result);
-                break;
-            case "10x":
-                Console.WriteLine("Enter number you want to use for 10^x:");
-                double tenXNum = Convert.ToDouble(Console.ReadLine());
-                result = Math.Pow(10, tenXNum);
-                _storeValue.Add(result);
-                break;
-            case "sin":
-                Console.WriteLine("Enter number you want to use for sin:");
-                double sinNum = Convert.ToDouble(Console.ReadLine());
-                result = Math.Sin(sinNum);
-                _storeValue.Add(result);
-                break;
-            case "1":
-
-                DisplayResult();
-                result = 0;
-                break;
-            case "2":
-
-                RemoveList();
-                result = 0;
-                break;
-
+            case "more":
+                Moreoperations();
+                    break;
             default:
                 break;
         }
         _count++;
-        Console.WriteLine($"The number of times Calculator was used {_count}"); //Challenge 1: Create a functionality that will count the amount of times the calculator was used.
-
+        Console.WriteLine($"The number of times Calculator was used {_count}");
         return result;
-
-
     }
-    //challenge 2:Store a list with the latest calculations. And give the users the ability to delete that list.
+    public void Moreoperations()
+    {
+        Console.WriteLine("Enter operation (pow,sqrt,10x,sin):");
+        string opera = Console.ReadLine();
+        if (opera == "pow")
+        {
+            Console.WriteLine("Enter number you want to use for power:");
+            double Num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"enter the number you wnat {Num} raised to ? ");
+            double powerNum = Convert.ToDouble(Console.ReadLine());
+            result = Math.Pow(Num, powerNum);
+            _storeValue.Add(result);
+        }
+        else if (opera == "sqrt")
+        {
+            Console.WriteLine("Enter number you want to use for square root:");
+            double sqrtNum = Convert.ToDouble(Console.ReadLine());
+            result = Math.Sqrt(sqrtNum);
+            _storeValue.Add(result);
+        }
+        else if (opera == "10x")
+        {
+            Console.WriteLine("Enter number you want to use for 10^x:");
+            double tenXNum = Convert.ToDouble(Console.ReadLine());
+            result = Math.Pow(10, tenXNum);
+            _storeValue.Add(result);
+        }
+        else if (opera == "sin")
+        {
+            Console.WriteLine("Enter number you want to use for sin:");
+            double sinNum = Convert.ToDouble(Console.ReadLine());
+            result = Math.Sin(sinNum);
+            _storeValue.Add(result);
+        }
+        else
+        {
+            Console.WriteLine("Invalid operation");
+        }
+    }
     public void RemoveList()
     {
         if (_storeValue.Count != 0)
@@ -102,20 +98,14 @@ class Calculator
             Console.WriteLine("Result is empty");
         }
     }
-
     public void DisplayResult()
     {
         if (!_storeValue.Any())
         {
             Console.WriteLine("List is empty");
         }
-
         for (int i = 0; i < _storeValue.Count; i++)
             Console.WriteLine("Result index=" + (i) + " :" + _storeValue[i]);
-
     }
-
-
-
 }
 
--- Calculator.yashsachdev/Program.cs
+++ Calculator.yashsachdev/Program.cs
@@ -1,44 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 global using System;
-using Calculator;
 
 bool endApp = false;
-
 Console.WriteLine("Console Calculator in c#\r");
 Console.WriteLine("------------------------\n");
-
-var calculator = new Calculator.yashsachdev.Calculator(); // creating a instance of class
-
+var calculator = new Calculator.yashsachdev.Calculator(); 
 while (!endApp)
 {
     string numInput1 = "";
     string numInput2 = "";
     double result = 0;
-
     Console.Write("Type a number, and then press Enter: ");
     numInput1 = Console.ReadLine();
-
     double cleanNum1 = 0;
     while (!double.TryParse(numInput1, out cleanNum1))
     {
         Console.Write("This is not valid input. Please enter an integer value: ");
         numInput1 = Console.ReadLine();
     }
-
-    if (calculator.cnt == 0) // Condition so user enter 2nd number and not list.
+    if (calculator.cnt == 0) 
     {
         Console.WriteLine("Type Another Number");
     }
-
     else
     {
         Console.Write("Type another number OR type 'list' to use results from previous calculations , and then press Enter: ");
     }
-
     numInput2 = Console.ReadLine();
-    if (numInput2 == "list" && calculator.store.Count != 0) // Challenge 3: Allow the users to use the results in the list above to perform new calculations. 
+    if (numInput2 == "list" && calculator.store.Count != 0)
     {
-        calculator.DisplayResult(); //function to display list and its indexes.
+        calculator.DisplayResult();
         Console.Write("Enter result index: ");
         int index = int.Parse(Console.ReadLine());
         double Input2 = 0;
@@ -51,96 +42,104 @@ while (!endApp)
             Console.WriteLine("oops the index was out of bound lets try again.");
             continue;
         }
-        Console.WriteLine("Choose an operator from the following list:");
-        Console.WriteLine("\ta - Add");
-        Console.WriteLine("\ts - Subtract");
-        Console.WriteLine("\tm - Multiply");
-        Console.WriteLine("\td - Divide");
-        Console.WriteLine("\t^ - power");
-        Console.WriteLine("\tsqrt - square root");
-        Console.WriteLine("\t10x - 10 times n");
-        Console.WriteLine("\tsin - sin function");
-
         Console.WriteLine("\t1 - Display Result");
         Console.WriteLine("\t2 - Clear Result");
-        Console.Write("Your option? ");
-        string oper = Console.ReadLine();
-        try
+        Console.WriteLine("\t3 - Perform operations");
+        string str = Console.ReadLine();
+        switch(str)
         {
-            result = calculator.DoOperation(cleanNum1, Input2, oper);
-            if (double.IsNaN(result))
-            {
-                Console.WriteLine("This operation will result in a mathematical error.\n");
-            }
-            else if (result == 0)
-            {
-                Console.WriteLine("\t");
-            }
-            else Console.WriteLine("Your result: {0:0.##}\n", result);
-
+            case "1": calculator.DisplayResult();
+                break;
+            case "2":calculator.RemoveList();
+                break;
+            case "3":
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("type 'more' to perform 10x, ^, sin, cos, tan  operations ");
+                Console.Write("Your option? ");
+                string oper = Console.ReadLine(); 
+                try
+                {
+                    result = calculator.DoOperation(cleanNum1, Input2, oper);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else if (result == 0)
+                    {
+                        Console.WriteLine("\t");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to the list .\n - Details: " + ex.Message);
+                }
+                Console.WriteLine("------------------------\n");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                 if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n");
+                break;
+            default: 
+                break;
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Oh no! An exception occurred trying to the list .\n - Details: " + ex.Message);
-        }
-        Console.WriteLine("------------------------\n");
-        Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-        if (Console.ReadLine() == "n") endApp = true;
-
-        Console.WriteLine("\n");
     }
-
     else
     {
-
         double cleanNum2 = 0;
         while (!double.TryParse(numInput2, out cleanNum2))
         {
             Console.Write("This is not valid input. Please enter an integer value: ");
             numInput2 = Console.ReadLine();
         }
-
-        Console.WriteLine("Choose an operator from the following list:");
-        Console.WriteLine("\ta - Add");
-        Console.WriteLine("\ts - Subtract");
-        Console.WriteLine("\tm - Multiply");
-        Console.WriteLine("\td - Divide");
-        Console.WriteLine("\t^ - power");
-        Console.WriteLine("\tsqrt - square root");
-        Console.WriteLine("\td10x - 10 times n");
-        Console.WriteLine("\tsin - sin function");
         Console.WriteLine("\t1 - Display Result");
         Console.WriteLine("\t2 - Clear Result");
-        Console.Write("Your option? ");
-
-        string op = Console.ReadLine();
-
-        try
+        Console.WriteLine("\t3 - Perform operations");
+        string str = Console.ReadLine();
+        switch (str)
         {
-            result = calculator.DoOperation(cleanNum1, cleanNum2, op);
-
-
-            if (double.IsNaN(result))
-            {
-                Console.WriteLine("This operation will result in a mathematical error.\n");
-            }
-            else if (result == 0)
-            {
-                Console.WriteLine(" \t");
-
-            }
-            else Console.WriteLine("Your result: {0:0.##}\n", result);
-
+            case "1":
+                calculator.DisplayResult();
+                break;
+            case "2":
+                calculator.RemoveList();
+                break;
+            case "3":
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("type 'more' to perform 10x, ^, sin, cos, tan  operations ");
+                Console.Write("Your option? ");
+                string op = Console.ReadLine();
+                try
+                {
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n"); 
+                    }
+                    else if (result == 0)
+                    {
+                        Console.WriteLine(" \t");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+                Console.WriteLine("------------------------\n");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+                Console.WriteLine("\n");
+                break;
+            default:
+                break;
         }
-        catch (Exception e)
-        {
-            Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
-        }
-
-        Console.WriteLine("------------------------\n");
-
-        Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-        if (Console.ReadLine() == "n") endApp = true;
-        Console.WriteLine("\n");
     }
 }
