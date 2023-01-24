4dc6e2be701d296aba4b41720765f797fee1ad1e
Sandis Smans <smanssandis@gmail.com>
2023-01-24 17:47:15 +0200

    Completed part 1 console app -  Calculator App (Microsoft Docs)

---
+++ S-Smans.Calculator/S-Smans.Calculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace S_Smans.Calculator
+{
+    internal class Calculator
+    {
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
