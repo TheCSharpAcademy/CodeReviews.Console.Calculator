ef850bcee2923626d4120e96c0cc9628db8003f3
danielforkner <daniel.forkner@gmail.com>
2023-03-02 08:15:05 -0500

    cleaning up unecessary code and bad syntax

--- danielforkner.Calculator/CalculatorLibrary/CalculatorLibrary.cs
+++ danielforkner.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -1,95 +1,102 @@
 ﻿using Newtonsoft.Json;
-using System.Linq;
 
-namespace CalculatorLibrary
+namespace CalculatorLibrary;
+
+public class Calculation
 {
-    public class Calculation
-    {
-        public int Id { get; set; }
-        public double Operand1 { get; set; }
-        public double Operand2 { get; set; }
-        public string Operation { get; set; }
-        public double Result { get; set; }
-        public bool Success { get; set; }
+    public int Id { get; set; }
+
+    public double Operand1 { get; set; }
+
+    public double Operand2 { get; set; }
+
+    public string Operation { get; set; }
+
+    public double Result { get; set; }
+
+    public bool Success { get; set; }
+
+}
+
+public class Calculator
+{
+    JsonWriter writer;
+    private int _operationsCount;
+    private List<Calculation> _operations;
+
+    public List<Calculation> Operations { 
+        get { return _operations; }
     }
-    public class Calculator
+    public int OperationsCount
     {
-        JsonWriter writer;
-        private int _operationsCount;
-        private List<Calculation> _operations;
-
-        public List<Calculation> Operations { 
-            get { return _operations; }
-        }
-        public int OperationsCount
-        {
-            get { return _operationsCount; }
-            set { _operationsCount = value; }
-        }
-        public Calculator()
-        {
-            _operationsCount = 0;
-            _operations = new List<Calculation>();
-            StreamWriter logFile = File.CreateText("calculatorlog.json");
-            logFile.AutoFlush = true;
-            writer = new JsonTextWriter(logFile);
-            writer.Formatting = Formatting.Indented;
-            writer.WriteStartObject();
-            writer.WritePropertyName("Operations");
-            writer.WriteStartArray();
+        get { return _operationsCount; }
+        set { _operationsCount = value; }
     }
-        public double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-            writer.WriteStartObject();
-            writer.WritePropertyName("Operand1");
-            writer.WriteValue(num1);
-            writer.WritePropertyName("Operand2");
-            writer.WriteValue(num2);
-            writer.WritePropertyName("Operation");
-            // Use a switch statement to do the math.
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    writer.WriteValue("Add");
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    writer.WriteValue("Subtract");
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    writer.WriteValue("Multiply");
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor.
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    writer.WriteValue("Divide");
-                    break;
-                // Return text for an incorrect option entry.
-                case "e":
-                    result = Math.Pow(num1, num2);
-                    writer.WriteValue("Power");
-                    break;
-                default:
-                    break;
-            }
-            writer.WritePropertyName("Result");
-            writer.WriteValue(result);
-            writer.WriteEndObject();
 
-            return result;
-        }
+    public Calculator()
+    {
+        _operationsCount = 0;
+        _operations = new List<Calculation>();
+        StreamWriter logFile = File.CreateText("calculatorlog.json");
+        logFile.AutoFlush = true;
+        writer = new JsonTextWriter(logFile);
+        writer.Formatting = Formatting.Indented;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operations");
+        writer.WriteStartArray();
+}
 
-        public void Finish()
+    public double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operand1");
+        writer.WriteValue(num1);
+        writer.WritePropertyName("Operand2");
+        writer.WriteValue(num2);
+        writer.WritePropertyName("Operation");
+        // Use a switch statement to do the math.
+        switch (op)
         {
-            writer.WriteEndArray();
-            writer.WriteEndObject();
-            writer.Close();
+            case "a":
+                result = num1 + num2;
+                writer.WriteValue("Add");
+                break;
+            case "s":
+                result = num1 - num2;
+                writer.WriteValue("Subtract");
+                break;
+            case "m":
+                result = num1 * num2;
+                writer.WriteValue("Multiply");
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                writer.WriteValue("Divide");
+                break;
+            // Return text for an incorrect option entry.
+            case "e":
+                result = Math.Pow(num1, num2);
+                writer.WriteValue("Power");
+                break;
+            default:
+                break;
         }
+        writer.WritePropertyName("Result");
+        writer.WriteValue(result);
+        writer.WriteEndObject();
+
+        return result;
+    }
+
+    public void Finish()
+    {
+        writer.WriteEndArray();
+        writer.WriteEndObject();
+        writer.Close();
     }
 }
--- danielforkner.Calculator/danielforkner.Calculator/Program.cs
+++ danielforkner.Calculator/danielforkner.Calculator/Program.cs
@@ -1,191 +1,158 @@
 ﻿using CalculatorLibrary;
 
-namespace CalculatorProgram
+namespace CalculatorProgram;
+
+class Program
 {
 
-    class Program
+    public static void ViewOperations(Calculator calculator)
     {
-
-        public static void ViewOperations(Calculator calculator)
-        {
-            Console.WriteLine("\n----- Viewing Past Operations -----\n");
-            bool viewingOperations = true;
-            int idx = 0;
-            while (viewingOperations)
+        Console.WriteLine("\n----- Viewing Past Operations -----\n");
+        bool viewingOperations = true;
+        int idx = 0;
+        while (viewingOperations)
+            {
+                if (calculator.Operations.Count == 0)
                 {
-                    if (calculator.Operations.Count == 0)
-                    {
-                        Console.WriteLine("You don't have any past operations to view!");
-                        viewingOperations = false;
-                        continue;
-                    }
-                    // operation
-                    Console.WriteLine($"Operation: {calculator.Operations[idx].Id}");
-                    Console.WriteLine($"Operand1: {calculator.Operations[idx].Operand1}");
-                    Console.WriteLine($"Operand2: {calculator.Operations[idx].Operand2}");
-                    Console.WriteLine($"Result: {calculator.Operations[idx].Result}");
-                    if (!calculator.Operations[idx].Success)
-                    {
-                        Console.WriteLine($"Success: {calculator.Operations[idx].Success}");
-                    }
-                    Console.Write("\n");
-                    Console.WriteLine($"{(calculator.Operations.Count > idx + 1 ? "Type next to move forward\n" : "")}{(idx != 0 ? "Type back for previous operation\n" : "")}Type delete to remove history\nType q to stop viewing operations");
-                    Console.Write("Selection: ");
-                    string selection = Console.ReadLine();
-                    Console.Write("\n");
-                    // convert to switch
-                    switch (selection.ToLower())
-                    {
-                        case "next":
-                            if (calculator.Operations.Count <= idx + 1)
-                            {
-                                break;
-                            } else
-                            {
-                                idx++;
-                                break;
-                            }
-                        case "back":
-                            if (idx <= 0)
-                            {
-                                break;
-                            } else
-                            {
-                                idx--;
-                                break;
-                            }
-                        case "delete":
-                            calculator.Operations.RemoveAt(idx);
-                            if (calculator.Operations.Count <= idx)
+                    Console.WriteLine("You don't have any past operations to view!");
+                    viewingOperations = false;
+                    continue;
+                }
+                Console.WriteLine($"Operation: {calculator.Operations[idx].Id}");
+                Console.WriteLine($"Operand1: {calculator.Operations[idx].Operand1}");
+                Console.WriteLine($"Operand2: {calculator.Operations[idx].Operand2}");
+                Console.WriteLine($"Result: {calculator.Operations[idx].Result}");
+                if (!calculator.Operations[idx].Success)
+                {
+                    Console.WriteLine($"Success: {calculator.Operations[idx].Success}");
+                }
+                Console.Write("\n");
+                Console.WriteLine($"{(calculator.Operations.Count > idx + 1 ? "Type next to move forward\n" : "")}{(idx != 0 ? "Type back for previous operation\n" : "")}Type delete to remove history\nType q to stop viewing operations");
+                Console.Write("Selection: ");
+                string selection = Console.ReadLine();
+                Console.Write("\n");
+                switch (selection.ToLower())
+                {
+                    case "next":
+                        if (calculator.Operations.Count <= idx + 1)
+                        {
+                            break;
+                        } else
+                        {
+                            idx++;
+                            break;
+                        }
+                    case "back":
+                        if (idx <= 0)
+                        {
+                            break;
+                        } else
                         {
                             idx--;
                             break;
-                        } 
+                        }
+                    case "delete":
+                        calculator.Operations.RemoveAt(idx);
+                        if (calculator.Operations.Count <= idx)
+                    {
+                        idx--;
                         break;
-                        case "q":
-                            viewingOperations = false;
-                            break;
-                    }
+                    } 
+                    break;
+                    case "q":
+                        viewingOperations = false;
+                        break;
                 }
-            Console.WriteLine("----- End View -----\n");
-        }
-        static void Main(string[] args)
+            }
+        Console.WriteLine("----- End View -----\n");
+    }
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        string[] operationOptions = { "a", "s", "d", "m", "e" };
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        Calculator calculator = new();
+        while (!endApp)
         {
-            bool endApp = false;
-            string[] operationOptions = { "a", "s", "d", "m", "e" };
-            // Display title as the C# console calculator app.
-            Console.WriteLine("Console Calculator in C#\r");
-            Console.WriteLine("------------------------\n");
+            string numInput1;
+            string numInput2;
 
-            Calculator calculator = new();
-            while (!endApp)
+            Console.WriteLine("Information on this Calculator:");
+            Console.WriteLine($"Operations Performed: {calculator.OperationsCount}");
+            Console.WriteLine($"Highest Result: {(calculator.OperationsCount == 0 ? "N/A" : calculator.Operations.Max(op => op?.Result))}");
+            Console.Write("\n");
+            Console.WriteLine("---\n");
+
+            Console.WriteLine("Calculator is open for business, what would you like to do?");
+            Console.WriteLine("\t1. Perform an operation");
+            Console.WriteLine("\t2. View past operations");
+            Console.Write("Your selection: ");
+            string selection = Console.ReadLine();
+            Console.Write("\n");
+
+            while (selection != "1" && selection != "2")
             {
-                // Declare variables and set to empty.
-                string numInput1;
-                string numInput2;
+                Console.Write("Choose 1 or 2: ");
+                selection = Console.ReadLine();
+            }
 
-                Console.WriteLine("Information on this Calculator:");
-                Console.WriteLine($"Operations Performed: {calculator.OperationsCount}");
-                Console.WriteLine($"Highest Result: {(calculator.OperationsCount == 0 ? "N/A" : calculator.Operations.Max(op => op?.Result))}");
-                Console.Write("\n");
-                Console.WriteLine("---\n");
-
-                Console.WriteLine("Calculator is open for business, what would you like to do?");
-                Console.WriteLine("\t1. Perform an operation");
-                Console.WriteLine("\t2. View past operations");
-                Console.Write("Your selection: ");
-                string selection = Console.ReadLine();
-                Console.Write("\n");
-
-                while (selection != "1" && selection != "2")
+            if (selection == "2")
+            {
+                ViewOperations(calculator);
+            }
+            
+            Console.Write("Type a new number" + $"{(calculator.Operations.Count > 0 ? ", or \"r\" to use previous result," : "")}" +  " and then press Enter: ");
+            numInput1 = Console.ReadLine();
+            double cleanNum1;
+            if (numInput1.ToLower() == "r") 
+            {
+                cleanNum1 = calculator.Operations[calculator.Operations.Count - 1].Result;
+            } else
+            {
+                while (!double.TryParse(numInput1, out cleanNum1))
                 {
-                    Console.Write("Choose 1 or 2: ");
-                    selection = Console.ReadLine();
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput1 = Console.ReadLine();
                 }
+            }
 
-                if (selection == "2")
+            Console.Write("Type another number" + $"{(calculator.Operations.Count > 0 ? ", or \"r\" to use previous result," : "")}" + " and then press Enter: ");
+            numInput2 = Console.ReadLine();
+            double cleanNum2;
+            if (numInput2.ToLower() == "r")
+            {
+                cleanNum2 = calculator.Operations[calculator.Operations.Count - 1].Result;
+            } else
+            {
+                while (!double.TryParse(numInput2, out cleanNum2))
                 {
-                    ViewOperations(calculator);
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = Console.ReadLine();
                 }
-                
-                // Ask the user to type the first number.
-                Console.Write("Type a new number" + $"{(calculator.Operations.Count > 0 ? ", or \"r\" to use previous result," : "")}" +  " and then press Enter: ");
-                numInput1 = Console.ReadLine();
-                double cleanNum1;
-                if (numInput1.ToLower() == "r") 
-                {
-                    cleanNum1 = calculator.Operations[calculator.Operations.Count - 1].Result;
-                } else
-                {
-                    while (!double.TryParse(numInput1, out cleanNum1))
-                    {
-                        Console.Write("This is not valid input. Please enter an integer value: ");
-                        numInput1 = Console.ReadLine();
-                    }
-                }
+            }
 
-                // Ask the user to type the second number.
-                Console.Write("Type another number" + $"{(calculator.Operations.Count > 0 ? ", or \"r\" to use previous result," : "")}" + " and then press Enter: ");
-                numInput2 = Console.ReadLine();
-                double cleanNum2;
-                if (numInput2.ToLower() == "r")
-                {
-                    cleanNum2 = calculator.Operations[calculator.Operations.Count - 1].Result;
-                } else
-                {
-                    while (!double.TryParse(numInput2, out cleanNum2))
-                    {
-                        Console.Write("This is not valid input. Please enter an integer value: ");
-                        numInput2 = Console.ReadLine();
-                    }
-                }
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("\te - Raise to the Power");
+            Console.Write("Your option: ");
 
-                // Ask the user to choose an operator.
-                Console.WriteLine("Choose an operator from the following list:");
-                Console.WriteLine("\ta - Add");
-                Console.WriteLine("\ts - Subtract");
-                Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
-                Console.WriteLine("\te - Raise to the Power");
-                Console.Write("Your option: ");
-
-                string op = Console.ReadLine();
-                while (!operationOptions.Contains(op))
+            string op = Console.ReadLine();
+            while (!operationOptions.Contains(op))
+            {
+                Console.Write("Supply a valid option: ");
+                op = Console.ReadLine();
+            }
+            try
+            {
+                double result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                if (double.IsNaN(result))
                 {
-                    Console.Write("Supply a valid option: ");
-                    op = Console.ReadLine();
-                }
-                try
-                {
-                    double result = calculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
-                    {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
-                        calculator.Operations.Add(new Calculation
-                        {
-                            Id = calculator.OperationsCount + 1,
-                            Operand1 = cleanNum1,
-                            Operand2 = cleanNum2,
-                            Operation = op,
-                            Result = double.NaN,
-                            Success = false
-                        });
-                    }
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
-                    calculator.Operations.Add(new Calculation
-                    {
-                        Id = calculator.OperationsCount + 1,
-                        Operand1 = cleanNum1,
-                        Operand2 = cleanNum2,
-                        Operation = op,
-                        Result = result,
-                        Success = true
-
-                    });
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
                     calculator.Operations.Add(new Calculation
                     {
                         Id = calculator.OperationsCount + 1,
@@ -196,21 +163,44 @@ namespace CalculatorProgram
                         Success = false
                     });
                 }
-                finally
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+                calculator.Operations.Add(new Calculation
                 {
-                    calculator.OperationsCount++;
-                }
+                    Id = calculator.OperationsCount + 1,
+                    Operand1 = cleanNum1,
+                    Operand2 = cleanNum2,
+                    Operation = op,
+                    Result = result,
+                    Success = true
 
-                Console.WriteLine("------------------------\n");
-
-                // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-                if (Console.ReadLine() == "n") endApp = true;
-
-                Console.Write("\n"); // Friendly linespacing.
+                });
             }
-            calculator.Finish();
-            return;
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                calculator.Operations.Add(new Calculation
+                {
+                    Id = calculator.OperationsCount + 1,
+                    Operand1 = cleanNum1,
+                    Operand2 = cleanNum2,
+                    Operation = op,
+                    Result = double.NaN,
+                    Success = false
+                });
+            }
+            finally
+            {
+                calculator.OperationsCount++;
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.Write("\n");
         }
+        calculator.Finish();
+        return;
     }
 }
