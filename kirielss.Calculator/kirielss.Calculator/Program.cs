0412b9a350ce026dfd575826cebcb14325101263
skiriel <gabrieldllopes@gmail.com>
2023-04-01 20:34:37 -0300

    feat: added event loop

--- kirielss.Calculator/kirielss.Calculator/Program.cs
+++ kirielss.Calculator/kirielss.Calculator/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace kirielss.Calculator
+﻿using System.Globalization;
+
+namespace kirielss.Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            bool uptime = true;
             double x = 0; double y = 0;
             bool check;
             char selector;
@@ -11,57 +14,66 @@
             Console.WriteLine("WELCOME TO KIRIEL'S CALCULATOR!");
             Console.WriteLine("-------------------------------");
 
-            Console.WriteLine("First insert two numbers");
-            Console.Write("X = ");
-            check = double.TryParse(Console.ReadLine(), out x);
-            while (check == false)
+            while (uptime)
             {
-                Console.WriteLine("Insert a valid number!");
-                check = double.TryParse(Console.ReadLine(), out x);
-            }
-            Console.Write("Y = ");
-            check = double.TryParse(Console.ReadLine(), out y);
-            while (check == false)
-            {
-                Console.WriteLine("Insert a valid number!");
+                Console.WriteLine("First insert two numbers");
+                Console.Write("X = ");
+                check = double.TryParse((Console.ReadLine()), out x);
+                while (check == false)
+                {
+                    Console.WriteLine("Insert a valid number!");
+                    check = double.TryParse(Console.ReadLine(), out x);
+                }
+                Console.Write("Y = ");
                 check = double.TryParse(Console.ReadLine(), out y);
-            }
+                while (check == false)
+                {
+                    Console.WriteLine("Insert a valid number!");
+                    check = double.TryParse(Console.ReadLine(), out y);
+                }
 
-            Console.Clear();
-            Console.WriteLine("Now choose your operation");
-            Console.WriteLine("\ta - Addition");
-            Console.WriteLine("\ts - Subtraction");
-            Console.WriteLine("\tm - Multiplication");
-            Console.WriteLine("\td - Division");
-            selector = char.Parse(Console.ReadLine().ToLower());
+                Console.Clear();
+                Console.WriteLine("Now choose your operation");
+                Console.WriteLine("\ta - Addition");
+                Console.WriteLine("\ts - Subtraction");
+                Console.WriteLine("\tm - Multiplication");
+                Console.WriteLine("\td - Division");
+                selector = char.Parse(Console.ReadLine().ToLower());
 
-            switch (selector)
-            {
-                case 'a':
-                    Console.WriteLine($"{x} + {y} = " + (x + y));
-                    break;
-                case 's':
-                    Console.WriteLine($"{x} - {y} = " + (x - y));
-                    break;
-                case 'm':
-                    Console.WriteLine($"{x} x {y} = " + (x * y));
-                    break;
-                case 'd':
-                    if (y != 0)
-                    {
-                        Console.WriteLine($"{x} / {y} = " + (x / y));
-                    }
-                    else
-                    {
-                        while (y == 0)
+                Console.Clear();
+                switch (selector)
+                {
+                    case 'a':
+                        Console.WriteLine($"{x} + {y} = " + (x + y));
+                        break;
+                    case 's':
+                        Console.WriteLine($"{x} - {y} = " + (x - y));
+                        break;
+                    case 'm':
+                        Console.WriteLine($"{x} x {y} = " + (x * y));
+                        break;
+                    case 'd':
+                        if (y != 0)
                         {
-                            Console.WriteLine("YOU CAN'T DIVIDE BY ZERO!");
-                            Console.WriteLine("Please insert a new value as divisor");
-                            double.TryParse(Console.ReadLine(), out y);
+                            Console.WriteLine($"{x} / {y} = " + (x / y).ToString("F3", CultureInfo.InvariantCulture));
                         }
-                        Console.WriteLine($"{x} / {y} = " + (x / y));
-                    }
-                    break;
+                        else
+                        {
+                            while (y == 0)
+                            {
+                                Console.WriteLine("YOU CAN'T DIVIDE BY ZERO!");
+                                Console.WriteLine("Please insert a new value as divisor");
+                                double.TryParse(Console.ReadLine(), out y);
+                            }
+                            Console.WriteLine($"{x} / {y} = " + (x / y).ToString("F3", CultureInfo.InvariantCulture));
+                        }
+                        break;
+                }
+                Console.WriteLine("Please insert 'n' to quit, or just press enter to continue");
+                if (Console.ReadLine() == "n")
+                {
+                    uptime = false;
+                }
             }
 
 
