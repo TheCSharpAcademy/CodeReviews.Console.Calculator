d39e080022b79c628bc09acebd2f3ddf8efa7a15
Storm <esteban.palaciov@hotmail.com>
2023-04-26 22:39:06 -0500

    Sqrt operation

--- Calculator.ItsSt0rm/Program.cs
+++ Calculator.ItsSt0rm/Program.cs
@@ -16,36 +16,22 @@ class Program
         while (!endApp)
         {
             // Declare variables and set to empty.
-            string numInput1 = "";
-            string numInput2 = "";
             string menuOption = "";
-            string operationSelected = "";
-            double previousOperation = double.NaN;
             double result = 0;
+            double cleanNum1 = 0;
+            double cleanNum2 = 0;
 
             // Shows first menu
             calculator.showMenuOptions();
 
             menuOption = Console.ReadLine();
             Console.WriteLine(""); // Friendly linespacing.       
-            while (!calculator.ReadMenuOptions(menuOption))
-            {
-                Console.Write("This is not valid input. Please enter an available option: ");
-                menuOption = Console.ReadLine();
-            }
+            calculator.ReadMenuOptions(menuOption);
 
             if (menuOption.Equals("n"))
             {
                 return;
             }
-
-            // Ask the user to type the first number.           
-            double cleanNum1 = calculator.ReadNumber();
-            Console.WriteLine("First number saved");
-
-            // Ask the user to type the second number.
-            double cleanNum2 = calculator.ReadNumber();
-           Console.WriteLine("Second number saved");
 
             // Ask the user to choose an operator.
             Console.WriteLine("Choose an operator from the following list:");
@@ -53,13 +39,33 @@ class Program
             Console.WriteLine("\ts - Subtract");
             Console.WriteLine("\tm - Multiply");
             Console.WriteLine("\td - Divide");
+            Console.WriteLine("\tr - Square Root");
             Console.Write("Your option? ");
 
             string op = Console.ReadLine();
 
+            bool twoOperations = true;
+            if (op.Equals("a") || op.Equals("s") || op.Equals("m") || op.Equals("d"))
+            {
+                // Ask the user to type the first number.           
+                cleanNum1 = calculator.ReadNumber();
+                Console.WriteLine("First number saved");
+
+                // Ask the user to type the second number.
+                cleanNum2 = calculator.ReadNumber();
+                Console.WriteLine("Second number saved");
+            }
+            else
+            {
+                // Ask the user to type a number.           
+                cleanNum1 = calculator.ReadNumber();
+                Console.WriteLine("Number saved");
+                twoOperations = false;
+            }            
+
             try
             {
-                result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                result = twoOperations == true ? calculator.DoOperation(cleanNum1, cleanNum2, op) : calculator.DoOperation(cleanNum1, op);
                 if (double.IsNaN(result))
                 {
                     Console.WriteLine("This operation will result in a mathematical error.\n");
