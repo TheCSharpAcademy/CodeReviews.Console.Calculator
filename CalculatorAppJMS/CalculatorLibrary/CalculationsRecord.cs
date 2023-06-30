363af4dc0e900514e9ba4cb991c0e64c04231c4d
CarDioLogic <61765922+CarDioLogic@users.noreply.github.com>
2023-06-30 05:12:13 +0100

    Add files via upload
    Project Finito

---
+++ CalculatorAppJMS/CalculatorLibrary/CalculationsRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary;
+
+public class CalculationsRecord
+{
+    public class OperationRecord //class that holds the list structure
+    {
+        public int Id { get; set; }
+        public double numA1 { get; set; }
+        public string operation { get; set; }
+        public double NumA2 { get; set; }
+        public double resultA { get; set; }
+    }
+}
