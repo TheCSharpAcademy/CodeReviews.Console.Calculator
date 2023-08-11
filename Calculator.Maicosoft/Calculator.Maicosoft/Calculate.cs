2b8679d53436f55f99464a2205ad0587474185e8
Maico Silva <maico1978@outlook.com>
2023-08-11 12:22:12 +0200

    first setup

---
+++ Calculator.Maicosoft/Calculator.Maicosoft/Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Maicosoft
+{
+    internal class Calculate
+    {
+        internal static double Addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        internal static double Division(double num1, double num2)
+        {
+            while (num2 == 0)
+            {
+                Console.WriteLine("You cannot divide by 0, please enter a valid number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+            }
+            return num1 / num2;
+        }
+
+        internal static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        internal static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ Calculator.Maicosoft/Calculator.Maicosoft/Program.cs
@@ -0,0 +1,41 @@
+﻿using Calculator.Maicosoft;
+
+Console.WriteLine("--------------------------");
+Console.WriteLine("\tCalculator");
+Console.WriteLine("--------------------------\n");
+
+Console.WriteLine("Please enter your first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter an operation:");
+Console.WriteLine("\ta - Adittion");
+Console.WriteLine("\ts - Subtraction");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Division");
+Console.WriteLine("Make you choice: ");
+
+var operation = Console.ReadLine().Trim().ToLower();
+
+switch (operation)
+{
+    case "a":
+        var resultA = Calculate.Addition(num1, num2);
+        Console.WriteLine($"{num1} + {num2} = {resultA}");
+        break;
+    case "s":
+        var resultS =  Calculate.Subtraction(num1, num2);
+        Console.WriteLine($"{num1} - {num2} = {resultS}");
+        break;
+    case "m":
+        var resultM =  Calculate.Multiply(num1, num2);
+        Console.WriteLine($"{num1} * {num2} = {resultM}");
+        break;
+    case "d":
+        var resultD = Calculate.Division(num1, num2);
+        Console.WriteLine($"{num1} / {num2} = {resultD}");
+        break;
+}
+    
