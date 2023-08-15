11323c3cc10c358ab5fac33cf3769cbb2f0ac7bd
CharlieDW <c.dewilde04@gmail.com>
2023-08-15 19:04:10 +0200

    Initial Commit

---
+++ Calculator.CharlieDW/Calculator.CharlieDW/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Calculator.CharlieDW
+{
+    internal class Helpers
+    {
+        internal static bool IsNotANumber(string userInput)
+        {
+            string rxPattern = @"[a-zA-z]";
+
+            if (Regex.IsMatch(userInput, rxPattern))
+                return true;
+
+            return false;
+        }
+    }
+}
