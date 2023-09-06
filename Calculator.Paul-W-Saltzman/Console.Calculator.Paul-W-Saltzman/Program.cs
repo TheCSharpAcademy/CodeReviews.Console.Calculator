68880658b79f3b74d49c7fa3ccca0d897410b19c
Paul Saltzman <paul.saltzman@gmail.com>
2023-09-06 15:30:22 -0500

    Added my Solution

---
+++ Calculator.Paul-W-Saltzman/Console.Calculator.Paul-W-Saltzman/Program.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorLibrary;
+
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Calculator calculator = new Calculator();
+
+            while (!endApp)
+            {
+                Menu.ShowMenu(calculator);
+
+                endApp = Helpers.EndApp();
+            }
+            calculator.Finish();
+            return;
+        }
+    }
+}
