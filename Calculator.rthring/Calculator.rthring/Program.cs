8064e08ec31aeb53d924ba391cced49be3504bf8
rthring <rcthring@gmail.com>
2023-09-13 18:55:13 -0400

    Create basic Calculator
    Implement basic calculator that takes 2 input integers, then operates on them with the chosen operator.

---
+++ Calculator.rthring/Calculator.rthring/Program.cs
@@ -0,0 +1,36 @@
+﻿int num1 = 0; int num2 = 0;
+
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+Console.WriteLine("Type a number, and then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type another number, and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
