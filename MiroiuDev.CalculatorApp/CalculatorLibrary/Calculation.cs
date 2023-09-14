8d3f539f4dd7403c81f81e41908a9f71eb95326a
MiroiuGabriel <miroiugabriel1231@gmail.com>
2023-09-14 20:13:13 +0300

    Project Finished!

---
+++ MiroiuDev.CalculatorApp/CalculatorLibrary/Calculation.cs
@@ -0,0 +1,43 @@
+﻿namespace CalculatorLibrary
+{
+    public class Calculation
+    {
+        public DateTime CalculatedAt { get; set; }
+        public double Result { get; set; }
+        public double? Value { get; set; }
+        public double? Operand1 { get; set; }
+        public double? Operand2 { get; set; }
+        public string? Operation { get; set; }
+
+        public OperationType Type { get; set; }
+        public override string ToString()
+        {
+            string tempOperation;
+
+            if (Type == OperationType.Binary)
+            {
+                tempOperation = Operation switch
+                {
+                    "a" => "+",
+                    "s" => "-",
+                    "m" => "*",
+                    "d" => "/",
+                    "p" => "^",
+                    _ => throw new Exception("Invalid operation."),
+                };
+
+                return $"{CalculatedAt} - {Operand1} {tempOperation} {Operand2} = {Result}";
+            }
+
+            tempOperation = Operation switch
+            {
+                "l" => "sin",
+                "k" => "cos",
+                "r" => "sqrt",
+                _ => throw new Exception("Invalid operation.")
+            };
+
+            return $"{CalculatedAt} - {tempOperation}({Value}) = {Result}";
+        }
+    }
+}
---
+++ MiroiuDev.CalculatorApp/CalculatorLibrary/Helpers.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculatorLibrary
+{
+    public class Helpers
+    {
+        public static double GetNumber()
+        {
+            string? num = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(num) || !double.TryParse(num, out double _))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+                num = Console.ReadLine();
+            }
+
+            return double.Parse(num);
+        }
+    }
+}
