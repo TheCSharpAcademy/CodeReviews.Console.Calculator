a9a9ec17e7aab643b3fbcfbeddd4fc5aca76e1d6
Andreas Abaidoo <abaidooandy@gmail.com>
2023-09-21 13:40:00 +0000

    Logging operations in Json

--- Calculator.AndreasGuy54/Calculator.AndreasGuy54/Program.cs
+++ Calculator.AndreasGuy54/Calculator.AndreasGuy54/Program.cs
@@ -72,6 +72,8 @@ class Program
 
             Console.WriteLine("\n"); // Friendly linespacing.
         }
+        // Add call to close the JSON writer before return
+        calculator.Finish();
         return;
     }
 }
