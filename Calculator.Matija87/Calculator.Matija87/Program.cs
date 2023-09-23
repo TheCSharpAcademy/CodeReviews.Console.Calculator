43446fc9fb697e1fdd40a587141168c29499be1e
Matija Sedlaček <matijasedlacek@gmail.com>
2023-09-23 19:07:03 +0200

    Newtonsoft.Json

--- Calculator.Matija87/Calculator.Matija87/Program.cs
+++ Calculator.Matija87/Calculator.Matija87/Program.cs
@@ -74,6 +74,8 @@ namespace CalculatorProgram
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+            // Add call to close the JSON writer before return
+            calculator.Finish();
             return;
         }
     }
