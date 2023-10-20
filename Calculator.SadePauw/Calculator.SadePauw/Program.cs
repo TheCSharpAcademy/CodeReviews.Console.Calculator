c53538d271907da133e6a7bb362d1f83497b6bd5
SadePauw <sanderpauw@gmail.com>
2023-10-20 20:54:54 +0200

    Changes

--- Calculator.SadePauw/Calculator.SadePauw/Program.cs
+++ Calculator.SadePauw/Calculator.SadePauw/Program.cs
@@ -1,35 +1,7 @@
-﻿class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+﻿using CalculatorLibrary;
 
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
+namespace CalculatorProgram;
+
 class Program
 {
     static void Main(string[] args)
