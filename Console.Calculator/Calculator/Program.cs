47f8d374c2a9a2c86625818a3831f1558bee51ca
Ali <halilovicali03@gmail.com>
2023-10-21 11:42:33 +0200

    Finish part 1

--- Console.Calculator/Calculator/Program.cs
+++ Console.Calculator/Calculator/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace Calculator
+﻿namespace Calculator;
+
+public class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		bool endApp = false;
+		Console.WriteLine("Console Calculator in C#\r");
+		Console.WriteLine("------------------------\n");
+
+		while (!endApp)
 		{
-			Console.WriteLine("Hello, World!");
+			string numInput1 = string.Empty;
+			string numInput2 = string.Empty;
+			double result = 0;
+
+			Console.Write("Type a number, and then press Enter: ");
+			numInput1 = Console.ReadLine();
+
+			double cleanNum1 = 0;
+			while (!double.TryParse(numInput1, out cleanNum1))
+			{
+                Console.WriteLine("This is not valid input, enter a valid number: ");
+				numInput1 = Console.ReadLine();
+            }
+
+			Console.Write("Type another number, and then press Enter: ");
+			numInput2 = Console.ReadLine();
+
+			double cleanNum2 = 0;
+			while (!double.TryParse(numInput2, out cleanNum2))
+			{
+				Console.WriteLine("This is not valid input, enter a valid number: ");
+				numInput2 = Console.ReadLine();
+			}
+
+			Console.WriteLine("Choose an operator from the following list:");
+			Console.WriteLine("\ta - Add");
+			Console.WriteLine("\ts - Subtract");
+			Console.WriteLine("\tm - Multiply");
+			Console.WriteLine("\td - Divide");
+			Console.Write("Your option? ");
+
+			string op = Console.ReadLine();
+
+			try
+			{
+				result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+				if (double.IsNaN(result))
+                    Console.WriteLine("This operation will result in error.");
+				else
+                    Console.WriteLine("Your result: {0:0.##}", result);
+            } catch (Exception e)
+			{
+                Console.WriteLine("An exception ocurred trying to do the math.");
+                Console.WriteLine("Details: " + e.Message);
+            }
+
+			Console.WriteLine("------------------------\n");
+
+			Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+			if (Console.ReadLine() == "n") endApp = true;
+
+			Console.WriteLine("\n"); // Friendly linespacing.
 		}
 	}
 }
