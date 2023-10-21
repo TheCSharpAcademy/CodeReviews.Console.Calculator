c7e0c5056d115a5d62b22e0b3fcbe5097b979cf3
Ali <halilovicali03@gmail.com>
2023-10-21 12:24:58 +0200

    Finish part 2

--- Console.Calculator/Calculator/Program.cs
+++ Console.Calculator/Calculator/Program.cs
@@ -1,6 +1,8 @@
-﻿namespace Calculator;
+﻿using CalculatorLibrary;
 
-public class Program
+namespace CalculatorProgram;
+
+class Program
 {
 	static void Main(string[] args)
 	{
@@ -8,29 +10,30 @@ public class Program
 		Console.WriteLine("Console Calculator in C#\r");
 		Console.WriteLine("------------------------\n");
 
+		Calculator calculator = new Calculator();
 		while (!endApp)
 		{
-			string numInput1 = string.Empty;
-			string numInput2 = string.Empty;
-			double result = 0;
+			string? numInput1;
+			string? numInput2;
+			double result;
 
 			Console.Write("Type a number, and then press Enter: ");
 			numInput1 = Console.ReadLine();
 
-			double cleanNum1 = 0;
+			double cleanNum1;
 			while (!double.TryParse(numInput1, out cleanNum1))
 			{
-                Console.WriteLine("This is not valid input, enter a valid number: ");
+				Console.Write("This is not valid input. Please enter an integer value: ");
 				numInput1 = Console.ReadLine();
-            }
+			}
 
 			Console.Write("Type another number, and then press Enter: ");
 			numInput2 = Console.ReadLine();
 
-			double cleanNum2 = 0;
+			double cleanNum2;
 			while (!double.TryParse(numInput2, out cleanNum2))
 			{
-				Console.WriteLine("This is not valid input, enter a valid number: ");
+				Console.Write("This is not valid input. Please enter an integer value: ");
 				numInput2 = Console.ReadLine();
 			}
 
@@ -41,27 +44,31 @@ public class Program
 			Console.WriteLine("\td - Divide");
 			Console.Write("Your option? ");
 
-			string op = Console.ReadLine();
+			string? op = Console.ReadLine();
 
 			try
 			{
-				result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+				result = calculator.DoOperation(cleanNum1, cleanNum2, op);
 				if (double.IsNaN(result))
-                    Console.WriteLine("This operation will result in error.");
-				else
-                    Console.WriteLine("Your result: {0:0.##}", result);
-            } catch (Exception e)
+				{
+					Console.WriteLine("This operation will result in a mathematical error.\n");
+				}
+				else Console.WriteLine("Your result: {0:0.##}\n", result);
+			}
+			catch (Exception e)
 			{
-                Console.WriteLine("An exception ocurred trying to do the math.");
-                Console.WriteLine("Details: " + e.Message);
-            }
+				Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+			}
 
 			Console.WriteLine("------------------------\n");
 
 			Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
 			if (Console.ReadLine() == "n") endApp = true;
 
-			Console.WriteLine("\n"); // Friendly linespacing.
+			Console.WriteLine("\n");
 		}
+
+		calculator.Finish();
+		return;
 	}
 }
