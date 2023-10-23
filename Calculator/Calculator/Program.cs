40b8134fe467f427cce9c998f04ab8b7297f2576
Veronika <116470467+vilvee@users.noreply.github.com>
2023-10-23 10:06:44 -0400

    ready for review

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,152 @@
+﻿using CalculatorLibrary;
+
+namespace CalculatorProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize Calculator Class
+            Calculator calculator = new();
+            bool endApp = false;
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                double num1;
+                double? num2 = null;
+                double retrievedResult = 0;
+                bool retrievedResults = false;
+
+                do
+                {
+                    // Ask the user to choose an operator.
+                    Menu();
+
+                    //validate user input for operation
+                    int userChoice = ValidateInt("Your option? ");
+
+                    // Ask the user to type the first number or check for history retrieve
+                    if (retrievedResults)
+                    {
+                        num1 = retrievedResult;
+                        retrievedResults = false;
+                    }
+                    else
+                    {
+                        num1 = ValidateDouble("Type a number, and then press Enter: ");
+                    }
+
+
+                    //if operation requires two numbers
+                    if (userChoice <= (int)Operation.Power)
+                    {
+                        num2 = ValidateDouble("Type another number, and then press Enter: ");
+                    }
+
+
+                    try
+                    {
+                        double result = calculator.DoOperation(num1, (Operation)userChoice, num2);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+
+                    Console.WriteLine("------------------------\n");
+
+
+                    // Wait for the user to respond before closing.
+                    Console.Write("Press 'q' and Enter to close the app\nPress 'h' to view your calculation history\nPress any other key and Enter to continue: ");
+
+                    string userInput = Console.ReadLine();
+
+                    switch (userInput.ToLower())
+                    {
+                        case "q":
+                            Console.WriteLine($"Times used: {calculator.TimesUsed}");
+                            endApp = true;
+                            break;
+                        case "h":
+                            retrievedResults = DisplayHistory(calculator);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input.");
+                            break;
+                    }
+
+                } while (!endApp);
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            // Add call to close the JSON writer before return
+            calculator.Finish();
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Choose an operator from the following list:");
+
+            foreach (var op in Enum.GetValues(typeof(Operation)))
+            {
+                Console.WriteLine($"\t{(int)op} - {op}");
+            }
+        }
+
+        private static int ValidateInt(string prompt)
+        {
+            Console.WriteLine(prompt);
+            int cleanInput;
+            while (!int.TryParse(Console.ReadLine(), out cleanInput))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+            }
+
+            return cleanInput;
+        }
+
+        private static double ValidateDouble(string prompt)
+        {
+            Console.WriteLine(prompt);
+            double cleanInput;
+            while (!double.TryParse(Console.ReadLine(), out cleanInput))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+            }
+
+            return cleanInput;
+        }
+
+        static bool DisplayHistory(Calculator calculator)
+        {
+            calculator.PrintHistory();
+            Console.WriteLine("Enter the entry number to retrieve result or 'd' to delete history.");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "d")
+            {
+                calculator.DeleteHistory();
+                Console.WriteLine("History deleted");
+                return false;
+            }
+            else if (int.TryParse(input, out int num))
+            {
+                calculator.RetrieveCalculationFromHistory(num);
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
---
+++ Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,168 @@
+﻿
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public enum Operation
+    { 
+        Add = 1,
+        Subtract,
+        Multiply,
+        Divide,
+        Power,
+        SquareRoot,
+        TenX,
+        Sine,
+        Cosine,
+        Tangent, 
+        Cotangent,
+        Secant,
+        Cosecant
+    }
+
+    public class Calculator
+    {
+        JsonWriter writer;
+        private List<string> calculationHistory = new List<string>(); // store history
+        public int TimesUsed { get; private set; }
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+            TimesUsed = 0;
+            calculationHistory = new List<string>();
+        }
+
+        public double DoOperation(double num1, Operation op, double? num2 = null)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+
+            if(num2 != null)
+            {
+                writer.WritePropertyName("Operand2");
+                writer.WriteValue(num2);
+            }
+
+            writer.WritePropertyName("Operation");
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case Operation.Add:
+                    result = num1 + num2.Value;
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Subtract:
+                    result = num1 - num2.Value;
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Multiply:
+                    result = num1 * num2.Value;
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Divide:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2.Value;
+                    }
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Power:
+                    result = Math.Pow(num1, num2.Value);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.SquareRoot:
+                    result = Math.Sqrt(num1);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.TenX:
+                    result = num1 * 10;
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Sine:
+                    result = Math.Sin(num1);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Cosine:
+                    result = Math.Cos(num1);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Tangent:
+                    result = Math.Tan(num1);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Cotangent:
+                    result = 1 / Math.Tan(num1);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Secant:
+                    result = 1 / Math.Cos(num1);
+                    writer.WriteValue(op);
+                    break;
+                case Operation.Cosecant:
+                    result = 1/ Math.Sin(num1);
+                    writer.WriteValue(op);
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            
+            TimesUsed++;
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            StoreCalculationHistory(num1, op, result, num2);
+
+            return result;
+        }
+
+        public double RetrieveCalculationFromHistory(int num)
+        {
+            string entry = calculationHistory[num - 1];
+
+            return Convert.ToDouble(entry.Split( " ").Last());
+        }
+
+
+        public void StoreCalculationHistory(double num1, Operation op, double result, double? num2 = null)
+        {
+ 
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Operation: {0}\n", op);
+            sb.AppendFormat("Operand 1: {0}\n", num1);
+
+            if (num2 != null)
+            {
+                sb.AppendFormat("Operand 2: {0}\n", num2);
+            }
+            
+            sb.AppendFormat("Result: {0}\n", result);
+
+            calculationHistory.Add(sb.ToString());
+        }
+
+        public void PrintHistory() => calculationHistory.ForEach(entry => Console.WriteLine($"[{calculationHistory.IndexOf(entry) + 1}] {entry}"));
+        
+        public void DeleteHistory() => calculationHistory.Clear();
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
