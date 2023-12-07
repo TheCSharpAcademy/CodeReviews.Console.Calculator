02636317d5087ab5bf60f2b9fb19a2b7b1f8a2b0
gazzz07 <124935058+gazzz07@users.noreply.github.com>
2023-12-07 00:00:54 +0000

    Finished calculator with 3 out of 4 challenges

---
+++ gazzz07.Calculator/Calculator/Program.cs
@@ -0,0 +1,116 @@
+﻿using CalculatorLibrary;
+namespace CalculatorProgram
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+            int timesUsed = 0;
+            bool endApp = false;
+
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("--------------------------");
+
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+
+                Console.WriteLine("Type a number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+                
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.WriteLine("This is not a valid input. Please enter an integer: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose an operator from the following list: ");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\tr - Square Root");
+                Console.WriteLine("\tp - Power");
+                Console.WriteLine("\t10x - 10x");
+                Console.WriteLine("\tsin - Sin");
+                Console.WriteLine("\ttan - Tan");
+                Console.WriteLine("\tcos - Cos");
+                Console.WriteLine("Your option?: ");
+
+                string op = Console.ReadLine();
+
+                if (op == "a" || op == "s" || op == "m" || op == "d" || op == "p")
+                {
+                    Console.WriteLine("Type another number, and then press Enter: ");
+                    numInput2 = Console.ReadLine();
+
+                    double cleanNum2 = 0;
+                    while (!double.TryParse(numInput2, out cleanNum2))
+                    {
+                        Console.WriteLine("This is not a valid input. Please enter an integer: ");
+                        numInput2 = Console.ReadLine();
+                    }
+                    try
+                    {
+                        result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error. \n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the maths.\n - Details: " + e.Message);
+                    }
+                } else if (op == "r" || op == "10x" || op == "sin" || op == "tan" || op == "cos")
+                {
+                    try
+                    {
+                    result = calculator.SingleDigitOp(cleanNum1, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error. \n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the maths.\n - Details: " + e.Message);
+                    } 
+                } else
+                {
+                    Console.WriteLine("NO!");
+                }
+
+                Console.WriteLine("------------------------");
+
+                timesUsed++;
+                Console.WriteLine($"Calculator has been used {timesUsed} times in this session.");
+                Console.WriteLine("History: ");
+                foreach (var sum in calculator.sums)
+                {
+                    Console.WriteLine(sum);
+                }
+
+                Console.WriteLine("------------------------");
+
+                Console.WriteLine("Would you like to clear your recent sums? (y/n): ");
+                if (Console.ReadLine().ToLower() == "y") calculator.sums.Clear();
+
+                Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n");
+            }
+            calculator.Finish();
+            return;
+        }
+    }
+}
---
+++ gazzz07.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+        public List<string> sums;
+
+        public Calculator()
+        {
+            sums = new List<string>();
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    sums.Add($"{num1} + {num2} = {result}");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    sums.Add($"{num1} - {num2} = {result}");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    sums.Add($"{num1} * {num2} = {result}");
+                    break;
+                case "d":
+
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        writer.WriteValue("Divide");
+                        sums.Add($"{num1} / {num2} = {result}");
+                    }
+                    break;
+                case "p":
+                    result = Math.Pow(num1, num2);
+                    writer.WriteValue("Multiply");
+                    sums.Add($"{num1} to the power of {num2} = {result}");
+                    break;
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public double SingleDigitOp(double num1, string op2)
+        {
+            double result = double.NaN;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operation");
+
+            switch (op2)
+            {
+                case "r":
+                    result = Math.Sqrt((double)num1);
+                    writer.WriteValue("Square root");
+                    sums.Add($"Square root of {num1} = {result}");
+                    break;
+                case "10x":
+                    result = num1 * 10;
+                    writer.WriteValue("10x");
+                    sums.Add($"10x {num1} = {result}");
+                    break;
+                case "sin":
+                    result = Math.Sin(num1);
+                    writer.WriteValue("Sin");
+                    sums.Add($"Sin of {num1} = {result}");
+                    break;
+                case "tan":
+                    result = Math.Tan(num1);
+                    writer.WriteValue("Tan");
+                    sums.Add($"Tan of {num1} = {result}");
+                    break;
+                case "cos":
+                    result = Math.Cos(num1);
+                    writer.WriteValue("Cos");
+                    sums.Add($"Cos of {num1} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
