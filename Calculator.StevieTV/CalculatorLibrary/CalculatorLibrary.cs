92bc0d98d7f262c1b4ac51beb1666a6eac1d95c6
Stevie Robinson <stevie.robinson@gmail.com>
2023-12-28 15:33:26 +0100

    Added operation counter, and operation list

--- Calculator.StevieTV/CalculatorLibrary/CalculatorLibrary.cs
+++ Calculator.StevieTV/CalculatorLibrary/CalculatorLibrary.cs
@@ -1,10 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using CalculatorLibrary.Models;
+using Newtonsoft.Json;
 
 namespace CalculatorLibrary;
 
 public class Calculator
 {
     JsonWriter writer;
+    public int timesUsed = 0;
+    internal static List<Operation> operations = new List<Operation> { };
     public Calculator()
     {
         StreamWriter logFile = File.CreateText("calculatorlog.json");
@@ -18,8 +21,11 @@ public class Calculator
 
     public double DoOperation(double num1, double num2, string op)
     {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        var result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        var operation = "";
         writer.WriteStartObject();
+        writer.WritePropertyName("Sequence");
+        writer.WriteValue(timesUsed + 1);
         writer.WritePropertyName("Operand1");
         writer.WriteValue(num1);
         writer.WritePropertyName("Operand2");
@@ -32,22 +38,36 @@ public class Calculator
             case "a":
                 result = num1 + num2;
                 writer.WriteValue("Add");
+                timesUsed++;
+                operation = "+";
                 break;
             case "s":
                 result = num1 - num2;
                 writer.WriteValue("Subtract");
+                timesUsed++;
+                operation = "-";
                 break;
             case "m":
                 result = num1 * num2;
                 writer.WriteValue("Multiply");
+                timesUsed++;
+                operation = "*";
                 break;
             case "d":
                 // Ask the user to enter a non-zero divisor.
                 if (num2 != 0)
                 {
                     result = num1 / num2;
-                    writer.WriteValue("Divide");
                 }
+                writer.WriteValue("Divide");
+                timesUsed++;
+                operation = "/";
+                break;
+            case "p":
+                result = Math.Pow(num1, num2);
+                writer.WriteValue("Power Of");
+                timesUsed++;
+                operation = "^";
                 break;
             // Return text for an incorrect option entry.
             default:
@@ -56,7 +76,7 @@ public class Calculator
         writer.WritePropertyName("Result");
         writer.WriteValue(result);
         writer.WriteEndObject();
-
+        RecordOperation(timesUsed, num1, num2, result, operation);
         return result;
     }
 
@@ -65,5 +85,25 @@ public class Calculator
         writer.WriteEndArray();
         writer.WriteEndObject();
         writer.Close();
+    }
+
+    public void ShowHistory()
+    {
+        foreach (var operation in operations)
+        {
+            Console.WriteLine($"{operation.Sequence}: {operation.Operand1}{operation.OperationType}{operation.Operand2} = {operation.Result}");
+        }
+    }
+
+    internal static void RecordOperation(int sequence, double operand1, double operand2, double result, string operation)
+    {
+        operations.Add(new Operation
+        {
+            Sequence = sequence,
+            Operand1 = operand1,
+            Operand2 = operand2,
+            Result = result,
+            OperationType = operation,
+        });
     }
 }
---
+++ Calculator.StevieTV/CalculatorLibrary/Models/Operations.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorLibrary.Models
+{
+    internal class Operation
+    {
+        public int Sequence { get; set; }
+        public double Operand1 { get; set; }
+        public double Operand2 { get; set; }
+        public double Result { get; set; }
+        public string OperationType { get; set; }
+    }
+}
