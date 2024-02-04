46de690b165a5a467de78bf531aa66cb23ee899e
Pugnetta <115831639+Pugnetta@users.noreply.github.com>
2024-02-04 21:42:27 +0100

    Calculator first commit

---
+++ Pugnetta.Calculator/Pugnetta.Calculator/Operation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pugnetta.Calculator;
+
+public record Operation
+{
+    public Guid Id { get; }
+    public decimal N1 { get; }
+    public decimal N2 { get; }
+    public Operator Operator { get; }
+    public string? Result { get; init; } = null;
+    private Operation(Guid id, decimal n1, decimal n2, Operator o) =>
+        (Id, N1, N2, Operator) = (id, n1, n2, o);    
+    public static Operation Create(decimal n1, decimal n2, Operator o) =>
+        new(Guid.NewGuid(), n1, n2, o);
+    public static Operation Create(Operation operation, decimal result) =>
+        operation with { Result = result.ToString() };
+}
+public enum Operator
+{
+    Sum,
+    Sub,
+    Mol,
+    Div
+}
+public static class OperationExtentions
+{
+    public static decimal Execute(this Operation operation, Operator o) => o switch
+    {
+        Operator.Sum => operation.N1 + operation.N2,
+        Operator.Sub => operation.N1 - operation.N2,
+        Operator.Mol => operation.N1 * operation.N2,
+        Operator.Div when operation.N2 != 0 => operation.N1 / operation.N2,
+        Operator.Div when operation.N2 == 0 =>
+        throw new ArgumentException("Cant divide by 0"),
+        _ => throw new InvalidOperationException(nameof(o))
+    };
+}
---
+++ Pugnetta.Calculator/Pugnetta.Calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using Pugnetta.Calculator;
+using System.Globalization;
+
+Console.WriteLine("Note: decimals are defined with \".\"");
+CultureInfo cultureInfo = CultureInfo.InvariantCulture;
+var failedOperations = new List<Operation>();
+var completedOperations = new List<Operation>();
+var operators = Enum.GetValues(typeof(Operator)).Cast<Operator>().ToArray();
+var map = Enumerable.Range(0, operators.Length).ToDictionary(key => key, val => operators[val]);
+while (true)
+{
+    foreach (var kvp in map) Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+    var op = OperatorSelectionLoop();
+    var (n1, n2) = NumberSelectionLoop();
+    var operation = Operation.Create(n1, n2, op);
+    try
+    {
+        var res = operation.Execute(op);
+        completedOperations.Add(Operation.Create(operation, res));
+        Console.WriteLine($"Operation result: {res}");
+    }
+    catch (Exception ex)
+    {
+        failedOperations.Add(operation);
+        Console.WriteLine(ex.ToString());
+    }
+    Console.Read();
+    Console.Clear();
+}
+
+Operator OperatorSelectionLoop()
+{
+    while (true)
+    {
+        Console.WriteLine("Select operation id");
+        bool b = int.TryParse(Console.ReadLine(), out int r);
+        if (b && map.TryGetValue(r, out var op)) return op;
+    }
+}
+(decimal n1, decimal n2) NumberSelectionLoop()
+{
+    bool firstSelected = false;
+    bool secondSelected = false;
+    decimal n1 = 0, n2 = 0;
+    while (true)
+    {
+        if (secondSelected) return (n1, n2);
+        if (!firstSelected)
+        {
+            Console.WriteLine("Select operation number1");
+            if (decimal.TryParse(Console.ReadLine(),
+                cultureInfo, out var r))
+            {
+                n1 = r; firstSelected = true;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Select operation number2");
+            if (decimal.TryParse(Console.ReadLine(),
+                cultureInfo, out var r))
+            {
+                n2 = r; secondSelected = true;
+            }
+        }
+    }
+}
