346260464284da998889ab0b3c743e24d136601b
Deviationlark <zilamovpetar@gmail.com>
2024-02-24 14:51:20 +0100

    Finished Calculator

---
+++ Calculator.Deviationlark/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,151 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        // if i delete this it gives an error on its absence
+        // ##########################
+        static void Main() { }
+        // ##########################
+        JsonWriter writer;
+        List<string> calculations = new List<string>();
+        List<double> results = new List<double>();
+        int count = 0;
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    History(num1, num2, result, op);
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    History(num1, num2, result, op);
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    History(num1, num2, result, op);
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    writer.WriteValue("Divide");
+                    History(num1, num2, result, op);
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    Console.WriteLine("Thats not an operation.");
+                    writer.WriteValue("Invalid operation");
+                    Console.ReadLine();
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+        public void History(double num1 = 0, double num2 = 0, double result = 0, string op = "")
+        {
+            op = Operator(op);
+            calculations.Add($"{count + 1}. {num1} {op} {num2} = {result}");
+            results.Add(result);
+            count++;
+        }
+
+        public double ShowHistory()
+        {
+            Console.Clear();
+            double num1 = 0;
+            string? result = "";
+            foreach (string calculation in calculations)
+            {
+                Console.WriteLine(calculation);
+            }
+            while (string.IsNullOrEmpty(result))
+            {
+
+                Console.WriteLine("u. Use previous result");
+                Console.WriteLine("d. Delete history");
+                Console.WriteLine("Enter any other key to do another calculation.");
+                result = Console.ReadLine();
+            }
+            if (result == "u")
+            {
+                Console.WriteLine("Type the number of the result you would like to use: ");
+                result = Console.ReadLine();
+                if (int.TryParse(result, out _) && Convert.ToDouble(result) > 0)
+                    num1 = Convert.ToDouble(results[(Convert.ToInt32(result) - 1)]);
+                else
+                {
+                    Console.WriteLine("Invalid number");
+                    Console.ReadLine();
+                }
+            }
+            else if (result == "d")
+            {
+                calculations.Clear();
+                results.Clear();
+                count = 0;
+            }
+            return num1;
+        }
+
+        public string Operator(string op)
+        {
+            switch (op)
+            {
+                case "a":
+                    op = "+";
+                    break;
+                case "s":
+                    op = "-";
+                    break;
+                case "m":
+                    op = "*";
+                    break;
+                case "d":
+                    op = "/";
+                    break;
+            }
+            return op;
+        }
+    }
+}
---
+++ Calculator.Deviationlark/Program.cs
@@ -0,0 +1,95 @@
+﻿using CalculatorLibrary;
+
+bool endApp = false;
+string? readlineResult;
+Calculator calculator = new Calculator();
+double cleanNum1 = 0;
+int count = 0;
+
+// Display title as the C# console calculator app.
+Console.Clear();
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+while (!endApp)
+{
+    // Declare variables and set to empty.
+    string? numInput1 = "";
+    string? numInput2 = "";
+    double result = 0;
+
+    if (cleanNum1 == 0)
+    {    // Ask the user to type the first number.
+        Console.Write("Type a number, and then press Enter: ");
+        numInput1 = Console.ReadLine();
+        while (!double.TryParse(numInput1, out cleanNum1))
+        {
+            Console.Write("This is not valid input. Please enter an integer value: ");
+            numInput1 = Console.ReadLine();
+        }
+    }
+    Console.Clear();
+    Console.WriteLine(cleanNum1);
+    // Ask the user to type the second number.
+    Console.Write("Type another number, and then press Enter: ");
+    numInput2 = Console.ReadLine();
+
+    double cleanNum2 = 0;
+    while (!double.TryParse(numInput2, out cleanNum2))
+    {
+        Console.Write("This is not valid input. Please enter an integer value: ");
+        numInput2 = Console.ReadLine();
+    }
+    // Ask the user to choose an operator.
+    string? op = "";
+    while (string.IsNullOrEmpty(op))
+    {
+        Console.Clear();
+        Console.WriteLine($"{cleanNum1} {cleanNum2}");
+        Console.WriteLine("Choose an operator from the following list:");
+        Console.WriteLine("\ta - Add");
+        Console.WriteLine("\ts - Subtract");
+        Console.WriteLine("\tm - Multiply");
+        Console.WriteLine("\td - Divide");
+        Console.Write("Your option: ");
+        op = Console.ReadLine();
+    }
+    try
+    {
+        result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+        if (double.IsNaN(result))
+        {
+            Console.WriteLine("This operation will result in a mathematical error.\n");
+        }
+        else
+        {
+            Console.Clear();
+            op = calculator.Operator(op);
+            Console.WriteLine($"{cleanNum1} {op} {cleanNum2} = ");
+            Console.WriteLine("Your result: {0:0.##}\n", result);
+            count++;
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+        calculator.Finish();
+    }
+
+    Console.WriteLine("------------------------\n");
+    Console.WriteLine($"Calculations done:{count}");
+    // Wait for the user to respond before closing.
+    Console.WriteLine("Press 'n' and Enter to close the app");
+    Console.WriteLine("Press 'h' and Enter to view history");
+    Console.WriteLine("Press any other key and/or Enter to do another calculation");
+    readlineResult = Console.ReadLine();
+    if (readlineResult == "n") endApp = true;
+    else if (readlineResult == "h")
+    {
+        cleanNum1 = calculator.ShowHistory();
+    }
+    else
+        cleanNum1 = 0;
+
+    Console.WriteLine("\n"); // Friendly linespacing.
+}
+calculator.Finish();
