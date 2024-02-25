e94be6a075dcc59c56f1d70126fd8ae365d177c9
mfawzy1337 <mfawzy1337@gmail.com>
2024-02-25 19:16:26 +0200

    Project Finished

---
+++ CalculatorConsoleUI/ConsoleApp1/ConsoleHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ConsoleHelper
+    {
+        public static string GetStringFromUser(string message)
+        {
+            string output;
+            string userInput;
+            do
+            {
+                Console.Write($"{message}: ");
+                userInput = Console.ReadLine();
+            } while (!isValidInput(userInput, InputType.CHOICE));
+
+            output = userInput.Trim().ToLower();
+            return output;
+        }
+        public static double GetNumberFromUser(string message)
+        {
+            double output;
+            string userInput;
+            do
+            {
+                Console.Write($"{message}: ");
+                userInput = Console.ReadLine();
+            } while (!isValidInput(userInput, InputType.NUMBER));
+            output = userInput.CastToDouble();
+            return output;
+        }
+        public static bool isValidInput(string input, InputType option)
+        {
+            bool result = false;
+            if (!input.IsEmptyOrNull())
+            {
+                switch (option)
+                {
+                    case InputType.NUMBER:
+                        {
+                            result = double.TryParse(input, out double temp);
+                            break;
+                        }
+                    case InputType.CHOICE:
+                        {
+                            result = input.IsValidChoice();
+                            break;
+                        }
+                }
+            }
+            return result;
+
+        }
+        public static bool IsEmptyOrNull(this string input) 
+        { 
+            return input == null || input == string.Empty;
+        }
+        public static bool IsValidChoice(this string input)
+        {
+            bool result = false;
+            if (char.TryParse(input.Trim(), out char choice))
+            {
+                result = true;
+            }
+            return result;
+
+        }
+        public static double CastToDouble(this string input)
+        {
+            double.TryParse(input, out double result);
+            return result;
+        }
+        public static double GetNumberOrUsePickedHistoryResult(string message, double? historyResult = null)
+        {
+            double result;
+
+            if (historyResult == null)
+            {
+                result = ConsoleHelper.GetNumberFromUser(message);
+            }
+            else
+            {
+                Console.Write($"{message} - or Enter 'ans' to use picked result from result: ");
+                string answer = Console.ReadLine().Trim().ToLower();
+                if (answer == "ans")
+                {
+                    result = historyResult.Value;
+                }
+                else
+                {
+                    while (!double.TryParse(answer, out result))
+                    {
+                        Console.Write($"{message}: ");
+                        answer = Console.ReadLine();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
