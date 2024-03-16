5dad3df197bb566e7944fdfdf0547f90a7979894
GetTeched <rathmaza@gmail.com>
2024-03-17 00:48:23 +0200

    Initial Commit

---
+++ GetTeched.Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * For your second project you'll build a Console Calculator App with the help of Microsoft’s Documentation.
+ * This project shouldn't be more difficult than the first, but you'll learn important skills such as having multiple
+ * projects in a solution, writing to files, and debugging. It will also serve as practice in a very important skill
+ * following written documentation. This is something you’ll be doing on a regular basis as a professional
+ * developer, so it’s essential that you’re comfortable applying text-based instructions when developing software.
+ * 
+ ******************************************CHALLENGES***************************************************************
+ *
+ * Create a functionality that will count the amount of times the calculator was used.
+ * Store a list with the latest calculations. And give the users the ability to delete that list.
+ * Allow the users to use the results in the list above to perform new calculations.
+ * Add extra calculations: Square Root, Taking the Power, 10x, Trigonometry functions.
+ */
