09886dc64db3b284c2b393ac97002e91bee390fa
GetTeched <rathmaza@gmail.com>
2024-03-17 20:45:37 +0200

    Tutorial Part 2 Complete

--- GetTeched.Calculator/Program.cs
+++ GetTeched.Calculator/Program.cs
@@ -12,81 +12,90 @@
  * Allow the users to use the results in the list above to perform new calculations.
  * Add extra calculations: Square Root, Taking the Power, 10x, Trigonometry functions.
  */
-
-using GetTeched.Calculator;
+using CalculatorLibrary;
 using System.Text.RegularExpressions;
+namespace CalculatorProgram;
 
-bool endApplication = false;
-
-
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-while (!endApplication)
+class Program
 {
-    string? firstNumber = "";
-    string? secondNumber = "";
-    double result = 0;
-
-    Console.WriteLine("Type a number, and then press Enter.");
-    firstNumber = Console.ReadLine();
-
-    double cleanFirstNumber = 0;
-    while (!double.TryParse(firstNumber, out cleanFirstNumber))
+    static void Main(string[] args)
     {
-        Console.WriteLine("This is not a vaild input. Please enter an integer value: ");
-        firstNumber = Console.ReadLine();
-    }
+        bool endApplication = false;
+        Calculator calculator = new();
 
-    Console.WriteLine("Type a number, and then press Enter.");
-    secondNumber = Console.ReadLine();
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
 
-    double cleanSecondNumber = 0;
-    while (!double.TryParse(secondNumber, out cleanSecondNumber))
-    {
-        Console.WriteLine("This is not a vaild input. Please enter an integer value: ");
-        secondNumber = Console.ReadLine();
-    }
-
-    Console.WriteLine("Choose an option from the following list:");
-    Console.WriteLine("\ta - Add");
-    Console.WriteLine("\ts - Subtract");
-    Console.WriteLine("\tm - Multiply");
-    Console.WriteLine("\td - Divide");
-    Console.Write("Your option? ");
-
-    string operation = Console.ReadLine();
-
-    if (operation == null || !Regex.IsMatch(operation, "[a|s|m|d]"))
-    {
-        Console.WriteLine("Error: Unrecognized input.");
-    }
-    else
-    {
-        try
+        while (!endApplication)
         {
-            result = Calculator.DoOperation(cleanFirstNumber, cleanSecondNumber, operation);
-            if (double.IsNaN(result))
+            string? firstNumber = "";
+            string? secondNumber = "";
+            double result = 0;
+
+            Console.WriteLine("Type a number, and then press Enter.");
+            firstNumber = Console.ReadLine();
+
+            double cleanFirstNumber = 0;
+            while (!double.TryParse(firstNumber, out cleanFirstNumber))
             {
-                Console.WriteLine("This operation will result in a mathematical error.\n");
+                Console.WriteLine("This is not a vaild input. Please enter an integer value: ");
+                firstNumber = Console.ReadLine();
+            }
+
+            Console.WriteLine("Type a number, and then press Enter.");
+            secondNumber = Console.ReadLine();
+
+            double cleanSecondNumber = 0;
+            while (!double.TryParse(secondNumber, out cleanSecondNumber))
+            {
+                Console.WriteLine("This is not a vaild input. Please enter an integer value: ");
+                secondNumber = Console.ReadLine();
+            }
+
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            string operation = Console.ReadLine();
+
+            if (operation == null || !Regex.IsMatch(operation, "[a|s|m|d]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
             }
             else
             {
-                Console.WriteLine("Your reslut: {0:0.##}\n", result);
+                try
+                {
+                    result = calculator.DoOperation(cleanFirstNumber, cleanSecondNumber, operation);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your reslut: {0:0.##}\n", result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
             }
+            Console.WriteLine("------------------------\n");
+
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApplication = true;
+
+            Console.WriteLine("\n");
+
         }
-        catch (Exception e)
-        {
-            Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
-        }
+        calculator.Finish();
+        return;
     }
-    Console.WriteLine("------------------------\n");
-
-    Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-    if (Console.ReadLine() != null) endApplication = true;
-
-    Console.WriteLine("\n");
-
+    
 }
-return;
+
 
---
+++ GetTeched.CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+namespace CalculatorLibrary;
+public class Calculator
+{
+    JsonWriter writer;
+    public Calculator()
+    {
+        StreamWriter logFile = File.CreateText("calculator.json");
+        logFile.AutoFlush = true;
+        writer = new JsonTextWriter(logFile);
+        writer.Formatting = Formatting.Indented;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operations");
+        writer.WriteStartArray();
+    }
+    public double DoOperation(double firstNumber, double secondNumber, string operation)
+    {
+        double result = double.NaN;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operand1");
+        writer.WriteValue(firstNumber);
+        writer.WritePropertyName("Operand2");
+        writer.WriteValue(secondNumber);
+        writer.WritePropertyName("Operation");
+
+        switch (operation)
+        {
+            case "a":
+                result = firstNumber + secondNumber;
+                writer.WriteValue("Add");
+                break;
+            case "s":
+                result = firstNumber - secondNumber;
+                writer.WriteValue("Subract");
+                break;
+            case "m":
+                result = firstNumber * secondNumber;
+                writer.WriteValue("Multiply");
+                break;
+            case "d":
+                if (secondNumber != 0)
+                {
+                    result = firstNumber / secondNumber;
+                    writer.WriteValue("Divide");
+                }
+                break;
+            default:
+                break;
+        }
+        writer.WritePropertyName("Result");
+        writer.WriteValue(result);
+        writer.WriteEndObject();
+        return result;
+    }
+
+    public void Finish()
+    {
+        writer.WriteEndArray();
+        writer.WriteEndObject();
+        writer.Close();
+    }
+}
+
