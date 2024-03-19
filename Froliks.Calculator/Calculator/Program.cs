c8a0ade74b3368a7feaa34482039159f6552d229
Dima Yanushevych <darkness1198@gmail.com>
2024-03-19 20:25:19 +0200

    calculator project

---
+++ Froliks.Calculator/Calculator/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CalculatorProgram;
+
+public class Calculator
+{
+    private static CalculatorLibrary.Calculator _calculator = new CalculatorLibrary.Calculator();
+    private static bool usePreviousResult = false;
+
+    public static void Main(string[] args)
+    {
+        ShowMenu();
+    }
+
+    private static void ShowMenu()
+    {
+        var endApp = false;
+        while (!endApp)
+        {
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("\tc - Go to calculator");
+
+            if (_calculator.GetOperationsCount() != 0)
+            {
+                Console.WriteLine("\tp - Use calculator with previous result");
+            }
+            
+            Console.WriteLine("\td - Delete previous calculations");
+            Console.WriteLine("\te - Exit app");
+            Console.Write("Your option? ");
+            
+            switch (Console.ReadLine())
+            {
+                case "c":
+                    Console.Clear();
+                    usePreviousResult = false;
+                    ShowCalculator();
+                    break;
+                case "p" when _calculator.GetOperationsCount() > 0:
+                    Console.Clear();
+                    usePreviousResult = true;
+                    ShowCalculator();
+                    break;
+                case "d":
+                    _calculator.DeleteData();
+                    break;
+                case "e":
+                    endApp = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void ShowCalculator()
+    {
+        var endApp = false;
+
+        while (!endApp)
+        {
+            var result = 0.0;
+            var usageCount = _calculator.GetUsageCount();
+            var cleanNum1 = 0.0;
+            
+            if (usePreviousResult)
+            {
+                cleanNum1 = _calculator.GetNumberFromData();
+            }
+            else
+            {
+                Console.Write("Type a number, and then press Enter: ");
+                cleanNum1 = GetNumberFromConsole();
+            }
+            
+            Console.Write("Type another number, and then press Enter: ");
+            var cleanNum2 = GetNumberFromConsole();
+
+            Console.WriteLine($"\nYou used calculator {usageCount} times");
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            var operation = Console.ReadLine();
+            
+            if (operation == null || !Regex.IsMatch(operation, "[a|s|m|d]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            { 
+                try
+                {
+                    result = _calculator.DoOperation(cleanNum1, cleanNum2, operation);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+            
+            Console.Write("Press 'n' and Enter to go to the menu, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+            
+            Console.Clear();
+        }
+        _calculator.Finish();
+    }
+
+    private static double GetNumberFromConsole()
+    {
+        var numInput = Console.ReadLine();
+
+        var cleanNum = 0.0;
+        while (!double.TryParse(numInput, out cleanNum))
+        {
+            Console.Write("This is not valid input. Please enter an integer value: ");
+            numInput = Console.ReadLine();
+        }
+
+        return cleanNum;
+    }
+}
---
+++ Froliks.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,152 @@
+﻿using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace CalculatorLibrary;
+
+public class CalculatorData
+{
+    public int? Count { get; set; }
+    public List<OperationItem>? Operations { get; set; }
+}
+
+public class OperationItem
+{
+    public double Operand1 { get; set; }
+    public double Operand2 { get; set; }
+    public string Operation { get; set; }
+    public double Result { get; set; }
+}
+
+public class Calculator
+{
+    private CalculatorData? _data;
+    private List<OperationItem> _operations = new List<OperationItem>(); 
+    private int _usedCount = 0;
+    
+    public Calculator()
+    {
+        if (File.Exists("logger.json"))
+        {
+            var readFile = File.ReadAllText("logger.json");
+            _data = JsonConvert.DeserializeObject<CalculatorData>(readFile);
+            SetOperations();
+        }
+        
+        var logFile = File.CreateText("logger.json");
+        logFile.AutoFlush = true;
+    }
+    
+    public double DoOperation(double num1, double num2, string op)
+    {
+        var result = double.NaN;
+        var operationItem = new OperationItem { Operand1 = num1, Operand2 = num2 };
+        
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                operationItem.Operation = "Add";
+                break;
+            case "s":
+                result = num1 - num2;
+                operationItem.Operation = "Subtract";
+                break;
+            case "m":
+                result = num1 * num2;
+                operationItem.Operation = "Multiply";
+                break;
+            case "d":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    operationItem.Operation = "Divide";
+                }
+                break;
+            default:
+                break;
+        }
+
+        operationItem.Result = result;
+        _operations.Add(operationItem);
+
+        _usedCount++;
+        
+        return result;
+    }
+
+    public double GetNumberFromData()
+    {
+        ShowPreviousResults();
+        Console.Write("Choose what result you want to reuse: ");
+
+        int? index = null;
+        while (index == null)
+        {
+            var tmp = 0;
+            var isSuccesfulParse = int.TryParse(Console.ReadLine(), out tmp);
+
+            if (isSuccesfulParse && tmp < _operations.Count && tmp >= 0)
+            {
+                index = tmp;
+            }
+            else
+            {
+                Console.Write("Write an index which is presented above");
+            }
+        }
+
+        return _operations.ElementAt(index.Value).Result;
+    }
+
+    public void Finish()
+    {
+        var summaryCount = _usedCount + (_data?.Count ?? 0);
+        var outputObject = new
+        {
+            Count = summaryCount,
+            Operations = _operations
+        };
+
+        var jsonString = JsonSerializer.Serialize(outputObject);
+        
+        File.WriteAllText("logger.json", jsonString);
+    }
+
+    public int GetUsageCount()
+    {
+        return _usedCount + (_data?.Count ?? 0);
+    }
+    
+    public int GetOperationsCount()
+    {
+        return _operations.Count;
+    }
+
+    public void DeleteData()
+    {
+        _operations.Clear();
+        Finish();
+        Console.Clear();
+        Console.WriteLine("Calculations deleted");
+    }
+
+    public void ShowPreviousResults()
+    {
+        var index = 0;
+        
+        Console.WriteLine("Previous Results:");
+        _operations.ForEach(operation =>
+        {
+            Console.WriteLine($"{index} - {operation.Result}");
+            index++;
+        });
+    }
+
+    private void SetOperations()
+    {
+        if (_data != null && _data.Operations != null)
+        {
+            _operations.AddRange(_data.Operations);
+        }
+    }
+}
