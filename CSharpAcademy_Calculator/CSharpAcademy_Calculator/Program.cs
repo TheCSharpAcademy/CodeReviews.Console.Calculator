108a87af86d3cc9a9ffc7519743f1880b49fc88c
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-24 15:27:00 +0000

    Deleted original program to refactor using classes

--- CSharpAcademy_Calculator/CSharpAcademy_Calculator/Program.cs
+++ CSharpAcademy_Calculator/CSharpAcademy_Calculator/Program.cs
@@ -1,8 +1,32 @@
-﻿// Declare variables and then initialize to zero.
-int num1 = 0; int num2=0;
+﻿class Calculator
+{
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
 
-// Display title as the C# console calculator app.
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-// Ask the user 
+        // Use a switch statement to do the math.
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                break;
+        }
+        return result;
+    }
+}
