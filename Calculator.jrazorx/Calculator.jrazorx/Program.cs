939991f35bd632d69312f92c2db49691c26b9056
jra <julien.gault.50@gmail.com>
2024-04-03 16:23:18 +0200

    Fix the "divide by zero" error

--- Calculator.jrazorx/Calculator.jrazorx/Program.cs
+++ Calculator.jrazorx/Calculator.jrazorx/Program.cs
@@ -34,6 +34,12 @@ switch (Console.ReadLine())
         Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
         break;
     case "d":
+        // Ask the user to enter a non-zero divisor until they do so.
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
         break;
 }
