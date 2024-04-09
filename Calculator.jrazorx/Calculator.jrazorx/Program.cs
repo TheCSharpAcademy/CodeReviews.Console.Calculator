c06feb8f712edc79d8ecde611ed39c7188271aa8
jra <julien.gault.50@gmail.com>
2024-04-09 16:46:26 +0200

    changed logging to json file with Newtonsoft.Json NuGet package

--- Calculator.jrazorx/Calculator.jrazorx/Program.cs
+++ Calculator.jrazorx/Calculator.jrazorx/Program.cs
@@ -82,7 +82,9 @@ namespace CalculatorProgram
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+            // Add call to close the JSON writer before return
+            calculator.Finish();
             return;
         }
-    } 
+    }
 }
