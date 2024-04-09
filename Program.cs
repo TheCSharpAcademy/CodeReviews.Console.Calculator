a2afda9a51bdfb1c034e70b5b4c0560e80acb545
zen-pb <111935236+zen-pb@users.noreply.github.com>
2024-04-09 15:59:04 +0800

    Create initial version of the program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num = 0;
+char operation = ' ';
+string expression;
+
+bool firstTermFound = false;
+
+string? readInput;
+
+Console.WriteLine("Calculator");
+Console.WriteLine("Enter an expression(ex. 1 + 1):");
+
+readInput = Console.ReadLine();
+
+if (readInput != null)
+{
+    expression = readInput;
+
+    string[] expressionArray = expression.Split(' ');
+    int expressionLength = expressionArray.Length;
+    expression = "";
+
+    for (int i = 0; i < expressionLength; i++)
+    {
+        char[] terms = expressionArray[i].ToCharArray();
+
+        foreach(char term in terms)
+        {
+            if (term == 'x' || term == '/' || term == '+' || term == '-')
+            {
+                operation = term;
+                firstTermFound = true;
+                num = int.Parse(expression);
+                expression = "";
+            }
+            else
+            {
+                if (firstTermFound == false)
+                {
+                    expression += term;
+                }
+                else
+                {
+                    expression += term;
+                }   
+            }
+        }
+    }
+
+    switch (operation)
+    {
+        case 'x':
+            num *= int.Parse(expression);
+            break;
+
+        case '/':
+            num /= int.Parse(expression);
+            break;
+
+        case '+':
+            num += int.Parse(expression);
+            break;
+
+        case '-':
+            num -= int.Parse(expression);
+            break;
+    }
+
+    Console.WriteLine(num);
+}
