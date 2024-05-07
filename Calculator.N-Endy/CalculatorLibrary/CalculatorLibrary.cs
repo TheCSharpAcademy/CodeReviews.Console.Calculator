6409f73131c6b3022c36f9556e85893d038ce529
nelson@wiretooth.com <nelson@wiretooth.com>
2024-05-07 14:52:58 +0100

    Add DoOperation method

---
+++ Calculator.N-Endy/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,33 @@
+﻿namespace CalculatorLibrary;
+
+public class Calculator
+{
+    public double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return result;
+    }
+}
