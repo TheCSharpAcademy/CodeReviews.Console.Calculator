dc64cfb324133b3a5f3c607788851002cd1bbb40
nelson@wiretooth.com <nelson@wiretooth.com>
2024-05-08 11:54:06 +0100

    Correct Json writer code

--- Calculator.N-Endy/CalculatorLibrary/CalculatorLibrary.cs
+++ Calculator.N-Endy/CalculatorLibrary/CalculatorLibrary.cs
@@ -15,10 +15,12 @@ namespace CalculatorLibrary
             // Trace.WriteLine("Starting Calculator Log");
             // Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
 
-            StreamWriter logFile = File.CreateText("calculator.json");
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
             logFile.AutoFlush = true;
-            writer = new JsonTextWriter(logFile);
-            writer.Formatting = Formatting.Indented;
+            writer = new JsonTextWriter(logFile)
+            {
+                Formatting = Formatting.Indented
+            };
             writer.WriteStartObject();
             writer.WritePropertyName("Operations");
             writer.WriteStartArray();
@@ -27,6 +29,12 @@ namespace CalculatorLibrary
         public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Num1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Num2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
 
             switch (op)
             {
@@ -59,7 +67,7 @@ namespace CalculatorLibrary
             }
             writer.WritePropertyName("Result");
             writer.WriteValue(result);
-            writer.WriteEndArray();
+            writer.WriteEndObject();
 
             return result;
         }
