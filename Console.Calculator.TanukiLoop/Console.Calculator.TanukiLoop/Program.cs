bebccf656f228b3fd07ca3df39eabe2686b2b827
TanukiLoop <TanukiLoop@proton.me>
2024-05-23 10:15:40 -0500

    Explore integer math

--- Console.Calculator.TanukiLoop/Console.Calculator.TanukiLoop/Program.cs
+++ Console.Calculator.TanukiLoop/Console.Calculator.TanukiLoop/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿int a = 42;
+int b = 119;
+int c = a + b;
+Console.WriteLine(c);
+Console.ReadKey();
