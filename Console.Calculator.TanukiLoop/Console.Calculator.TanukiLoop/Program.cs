2380366f66d4187b0fae5602e2f92248947aa2d1
TanukiLoop <TanukiLoop@proton.me>
2024-05-23 10:40:01 -0500

    Add code to create a calculator

--- Console.Calculator.TanukiLoop/Console.Calculator.TanukiLoop/Program.cs
+++ Console.Calculator.TanukiLoop/Console.Calculator.TanukiLoop/Program.cs
@@ -1,5 +1,43 @@
-﻿int a = 42;
-int b = 119;
-int c = a + b;
-Console.WriteLine(c);
-Console.ReadKey();
+﻿// Declare variables and then initialize to zero.
+int num1 = 0; int num2 = 0;
+
+// Display title as the C# console calculator app.
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+// Ask the user to type the first number.
+Console.WriteLine("Type a number, and then press Enter");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to type the second number.
+Console.WriteLine("Type another number, and then press Enter");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+// Ask the user to choose an option.
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+
+// Use a switch statement to do the math.
+switch (Console.ReadLine())
+{
+    case "a" :
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+
+// Wait for the user to respond before closing.
+
+Console.WriteLine("Press any key to close the Calculator console app...");
