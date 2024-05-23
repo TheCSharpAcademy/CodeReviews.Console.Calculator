f799b15432d16d70b9b1c6700e9430b983beb42a
TanukiLoop <TanukiLoop@proton.me>
2024-05-23 13:29:12 -0500

    Finish "Tutorial: Extend C# console app and debug in Visual Studio (part 2 of 2)"

--- Console.Calculator.TanukiLoop/CalculatorLibrary/CalculatorLibrary.cs
+++ Console.Calculator.TanukiLoop/CalculatorLibrary/CalculatorLibrary.cs
@@ -4,51 +4,51 @@ using System.Diagnostics;
 namespace CalculatorLibrary;
 public class Calculator
 {
-    private JsonWriter writer;
+    private JsonWriter _writer;
 
     public Calculator()
     {
         StreamWriter logFile = File.CreateText("calculator.log");
         logFile.AutoFlush = true;
-        writer = new JsonTextWriter(logFile);
-        writer.Formatting = Formatting.Indented;
-        writer.WriteStartObject();
-        writer.WritePropertyName("Operations");
-        writer.WriteStartArray();
+        _writer = new JsonTextWriter(logFile);
+        _writer.Formatting = Formatting.Indented;
+        _writer.WriteStartObject();
+        _writer.WritePropertyName("Operations");
+        _writer.WriteStartArray();
     }
 
     public double DoOperation(double num1, double num2, string op)
     {
         double result = double.NaN; // Default is NAN if an operation, such as division could result in an error.
-        writer.WriteStartObject();
-        writer.WritePropertyName("Operand1");
-        writer.WriteValue(num1);
-        writer.WritePropertyName("Operand2");
-        writer.WriteValue(num2);
-        writer.WritePropertyName("Operation");
+        _writer.WriteStartObject();
+        _writer.WritePropertyName("Operand1");
+        _writer.WriteValue(num1);
+        _writer.WritePropertyName("Operand2");
+        _writer.WriteValue(num2);
+        _writer.WritePropertyName("Operation");
         
-        // Use a switch statemewnt to do the math.
+        // Use a switch statement to do the math.
         switch (op)
         {
             case "a":
                 result = num1 + num2;
-                writer.WriteValue("Add");
+                _writer.WriteValue("Add");
                 break;
             case "s":
                 result = num1 - num2;
-                writer.WriteValue("Subtract");
+                _writer.WriteValue("Subtract");
                 break;
             case "m":
                 result = num1 * num2;
-                writer.WriteValue("Multiply");
+                _writer.WriteValue("Multiply");
                 break;
             case "d":
-                // ASk the user to enter a non-zero divisor
+                // Ask the user to enter a non-zero divisor
 
                 if (num2 != 0)
                 {
                     result = num1 / num2;
-                writer.WriteValue("Divide");
+                _writer.WriteValue("Divide");
                 }
                 break;
             default:
@@ -56,17 +56,17 @@ public class Calculator
             
         }
 
-        writer.WritePropertyName("Result");
-        writer.WriteValue(result);
-        writer.WriteEndObject();
+        _writer.WritePropertyName("Result");
+        _writer.WriteValue(result);
+        _writer.WriteEndObject();
         return result;
     }
 
     public void Finish()
     {
-        writer.WriteEndArray();
-        writer.WriteEndObject();
-        writer.Close();
+        _writer.WriteEndArray();
+        _writer.WriteEndObject();
+        _writer.Close();
     }
 
 }
