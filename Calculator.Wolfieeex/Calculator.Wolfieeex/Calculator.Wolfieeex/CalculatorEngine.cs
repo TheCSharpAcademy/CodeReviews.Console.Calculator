7aac74e9313c7403f5b757ab370e79a1d627d887
Wolfieeex <Wolfieeex400@users.noreply.github.com>
2024-06-22 16:28:00 +0100

    The 1st part completion

---
+++ Calculator.Wolfieeex/Calculator.Wolfieeex/Calculator.Wolfieeex/CalculatorEngine.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculator.Wolfieeex;
+
+class CalculatorEngine
+{
+    public static string GetMathematicalSign(string operation)
+    {
+        switch (operation.ToLower())
+        {
+            case "a":
+                return "+";
+            case "s":
+                return "-";
+            case "m":
+                return "*";
+            case "d":
+                return "/";
+        }
+        return "";
+    }
+    internal static double CalculateResult(ref double number1, ref double number2, string operation)
+    {
+        switch (operation.ToLower())
+        {
+            case "a":
+                return number1 + number2;
+            case "s":
+                return number1 - number2;
+            case "m":
+                return number1 * number2;
+            case "d":
+                if (number2 == 0)
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.Write($"\r{new string(' ', Console.BufferWidth)}");
+                    Console.Write($"\rYour divisor must not equal 0. Please try again- reinsert your second number and press ENTER: ");
+                    HelperMethods.ReadNumericInput(ref number2, "second", true);
+                }
+                return number1 / number2;
+        }
+        return double.NaN;
+    }
+}
--- Calculator.Wolfieeex/Calculator.Wolfieeex/Calculator.Wolfieeex/Program.cs
+++ Calculator.Wolfieeex/Calculator.Wolfieeex/Calculator.Wolfieeex/Program.cs
@@ -1,112 +1,65 @@
-﻿double firstNumber = 0;
-double secondNumber = 0;
+﻿using Calculator.Wolfieeex;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Welcome to Console Calculator in C#!\r");
-Console.WriteLine($"{new string('-', Console.BufferWidth)}\n");
-
-AskForNumber("first");
-ReadNumericInput(ref firstNumber, "first");
-
-AskForNumber("second");
-ReadNumericInput(ref secondNumber, "second");
-
-Console.WriteLine("\n\nChoose one of the options below: ");
-Console.WriteLine($"\tA - Add");
-Console.WriteLine($"\tS - Subtract");
-Console.WriteLine($"\tM - Multiply");
-Console.WriteLine($"\tD - Divide");
-Console.Write($"Please select your option: ");
-
-string? userOption = null;
-ReadMatchingInput(ref userOption, new string[] { "a", "s", "m", "d" });
-
-switch (userOption.ToLower())
+class Program
 {
-    case "a":
-        Console.WriteLine($"Your result: {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-        break;
-    case "s":
-        Console.WriteLine($"Your result: {firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
-        break;
-    case "m":
-        Console.WriteLine($"Your result: {firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
-        break;
-    case "d":
-        if (secondNumber == 0)
-        {
-            ReadNumericInput(ref secondNumber, "second", true);
-        }
-        Console.WriteLine($"Your result: {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
-        break;
-}
-
-Console.Write("\nThank you for using this application! Press any key to exit: ");
-Console.ReadKey();
-
-void ReadNumericInput(ref double number, string ordinalString, bool divisor = false)
-{
-    bool checkNumber = true;
-    while (checkNumber)
+    static void Main(string[] args)
     {
-        string? userInput = Console.ReadLine();
-        ordinalString = ordinalString == "" ? ordinalString : ordinalString + " ";
+        Console.WriteLine("Welcome to Console Calculator in C#!\r");
+        Console.WriteLine($"{new string('-', Console.BufferWidth)}\n");
 
-        if (string.IsNullOrEmpty(userInput))
+        bool programRun = true;
+        while (programRun)
         {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write($"\r{new string(' ', Console.BufferWidth)}");
-            Console.Write($"\rYour input was empty. Please try again- insert your {ordinalString}number and press ENTER: ");
-            continue;
-        }
-        if (!double.TryParse(userInput, out number))
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write($"\r{new string(' ', Console.BufferWidth)}");
-            Console.Write($"\rYour input must not include letters or symbols. Please try again- insert your {ordinalString}number and press ENTER: ");
-            continue;
-        }
-        if (divisor && number == 0)
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write($"\r{new string(' ', Console.BufferWidth)}");
-            Console.Write($"\rYour divisor must not equal 0. Please try again- reinsert your {ordinalString}number and press ENTER: ");
-            continue;
-        }
-        checkNumber = false;
-    }
-}
-void ReadMatchingInput(ref string? letter, string[] options)
-{
-    bool checkNumber = true;
-    while (checkNumber)
-    {
-        string? userInput = Console.ReadLine();
+            double firstNumber = 0;
+            double secondNumber = 0;
 
-        if (string.IsNullOrEmpty(userInput))
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write($"\r{new string(' ', Console.BufferWidth)}");
-            Console.Write($"\rYour input was empty. Please try to select your option again and press ENTER: ");
-            continue;
-        }
-        foreach (string option in options)
-        {
-            if (option == userInput.ToLower())
+            HelperMethods.AskForNumber("first");
+            HelperMethods.ReadNumericInput(ref firstNumber, "first");
+
+            HelperMethods.AskForNumber("second");
+            HelperMethods.ReadNumericInput(ref secondNumber, "second");
+            Console.WriteLine($"{new string('-', Console.BufferWidth)}");
+
+            Console.WriteLine("\nChoose one of the options below: ");
+            Console.WriteLine($"\tA - Add");
+            Console.WriteLine($"\tS - Subtract");
+            Console.WriteLine($"\tM - Multiply");
+            Console.WriteLine($"\tD - Divide\n");
+            Console.WriteLine($"{new string('-', Console.BufferWidth)}\n");
+            Console.Write($"Please select your option: ");
+
+            string? userOption = null;
+            HelperMethods.ReadMatchingInput(ref userOption, @"^[asmd]$");
+            try
             {
-                checkNumber = false;
-                letter = userInput;
-                break;
+                double result = CalculatorEngine.CalculateResult(ref firstNumber, ref secondNumber, userOption);
+
+                if (double.IsNaN(result))
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                else
+                    Console.WriteLine("Your result: {0:0.##} {1} {2:0.##} = {3:0.##}\n", firstNumber, CalculatorEngine.GetMathematicalSign(userOption), secondNumber, result);
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\nOh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+
+            Console.WriteLine($"{new string('-', Console.BufferWidth)}");
+            Console.Write("Press 'E' and Enter to close the app, or press any other key and Enter to continue: ");
+
+            userOption = Console.ReadLine();
+            if (userOption != null)
+            {
+                if (userOption.ToLower() == "e")
+                {
+                    programRun = false;
+                }
+            }
+            Console.Clear();
         }
-        if (checkNumber)
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1);
-            Console.Write($"\r{new string(' ', Console.BufferWidth)}");
-            Console.Write($"\rYour selection doesn't match any of the options above. Please try to select your option again and press ENTER: ");
-        }
+
+        Console.Write("Thank you for using this application! Press any key to exit: ");
+        Console.ReadKey();
     }
-}
-void AskForNumber(string ordinalString)
-{
-    Console.Write($"Please type in your {ordinalString} number, and then press ENTER: ");
 }
