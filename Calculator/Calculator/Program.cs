e5ccb613ec44ae92d8f89c68ef32ce5933aeeab3
SWToh <toh.shengwei@gmail.com>
2024-06-26 22:17:58 +0800

    Console.Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorLibrary;
+
+namespace Calculator;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool stop = false;
+
+        while (!stop)
+        {
+            Menu.DisplayWelcomeMessage();
+            Utility.ReadUserNumberInput();
+            Menu.DisplayOptionMenu();            
+            stop = Menu.DisplayAgainOption();
+            Menu.DisplayUseCount();
+        }
+        //BrainCount.SaveBrainCount(Brain.count);
+    }
+}
