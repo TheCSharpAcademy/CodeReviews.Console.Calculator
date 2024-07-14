b906ec70ee35745362502b619f3c91e1770d0196
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2024-07-14 18:42:34 -0400

    Create main program files application: program.cs and calculatorlibrary.cs

---
+++ Calculator.alexgit55/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.alexgit55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
