db3b42119bfaea68a236bd0b68917c88142ac467
KroksasC <gustascemerka@gmail.com>
2024-07-31 10:05:05 +0300

    First version of calculators

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿using CalculatorLibrary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.GetMenu();
+    }
+}
+
---
+++ Calculator/CalculatorLibrary/Helpers.cs
@@ -0,0 +1,37 @@
+﻿namespace CalculatorLibrary
+{
+    internal class Helpers
+    {
+        /// <summary>
+        /// Validates number and return double type number
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static double NumberValidation(string? num)
+        {
+            double validatedNumber = 0;
+            while (!double.TryParse(num, out validatedNumber))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                num = Console.ReadLine();
+            }
+            return validatedNumber;
+        }
+        /// <summary>
+        /// Validates number and returns int type number
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int NumberValidation1(string? num)
+        {
+            int parsedNumber;
+            while (!int.TryParse(num, out parsedNumber))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                num = Console.ReadLine();
+            }
+
+            return parsedNumber;
+        }
+    }
+}
