d690e54df092a6ddb37852534c3bb08edeb3e722
Airiel Altemara <vsokord@gmail.com>
2024-08-06 12:59:23 -0700

    working calculator no error management

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double num1 = 0;
+        double num2 = 0;
+
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        Console.WriteLine("Type a number, and press Enter");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Type a number, and press Enter");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Choose an option from the following list:");
+        Console.WriteLine("\ta - Add");
+        Console.WriteLine("\ts - Subtract");
+        Console.WriteLine("\tm - Multiply");
+        Console.WriteLine("\td - Divide");
+        Console.WriteLine("Your option?");
+
+        switch (Console.ReadLine())
+        {
+            case "a":
+                Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                break;
+            case "s":
+                Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                break;
+            case "m":
+                Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                break;
+            case "d":
+                Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                break;
+        }
+
+        Console.WriteLine("Press any key to close the Calculator console app...");
+        Console.ReadKey();
     }
 }
