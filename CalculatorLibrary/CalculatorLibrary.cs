f344bf8d97f123e182a469aba499c74174a8e17f
Jackua <kuangja@oregonstate.edu>
2024-08-08 15:02:24 -0400

    Moved CalculatorLibrary into this project.

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public float CalculateAnswer(string operation, float num1, float num2)
+        {
+            float result = float.NaN;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            switch (operation)
+            {
+                case "a":
+                    writer.WriteValue("Add");
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    writer.WriteValue("Subtract");
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    writer.WriteValue("Multiply");
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    writer.WriteValue("Divide");
+                    break;
+                default:
+                    writer.WriteValue("Operation not recognized");
+                    break;
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
