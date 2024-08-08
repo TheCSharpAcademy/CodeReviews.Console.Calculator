2767e6e347521d99dae0acd6295cb8cdbf1134f0
Jackua <kuangja@oregonstate.edu>
2024-08-08 15:02:44 -0400

    Created Model

---
+++ ConsoleCalculatorApp/Model/MathProblem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator_App.ConsoleCalculatorApp.Model
+{
+    internal class MathProblem
+    {
+        internal float Answer { get; set; } = float.NaN;
+        internal float Num1 { get; set; }
+        internal float Num2 { get; set; }
+        internal string Operation { get; set; } = string.Empty;
+        internal string Input1 { get; set; } = string.Empty;
+        internal string Input2 { get; set; } = string.Empty;
+    }
+}
