91813f2b023f848b065ebd632a5aeabc7197cdb0
Thomas Henley <thomas.e.henley@gmail.com>
2024-08-13 14:39:02 -0400

    tutorial complete

--- Calculator.thomas-henley/Calculator/Program.cs
+++ Calculator.thomas-henley/Calculator/Program.cs
@@ -1,37 +1,7 @@
 ﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
 
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
+namespace CalculatorProgram;
 
 class Program
 {
@@ -41,6 +11,8 @@ class Program
         // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
+
+        Calculator calculator = new();
 
         while (!endApp)
         {
@@ -91,7 +63,7 @@ class Program
             {
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -111,6 +83,7 @@ class Program
 
             Console.WriteLine("\n"); // Friendly linespacing.
         }
+        calculator.Finish();
         return;
     }
 }
