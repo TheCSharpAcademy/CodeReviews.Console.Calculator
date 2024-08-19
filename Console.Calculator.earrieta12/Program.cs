7a62bac64c37a4edd622e07770c836e0ce5eee52
Edwin Arrieta <earrieta12@hotmail.com>
2024-08-18 21:42:20 -0400

    Console Calculator
    Console Calculator CSharpAcademy for review

---
+++ Console.Calculator.earrieta12/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyFirstProgram;
+
+var menu = new Menu();
+var date = DateTime.UtcNow;
+string name = Helper.GetName();
+List<int> difficulty = Helper.gameDifficulty();
+menu.ShowMenu(name, date, difficulty);
+
+
+
+
+
+
+
+
+
+
+
+
+
