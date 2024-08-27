9cc5726ae9ae97f0c3fea9a0d5b43a359cd644fd
walidhakimi62 <walidhakimi62@yahoo.com>
2024-08-27 11:19:30 +0300

    Finished calculator console app

---
+++ CalculatorApp/CalculatorApp/Program.cs
@@ -0,0 +1,182 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+using System.Globalization;
+public class CalculatorApp
+{
+    
+    static void Main(string[] args)
+    {   
+        Calculator calculator = new Calculator();
+
+        string? usInput;
+
+        do
+        {
+            int calCount = 0;
+            bool endApp = false;
+
+            Console.WriteLine("Welcome to calculator console App");
+            Console.WriteLine("___________________________________");
+            Console.Clear();
+            Console.WriteLine("_________\tMenu:_________");
+            Console.WriteLine("\tC => Calculation");
+            Console.WriteLine("\tH => Calculation History");
+            Console.WriteLine("\tD => Delete calculation record");
+            Console.WriteLine("\tQ => Quite calculation app");
+
+            usInput = Console.ReadLine().ToLower().Trim();
+            while (usInput == null || !Regex.IsMatch(usInput, "[c|h|d|q]"))
+            {
+                Console.WriteLine("Invalid option chosen. Please choose correct option: ");
+                usInput = Console.ReadLine().ToLower().Trim();
+            }
+
+            switch (usInput)
+            {
+                case "c":
+                    bool PreviousResult = false;
+                    while (!endApp)
+                    {
+                        string? firstInput;
+                        string? secondInput;
+
+                        Console.WriteLine("Calculation selected");
+                        Console.WriteLine("________________________");
+                        Console.Clear();
+                        double result = 0;
+
+                        double cleanNum = 0;
+
+                        if (!PreviousResult)
+                        {
+                            Console.WriteLine("Type a number and press Enter: ");
+                            firstInput = Console.ReadLine().Trim();
+
+
+                            while (!double.TryParse(firstInput, NumberStyles.Number, CultureInfo.InvariantCulture, out cleanNum))
+                            {
+                                Console.WriteLine("Invalid input value, please type a numeric value: ");
+                                firstInput = Console.ReadLine().Trim();
+                            }
+                        }
+                        else
+                        {
+                            cleanNum = calculator.ResultsList.Last().Answer;
+                            Console.WriteLine($"Previous Result: {cleanNum}");
+                        }
+
+                        Console.WriteLine("Please a choose an operation");
+                        Console.WriteLine("\t+ => Add");
+                        Console.WriteLine("\t- => Subtract");
+                        Console.WriteLine("\t* => Multiply");
+                        Console.WriteLine("\t/ => Divide");
+                        Console.WriteLine("\t^ => Power");
+                        Console.WriteLine("\tr => Square root");
+                        Console.WriteLine("\tcos => Cosine");
+                        Console.WriteLine("\tsin => Sine");
+                        Console.WriteLine("\ttan => Tangant");
+                        Console.WriteLine("Your operation: ");
+                        string? oper = Console.ReadLine().Trim().ToLower();
+
+                        while (oper == null || !Regex.IsMatch(oper, @"^[\+|\-|\*|/|\^|r|c|s|t]"))
+                        {
+                            Console.WriteLine("Error:Unrecognized operation");
+                            oper = Console.ReadLine().ToLower().Trim();
+                        }
+
+                        double cleanNum2 = 0;
+
+                        if (Regex.IsMatch(oper, @"[\+|\-|\*|/|\^|]"))
+                        {
+                            Console.WriteLine("Type another number and press Enter: ");
+                            secondInput = Console.ReadLine().Trim();
+
+
+                            while (!double.TryParse(secondInput, NumberStyles.Number, CultureInfo.InvariantCulture, out cleanNum2))
+                            {
+                                Console.WriteLine("Invalid input value, please type a numeric value: ");
+                                secondInput = Console.ReadLine().Trim();
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                result = calculator.OperationToDo(cleanNum, cleanNum2, oper);
+                                calCount++;
+
+                                if (double.IsNaN(result))
+                                {
+                                    Console.WriteLine("This operation will result a mathematical error.\n");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Your result: {result:0.##}\n");
+
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Oh no!!! An exception occured trying to do math.\nDetails:", e.Message);
+                            }
+                        }
+
+                        Console.WriteLine("_______________________________");
+                        Console.WriteLine($"Calculator was used{calCount} times");
+                        Console.WriteLine("Press 'x' to discard previous result and start new operation.");
+                        Console.WriteLine("Press 'n' and to go back to main menu, or press any other key press Enter to continue");
+
+                        string? endOption = Console.ReadLine().ToLower().Trim();
+
+                        if (endOption == "n")
+                        {
+                            endApp = true;
+                        }
+                        else if (endOption != "x")
+                        {
+                            PreviousResult = true;
+                        }
+                        else
+                        {
+                            PreviousResult = false;
+                        }
+                        Console.WriteLine("\n");
+                    }
+                    calculator.Finish();
+                    break;
+
+                case "h":
+
+                    Console.Clear();
+                    if (calculator.ResultsList.Count == 0)
+                    {
+                        Console.WriteLine("--Currently there is no calculation record");
+                    }
+                    else
+                    {
+                        Console.WriteLine("-- All Calculation history---");
+                        foreach (var result in calculator.ResultsList)
+                        {
+                            result.Display();
+                        }
+                    }
+                    Console.ReadLine();
+                    break;
+                case "d":
+                    Console.Clear();
+                    Console.WriteLine("Are you sure you want to delete all Calculation records(Y/N)");
+                    string? answer = Console.ReadLine().ToLower().Trim();
+
+                    if (answer == "y")
+                    {
+                        calculator.ResultsList.Clear();
+                        Console.Clear();
+                        Console.WriteLine("All the calculation records deleted! (Press Enter)");
+                        Console.ReadLine();
+                    }
+                    break;
+            }
+        } while (usInput != "q");
+    }
+}
+    
---
+++ CalculatorApp/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,171 @@
+﻿using System.Diagnostics;
+
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    
+    public class Calculator
+    {
+        JsonWriter writer;
+
+        public List<Result> ResultsList = new List<Result>();
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("Calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteStartArray();
+            //Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            //Trace.AutoFlush = true;
+            //Trace.WriteLine("Starting Calculator Log");
+            //Trace.WriteLine(string.Format("Started {0}"), System.DateTime.Now.ToString());
+        }
+        public double OperationToDo(double firstNumber, double secondNumber, string op)
+        {
+            double result = double.NaN;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(firstNumber);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(secondNumber);
+            writer.WritePropertyName("Operation");
+
+            switch(op)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = secondNumber,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Add");
+                    //Trace.WriteLine(string.Format("{0} + {1} = {2}", firstNumber, secondNumber, result));
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = secondNumber,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Subtract");
+                    //Trace.WriteLine(string.Format("{0} - {1} = {2}", firstNumber, secondNumber, result));
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = secondNumber,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Multiply");
+                    //Trace.WriteLine(string.Format("{0} * {1} = {2}", firstNumber, secondNumber, result));
+                    break;
+                case "/":
+                    if(secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                        ResultsList.Add(new Result
+                        {
+                            Operand1 = firstNumber,
+                            Operand2 = secondNumber,
+                            Operation = op,
+                            Answer = result
+                        });
+                        writer.WriteValue("Divide");
+                        //Trace.WriteLine(string.Format("{0} / {1} = {2}", firstNumber, secondNumber, result));
+                    }
+                    
+                    break;
+                case "^":
+                    result = Math.Pow(firstNumber, secondNumber);
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = secondNumber,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Power");
+                    //Trace.WriteLine(string.Format("{0} + {1} = {2}", firstNumber, secondNumber, result));
+                    break;
+                case "r":
+                    result = Math.Sqrt(firstNumber);
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = null,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Square root");
+                    //Trace.WriteLine(string.Format("{0} = {2}", firstNumber, result));
+                    break;
+                case "c":
+                    result = Math.Cos(firstNumber);
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = null,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Cosine");
+                    //Trace.WriteLine(string.Format("{0} = {2}", firstNumber, result));
+                    break;
+                case "s":
+                    result = Math.Sin(firstNumber);
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = null,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Sine");
+                    //Trace.WriteLine(string.Format("{0} = {2}", firstNumber, result));
+                    break;
+                case "t":
+                    result = Math.Tan(firstNumber);
+                    ResultsList.Add(new Result
+                    {
+                        Operand1 = firstNumber,
+                        Operand2 = null,
+                        Operation = op,
+                        Answer = result
+                    });
+                    writer.WriteValue("Tangant");
+                    //Trace.WriteLine(string.Format("{0} = {2}", firstNumber, result));
+                    break;
+                default:
+                    break;
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
