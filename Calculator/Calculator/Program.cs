16dbcfc58f011f44b5e27e46363e5c7a3c85a2e0
Hayk Grigoryan <grig.aik@gmail.com>
2024-09-05 18:07:10 +0400

    Calculator according to C# academy task Extra tasks (functionality that will count the amount of times the calculator was used, Storing a list with the latest calculations,  extra calculations: Square Root, Taking the Power, 10x, Trigonometry functions) are also complete

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+                //Display title as the C# console calculator app and amount of usages
+                Console.Clear();
+                Console.WriteLine("Console Calculator in C#\r");
+                Console.WriteLine("------------------------\n");
+                Console.WriteLine($"Calculator used: {calculator.GetUsageTimes()} times\n");
+                calculator.ShowCalculationsList();
+
+                // Declare variables and set to empty.
+                // Use Nullable types (with ?) to match type of System.Console.ReadLine
+                string? numInput1 = "";
+                string? numInput2 = "";
+                double result = 0;
+
+                // Ask the user to type the first number.
+                Console.Write("Type 1st number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Ask the user to type the second number.
+                Console.Write("Type 2nd number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\tr - Square Root (will be done on 1st number you've entered)");
+                Console.WriteLine("\tp - Taking Power (1st number will be powered in 2nd number)");
+                Console.WriteLine("\tx - 10^x (number 10 will be powered in 1st number)");
+                Console.WriteLine("\tc - cos of 1st number");
+                Console.WriteLine("\tt - sin of 1st number");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+
+                // Validate input is not null, and matches the pattern
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d|r|p|x|c|t]"))
+                {
+                    Console.WriteLine("Error: Unrecognized input.");
+                }
+                else
+                {
+                    try
+                    {
+                        result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+                }
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app\nPress 'c' Enter to clear calculation history list\nPress any other key and Enter to continue:\n");
+                var readResult = Console.ReadLine();
+                switch (readResult)
+                {
+                    case "c":
+                        calculator.ClearCalculationList(); 
+                        break;
+                    
+                    case "n": 
+                        endApp = true; 
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            // Add call to close the JSON writer before return
+            calculator.Finish();
+            return;
+        }
+    }
+}
