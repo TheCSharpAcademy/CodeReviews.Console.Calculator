ba3ff351401373c93ceb5203afe41977f3fab65b
kerolosnesim1 <112533022+kerolosnesim1@users.noreply.github.com>
2024-09-08 03:47:41 +0300

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,137 @@
+﻿using CalculatorLibrary;
+
+using System.Text.RegularExpressions;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        public static int usageCount = 0;
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+                Console.WriteLine("==================================");
+                Console.WriteLine("     Console Calculator in C#");
+                Console.WriteLine("==================================");
+                Console.WriteLine();
+                // Declare variables and set to empty.
+                // Use Nullable types (with ?) to match type of System.Console.ReadLine
+                string? numInput1 = "";
+                string? numInput2 = "";
+                double result = 0;
+
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine();
+                Console.WriteLine("  a  - Add                d  - Divide             tan - Tangent              w  - 10x");
+                Console.WriteLine("  s  - Subtract           p  - Power              sin - Sine                 u  - UsageCount");
+                Console.WriteLine("  m  - Multiply           r  - Square Root        cos - Cosine               l  - LatestRusult");
+                Console.WriteLine();
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine().ToLower();
+
+                
+                // Validate input is not null, and matches the pattern
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d|p|r|tan|cos|sin|w|u|l|c]"))
+                {
+                    Console.WriteLine("Error: Unrecognized input.");
+                    continue;
+                }
+
+                if (op == "u")
+                {
+                    Console.WriteLine($"Calculator usage count is {usageCount} times.");
+                }
+                
+                else if (op == "l" )
+                {
+                    calculator.DoOperation(0, 0, "l", ref usageCount);
+                }
+                else if (op == "c")
+                {
+                    calculator.DoOperation(0, 0, "c", ref usageCount);
+                }
+
+                else 
+                {// Ask the user to type the first number.
+                    Console.Write("Type a number, and then press Enter: ");
+                    numInput1 = Console.ReadLine();
+
+                    double cleanNum1 = 0;
+                    while (!double.TryParse(numInput1, out cleanNum1))
+                    {
+                        Console.Write("This is not valid input. Please enter an integer value: ");
+                        numInput1 = Console.ReadLine();
+                    }
+
+                    // Ask the user to type the second number.
+                    if (op != "r" && op != "w" && op != "sin" && op != "cos" && op != "tan")
+                    {
+                        Console.Write("Type another number, and then press Enter: ");
+
+                        numInput2 = Console.ReadLine();
+
+                        double cleanNum2 = 0;
+                        while (!double.TryParse(numInput2, out cleanNum2))
+                        {
+                            Console.Write("This is not valid input. Please enter an integer value: ");
+                            numInput2 = Console.ReadLine();
+                        }                   
+                        try
+                        {
+                            result = calculator.DoOperation(cleanNum1, cleanNum2 , op, ref usageCount);
+                            if (double.IsNaN(result) && op != "u" && op !="l")
+                            {
+                                Console.WriteLine("This operation will result in a mathematical error.\n");
+                            }
+
+                            else if (op != "u" && op != "l")
+                            {
+                                Console.WriteLine("Your result: {0:0.##}\n", result);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                        }
+                    }
+                    else
+                    {
+                        // For operations like 'r', 'w', 'sin', 'cos', 'tan'
+                        try
+                        {
+                            result = calculator.DoOperation(cleanNum1, 0, op, ref usageCount); // Passing 0 for unused operand
+                            if (double.IsNaN(result) && op != "u" && op != "l")
+                            {
+                                Console.WriteLine("This operation will result in a mathematical error.\n");
+                            }
+                            else if (op != "u" && op != "l")
+                            {
+                                Console.WriteLine("Your result: {0:0.##}\n", result);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                        }
+                    }
+                }
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true ;
+
+                Console.Clear();                
+            }
+            calculator.Finish();
+            return;
+        }
+    }
+}
+    
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,141 @@
+﻿using Newtonsoft.Json;
+
+namespace CalculatorLibrary;
+
+    public class Calculator
+    {
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public double DoOperation(double num1, double num2, string op , ref int usageCount)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    writer.WriteValue("Divide");
+                    break;
+                case "r":
+                if (num1 >= 0)
+                   {
+                       result = Math.Sqrt(num1);
+                   }
+                writer.WriteValue("Square Root");
+                    break;
+                case "p":
+                    result = Math.Pow(num1 , num2);
+                    writer.WriteValue("Taking the Power");
+                    break;
+                case "w":
+                    result = result = 10 * num1;
+                    writer.WriteValue("10x");
+                    break;
+                case "sin":
+                    result = Math.Sin(num1 * (Math.PI / 180.0)); 
+                    writer.WriteValue("Sine");
+                    break;
+                case "cos":
+                    result = Math.Cos(num1 * (Math.PI / 180.0));
+                    writer.WriteValue("Cosine");
+                    break;
+                case "tan":
+                    result = Math.Tan(num1 * (Math.PI / 180.0));
+                    writer.WriteValue("Tangent");
+                    break;
+                case "u":
+                    Console.WriteLine($"Calculator usage count is {usageCount} times.");
+                    writer.WriteValue("UsageCount");
+                    break;
+                 case "l":
+                     Get_Latest_Calculations();
+                     Console.WriteLine("\nTo Clear the history list type option 'C'");
+                     writer.WriteValue("last calc");
+                     break;
+                case "c":
+                    DeleteGameResult(); // this delete the whole list
+                    writer.WriteValue("DeletList");
+                    break;
+                default:     // Return text for an incorrect option entry.
+                    break;
+            }
+
+           if (op != "u" && op != "l" && op !="c")  
+            {
+                usageCount++;
+                AddGameResult(result.ToString());
+            }
+
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+            return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+    //  latest calculations functions.
+    private static List<string> results = new List<string>();
+    public static void AddGameResult(string result)
+        {
+           results.Add(result);
+        }
+    public static void DeleteGameResult()
+        {
+            results.Clear();
+            Console.WriteLine("Result list clear now.");
+        }
+
+    public static void Get_Latest_Calculations()
+    {
+        if (results.Count == 0 )
+        {
+            Console.WriteLine("There is no calculation history yet.");
+        }
+        else
+        {
+            foreach (var result in results)
+            {
+                Console.WriteLine($"your latest calculation result was {result}");
+            }
+        }
+    }
+}       
