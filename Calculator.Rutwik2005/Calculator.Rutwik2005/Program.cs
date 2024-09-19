ee4fbe1f6fcc95ffb758bf846f1dd62f22535b3e
Rutwik Vaidya <rutwik2005vaidya@gmail.com>
2024-09-19 08:02:25 +0530

    Final all changes done followed the tutorial

--- Calculator.Rutwik2005/Calculator.Rutwik2005/Program.cs
+++ Calculator.Rutwik2005/Calculator.Rutwik2005/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Text.RegularExpressions;
-
+using CalculatorLibrary;
 class Program
 {
     static void Main(string[] args)
@@ -8,7 +8,8 @@ class Program
         // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
-
+        // Program.cs
+        Calculator calculator = new Calculator();
         while (!endApp)
         {
             // Declare variables and set to empty.
@@ -58,7 +59,7 @@ class Program
             {
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -78,38 +79,7 @@ class Program
 
             Console.WriteLine("\n"); // Friendly linespacing.
         }
+        calculator.Finish();
         return;
-    }
-}
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
     }
 }
