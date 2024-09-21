9ff838bb6929c08e2979a3c12b0d55fc38be49a9
Piotr Kubas <piotrkubas39@gmail.com>
2024-09-21 11:33:20 +0200

    Part 1 of the tutrial done!

---
+++ PiotrrKubas.Calculator/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PiotrrKubas.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            Console.WriteLine("Consosle calculator in c#\r");
+            Console.WriteLine("------------------------\n");
+            while (!endApp)
+            {
+                string? numInput1 = "";
+                string? numInput2 = "";
+                double result = 0;
+
+                Console.WriteLine("Type a number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.WriteLine("This is not a valid input, please enter a numeric value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                Console.WriteLine("Type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.WriteLine("This is not a valid input, please enter a numeric value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+                {
+                    Console.WriteLine("Error: Unrecognized input.");
+                }
+                else
+                {
+                    try
+                    {
+                        result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Your result: {0:0.##}\n", result);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occured trying to do the math.\n - Details: " e.Message);
+                        throw;
+                    }
+                }
+                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("Press 'n' and Enter to close the app, or press any other key and Enter to Continue: ");
+                if (Console.ReadLine() == "n")
+                {
+                    endApp = true;
+                }
+                Console.WriteLine("\n");
+            }
+            return;
+        }
+    }
+
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 !=)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
