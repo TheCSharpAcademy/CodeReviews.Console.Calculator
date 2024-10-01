2a778d8d7b351105bf85d78f2024ac2f5c6f5dbb
DigitalJSon <pardinasjaysonb@gmail.com>
2024-10-01 09:23:10 +0800

    Added project to fork

---
+++ Calculator.DigitalJSon/Calculator.DigitalJSon/Program.cs
@@ -0,0 +1,203 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            string? input = "";
+            string choice = "";
+            bool previousCalculationsExist = File.Exists("calculation.json");
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Calculator calculator = new Calculator();
+            if (previousCalculationsExist)
+            {
+                Console.WriteLine("List of previous calculations found. Would you like to keep it? (y/n)");
+                do
+                {
+                    input = Console.ReadLine();
+                    if (input != null)
+                    {
+                        choice = input.ToLower().Trim();
+                    }
+                    switch (choice)
+                    {
+                        case "y":
+                            Console.WriteLine("Using existing list...");
+                            calculator.LoadCalculationJson();
+                            break;
+                        case "n":
+                            Console.WriteLine("Deleting list...");
+                            calculator.DeleteJson();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid input.");
+                            break;
+                    }
+                } while (choice != "y" && choice != "n");
+
+
+
+
+            }
+            
+            while (!endApp)
+            {
+                double cleanNum1 = 0;
+                double cleanNum2 = 0;
+                previousCalculationsExist = File.Exists("calculation.json");
+               
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\te - Taking the power");
+                Console.WriteLine("\tsqr - Square Root");
+                Console.WriteLine("\t10x - 10 raised to x");
+                Console.WriteLine("\tsin - Sine function");
+                Console.WriteLine("\tcos - Cosine function");
+                Console.WriteLine("\ttan - Tangent function");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+
+                // Validate input is not null, and matches the pattern
+                while (op == null || !Regex.IsMatch(op, "^(a|s|m|d|sqr|e|10x|sin|cos|tan)$"))
+                {
+                    Console.WriteLine("Error: Unrecognized input. Please try again.");
+                    op = Console.ReadLine();
+                }
+                try
+                {
+                    // Declare variable and set to empty.
+                    // Use Nullable types (with ?) to match type of System.Console.ReadLine
+                    string? numInput1 = "";
+                    string? numInput2 = "";
+                    double result = 0;
+                    double previosResult = 0;
+                    bool previousResultUsed = false;
+                    bool num1Used = false;
+                    bool num2Used = false;
+                    int chosenOperand = 0;
+
+                    if (previousCalculationsExist)
+                    {
+                        calculator.LoadCalculationJson();
+                        Console.WriteLine("Would you like to reuse a result from a previous calculation? Type y and enter if yes and any other key if no.");
+                        input = Console.ReadLine();
+                        if (input != null)
+                        {
+                            choice = input.ToLower().Trim();
+                        }
+                        if (choice == "y")
+                        {
+                            calculator.ViewPreviousCalculations();
+                            previosResult = calculator.UseResultAsOperand();
+                            previousResultUsed = true;
+                        }
+                    }
+                    if (op == "sqr" || op == "10x" || op == "sin" || op == "cos" || op == "tan")
+                    {
+                        if (!previousResultUsed)
+                        {
+                            Console.WriteLine("Type a number, then press Enter.");
+                            numInput1 = Console.ReadLine();
+                            while (!double.TryParse(numInput1, out cleanNum1))
+                            {
+                                Console.Write("This is not a valid input. Please enter a numeric value:");
+                                numInput1 = Console.ReadLine();
+                            }
+                        }
+                        else if (previousResultUsed)
+                        {
+                            cleanNum1 = previosResult;
+                        }
+                        result = calculator.DoOperation(cleanNum1, op, cleanNum2);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    else
+                    {
+                        if (previousResultUsed)
+                        {
+                            Console.WriteLine("Where would you like to use the previous result?");
+                            Console.WriteLine("Type 1 if Operand 1 or 2 if Operand 2 then press Enter.");
+                            input = Console.ReadLine();
+                            while (!int.TryParse(input, out chosenOperand) || (chosenOperand < 1 || chosenOperand > 2))
+                            {
+                                Console.WriteLine("Invalid input. Please enter either 1 if Operand 1 or 2 if Operand 2");
+                                input = Console.ReadLine();
+                            }
+                            if (chosenOperand == 1)
+                            {
+                                cleanNum1 = previosResult;
+                                num1Used = true;
+
+                            }
+                            else if (chosenOperand == 2)
+                            {
+                                cleanNum2 = previosResult;
+                                num2Used = true;
+                            }
+                        }
+                        // Ask the user to type the first number.
+                        if (!num1Used)
+                        {
+                            Console.WriteLine("Type a number, then press Enter (Operand 1)");
+                            numInput1 = Console.ReadLine();
+
+                            while (!double.TryParse(numInput1, out cleanNum1))
+                            {
+                                Console.Write("This is not a valid input. Please enter a numeric value:");
+                                numInput1 = Console.ReadLine();
+                            }
+                        }
+                        // Ask the user to type the second number.
+                        if (!num2Used)
+                        {
+                            Console.WriteLine("Type a number, then press Enter (Operand 2)");
+                            numInput2 = Console.ReadLine();
+
+                            while (!double.TryParse(numInput2, out cleanNum2))
+                            {
+                                Console.Write("This is not a valid input. Please enter a numeric value:");
+                                numInput2 = Console.ReadLine();
+                            }
+                        }
+                        result = calculator.DoOperation(cleanNum1, op, cleanNum2);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                }
+                    
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine("Oh no! An exception occured trying to do the math.\n - Details: " + e.Message);
+                }
+                Console.WriteLine($"Calculator used {calculator.calcCounter} times");
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                calculator.SaveCalculationToJSon();
+            }
+            return;
+        }
+    }
+}
---
+++ Calculator.DigitalJSon/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,252 @@
+﻿
+using Newtonsoft.Json;
+using System.Diagnostics.Metrics;
+
+
+namespace CalculatorLibrary
+{
+
+    public class CalculationLog
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public double Result { get; set; }
+        public string? Operation { get; set; }
+
+    }
+
+    public class CalculatorTracker
+    {
+        public int Counter = 0;
+    }
+
+    public class Calculator
+    {  
+        string jsonFileLoc = "calculation.json";
+        string counterFileLoc = "counter.json";
+        List<CalculationLog> calcLogList = new List<CalculationLog>();
+        List<CalculationLog> jsonList = new List<CalculationLog>();
+        CalculatorTracker counter = new CalculatorTracker();
+        public int calcCounter = 0;
+        bool loadCounterJsonAlready = false;
+        string? input = "";
+        public double DoOperation(double num1, string op, double num2 = double.NaN)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            // Use a switch statement to do the math.
+            string operationUsed = "";
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    operationUsed = "Addition";
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    operationUsed = "Subtraction";
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    operationUsed = "Multiplication";
+                    break;
+                case "d":
+                    //Ask the user to enter a non-zero divisor
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        operationUsed = "Division";
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Mathematical Error. Dividing by Zero");
+                    }
+                    break;
+                case "sqr":
+                    result = Math.Sqrt(num1);
+                    operationUsed = "Square Root";
+                    break;
+                case "e":
+                    result = Math.Pow(num1, num2);
+                    operationUsed = "Exponential";
+                    break;
+                case "10x":
+                    result = Math.Pow(10, num1);
+                    operationUsed = "10x";
+                    break;
+                case "sin":
+                    result = Math.Sin(num1);
+                    operationUsed = "Sine Function";
+                    break;
+                case "cos":
+                    result = Math.Cos(num1);
+                    operationUsed = "Cosine Function";
+                    break;
+                case "tan":
+                    result = Math.Tan(num1);
+                    operationUsed = "Tangent Function";
+                    break;
+                // Return text for an incorrect ption entry
+                default:
+                    break;
+            }
+            if (!File.Exists(jsonFileLoc))
+            {
+                if (operationUsed == "Square Root" || operationUsed == "10x" || operationUsed == "Cosine Function" || operationUsed == "Sine Function" || operationUsed == "Tangent Function")
+                {
+                    calcLogList.Add(new CalculationLog { Num1 = num1, Num2 = double.NaN, Operation = operationUsed, Result = result });
+                }
+                else
+                {
+                    calcLogList.Add(new CalculationLog { Num1 = num1, Num2 = num2, Operation = operationUsed, Result = result });
+                }
+            }
+            else
+            {
+                if (operationUsed == "Square Root" || operationUsed == "10x" || operationUsed == "Cosine Function" || operationUsed == "Sine Function" || operationUsed == "Tangent Function")
+                {
+                    jsonList.Add(new CalculationLog { Num1 = num1, Num2 = double.NaN, Operation = operationUsed, Result = result });
+                }
+                else
+                {
+                    jsonList.Add(new CalculationLog { Num1 = num1, Num2 = num2, Operation = operationUsed, Result = result });
+                }
+                
+            }
+            if (!File.Exists (counterFileLoc) || loadCounterJsonAlready)
+            {
+                counter.Counter++;
+            }
+            else if (File.Exists(counterFileLoc) && !loadCounterJsonAlready)
+            {
+                loadCounterJsonAlready = true;
+                string jsonCounter = File.ReadAllText(counterFileLoc);
+                int index = jsonCounter.IndexOf (": ");
+                string number = jsonCounter.Substring(index + 1).Trim().TrimEnd('\r', '\n', '}');
+                counter.Counter = Convert.ToInt32 (number);
+                counter.Counter++;
+            }
+            calcCounter = counter.Counter;
+            
+            return result;
+        }
+
+        public void SaveCalculationToJSon()
+        {
+            if (!File.Exists(jsonFileLoc))
+            {
+                string calcLogJson = JsonConvert.SerializeObject(calcLogList, Formatting.Indented);
+                File.WriteAllText(jsonFileLoc, calcLogJson);
+            }
+            else
+            {
+                string calcLogJson = JsonConvert.SerializeObject(jsonList, Formatting.Indented);
+                File.WriteAllText(jsonFileLoc, calcLogJson);
+            }
+            SaveCounterToJson();
+            
+        }
+
+        private void SaveCounterToJson()
+        {
+            string counterLogJson = JsonConvert.SerializeObject(counter, Formatting.Indented);
+            File.WriteAllText(counterFileLoc, counterLogJson);
+        }
+
+        public void LoadCalculationJson()
+        {
+            string jsonFile = File.ReadAllText(jsonFileLoc);
+            jsonList = JsonConvert.DeserializeObject<List<CalculationLog>>(jsonFile);
+        }
+        public void DeleteJson()
+        {
+            File.Delete(jsonFileLoc);
+            Console.WriteLine("File deleted successfully.");
+        }
+
+        public void ViewPreviousCalculations()
+        {
+            Console.WriteLine("Showing previous calculations...");
+            string operationUsed = "";
+
+            if (File.Exists(jsonFileLoc))
+            {
+                for (int i = 0; i < jsonList.Count; i++)
+                {
+                    switch (jsonList[i].Operation)
+                    {
+                        case "Addition":
+                            operationUsed = "+";
+                            break;
+                        case "Subtraction":
+                            operationUsed = "-"; 
+                            break;
+                        case "Multiplication":
+                            operationUsed = "*";
+                            break;
+                        case "Division":
+                            operationUsed = "/";
+                            break;
+                        case "Square Root":
+                            operationUsed = "√";
+                            break;
+                        case "Exponential":
+                            operationUsed = "^";
+                            break;
+                        case "10x":
+                            operationUsed = "10^x";
+                            break;
+                        case "Sine Function":
+                            operationUsed = "sin";
+                            break;
+                        case "Cosine Function":
+                            operationUsed = "cos";
+                            break;
+                        case "Tangent Function":
+                            operationUsed = "tan";
+                            break;
+                    }
+                    if (operationUsed == "√")
+                    {
+                        Console.WriteLine($"{i + 1}. {operationUsed}{jsonList[i].Num1} = {jsonList[i].Result}");
+                    }
+                    else if (operationUsed == "^")
+                    {
+                        Console.WriteLine($"{i + 1}. {jsonList[i].Num1}{operationUsed}{jsonList[i].Num2} = {jsonList[i].Result}");
+                    }
+                    else if (operationUsed == "10^x")
+                    {
+                        Console.WriteLine($"{i + 1}. 10^{jsonList[i].Num1} = {jsonList[i].Result}");
+                    }
+                    else if (operationUsed == "sin" || operationUsed == "cos" || operationUsed == "tan")
+                    {
+                        Console.WriteLine($"{i + 1}. {operationUsed}({jsonList[i].Num1}) = {jsonList[i].Result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i + 1}. {jsonList[i].Num1} {operationUsed} {jsonList[i].Num2} = {jsonList[i].Result}");
+                    }
+                    
+                }
+            }
+        }
+
+        public double UseResultAsOperand()
+        {
+            int chosenCalc = 0;
+
+            Console.WriteLine("Enter the number at the left of the calculation of the result you wish to use.");
+            input = Console.ReadLine();
+            while (!int.TryParse(input, out chosenCalc) || (chosenCalc > jsonList.Count || chosenCalc < 1))
+            {
+                Console.WriteLine("Invald input. Please type the number at the left of the calculation you wish to choose.");
+                input = Console.ReadLine();
+            }
+            return jsonList[chosenCalc - 1].Result;
+
+        }
+
+        
+    }
+}
+
+
