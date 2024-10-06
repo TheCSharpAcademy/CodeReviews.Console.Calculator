443976df10fe3dfdc0e4a6e60bbb6d81d75b20b7
Alice Hagström <alice@somnolentgames.com>
2024-10-06 23:48:41 +0200

    completed

---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+
+namespace CalculatorLibrary;
+
+public class Calculator
+{
+    private JsonWriter _writer;
+    public Calculator() {
+        StreamWriter logFile = File.CreateText("calculator.json");
+        logFile.AutoFlush = true;
+        _writer = new JsonTextWriter(logFile);
+        _writer.Formatting = Formatting.Indented;
+        _writer.WriteStartObject();
+        _writer.WritePropertyName("Operations");
+        _writer.WriteStartArray();
+
+    }
+    public double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+        _writer.WriteStartObject();
+        _writer.WritePropertyName("Operand1");
+        _writer.WriteValue(num1);
+        _writer.WritePropertyName("Operand2");
+        _writer.WriteValue(num2);
+        _writer.WritePropertyName("Operation");
+        
+        // Use a switch statement to do the math.
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                _writer.WriteValue("Add");
+                break;
+            case "s":
+                result = num1 - num2;
+                _writer.WriteValue("Subtract");
+                break;
+            case "m":
+                result = num1 * num2;
+                _writer.WriteValue("Multiply");
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                _writer.WriteValue("Divide");
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                break;
+        }
+
+        _writer.WritePropertyName("Result");
+        _writer.WriteValue(result);
+        _writer.WriteEndObject();
+        return result;
+    }
+
+    public void Finish() {
+        _writer.WriteEndArray();
+        _writer.WriteEndObject();
+        _writer.Close();
+    }
+}
