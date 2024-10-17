6598b0360c5c9e0a040cf8eb4bd600260d61f60c
Lux <Lux@GREYKLOUDS>
2024-10-17 16:13:30 -0500

    Project finished! I implemented the uses counter to show the user how many times they've used the app, and I made a list to store the history of the previous calculations

---
+++ Calculator [Grey Klouds]/Calculator/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+using System.Collections.Generic;
+using System.Reflection.Emit; // Importing the name space to create a List
+
+namespace CalculatorProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            int count = 0;
+           List<double> list = new List<double>();
+
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Calculator calculator = new Calculator();
+
+            while (!endApp)
+            {
+                // Declare variables and set to empty.
+                // Use Nullable types (with ?) to match type of System.Console.ReadLine
+                string? numInput1 = "";
+                string? numInput2 = "";
+                double result = 0;
+                double cleanNum1 = 0;
+                double cleanNum2 = 0;
+
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\th - History");
+                Console.WriteLine("\tn - Exit");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+                if (op.Trim() == "h")
+                {
+                    calculator.getHistory(list);
+                    continue;
+                }
+                if(op.Trim() == "n")
+                {
+                    endApp = true;
+                    return;
+                }
+                // Ask the user to type the first number.
+                Console.Write("Type a number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Ask the user to type the second number.
+                Console.Write("Type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                // Validate input is not null, and matches the pattern
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d|h|n]"))
+                {
+                    Console.WriteLine("Error: Unrecognized input.");
+                }
+                else
+                {
+                    try
+                    {
+                        
+                        result = calculator.DoOperation(cleanNum1, cleanNum2, op.Trim(), count);
+                        list.Add(result);
+                        
+                        
+                        
+
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+                }
+                Console.WriteLine("------------------------\n");
+
+                // Show the number of uses at the end of each calculation.
+                // After, wait for the user to respond before closing.
+                Console.WriteLine("You have used this app: " + ++count + " time(s).");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            
+            // Add call to close the JSON writer before return
+            calculator.Finish();
+           
+            return;
+        }
+    }
+}
---
+++ Calculator [Grey Klouds]/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,96 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+        public double DoOperation(double num1, double num2, string op, int usageCount)
+        {
+            List<double> history = new List<double>();  
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    history.Add(result);
+                    writer.WriteValue("Add");
+                    usageCount++;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    history.Add(result);
+                    writer.WriteValue("Subtraction");
+                    usageCount++;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    history.Add(result);
+                    writer.WriteValue("Multiply");
+                    usageCount++;
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        history.Add(result);
+                        writer.WriteValue("Divide");
+                    }
+                    usageCount++;
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+            return result;
+        }
+
+        public List<double> getHistory(List<double> list)
+        {
+            Console.WriteLine("\t\t\t Calculation history:");
+            Console.WriteLine("\t\t\t-----------------");
+
+            foreach (double item in list)
+            {
+                Console.WriteLine(item);
+            }
+            return list;
+        }
+
+        public void ShowUses(int count)
+        {
+            Console.WriteLine($"You have used this application {count} times.");
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+    }
+}
