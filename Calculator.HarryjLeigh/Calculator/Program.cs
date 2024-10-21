999add48b436ee0728caedd534bea3d294ba2a39
harryL <harryleigh@hotmail.co.uk>
2024-10-21 12:19:49 +0100

    Project Finished.

---
+++ Calculator.HarryjLeigh/Calculator/Program.cs
@@ -0,0 +1,185 @@
+﻿
+using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+bool endApp = false;
+Calculator calculator = new Calculator();
+// Display title as the C# console calculator app.
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+while (!endApp)
+{
+    // Declare variables and set to empty.
+    // Use Nullable types (with ?) to match type of System.Console.ReadLine
+    string? numInput1 = "";
+    string? numInput2 = "";
+    double cleanNum1 = 0;
+    double cleanNum2 = 0;
+    double result = 0;
+    string? op = "";
+    string? previousCalculationInput = "";
+
+
+
+    // Ask user if they would like to delete previous calculations
+    if (calculator.PreviousCalculations.Count > 0)
+    {
+        Console.WriteLine("Delete previous calculations?  (y/n) ");
+        string? input = Console.ReadLine();
+        if (input == null || !Regex.IsMatch(input, "[y|n]"))
+        {
+            Console.WriteLine("Error: Unrecognised input.");
+        }
+        else
+        {
+            if (input == "y") { calculator.DeletePreviousCalculations(); Console.WriteLine("Previous calculations deleted."); }
+        }
+    }
+
+    // Ask user if they would like to use previous calculation
+    if (calculator.PreviousCalculations.Count > 0)
+    {
+        Console.WriteLine("Would you like to use a previous calculation? (y/n) ");
+        previousCalculationInput = Console.ReadLine();
+        if (previousCalculationInput == null || !Regex.IsMatch(previousCalculationInput, "[y|n]"))
+        {
+            Console.WriteLine("Error: Unrecognised input");
+        }
+    }
+
+    // Uses previous calculation as first number
+    if (previousCalculationInput == "y")
+    {
+        //  Ask for number from previous calculations, parses number and perfoms operation
+        numInput1 = calculator.UsePreviousCalculations();
+        cleanNum1 = parseStringToDouble(numInput1);
+        op = chooseOperation();
+
+        // Validate input is not null, and matches the pattern
+        if (op == null || !Regex.IsMatch(op, "[a|s|m|d|p|sqrt|10x|sin|cos|tan]"))
+        {
+            Console.WriteLine("Error: Unrecognised input.");
+        }
+        else
+        {
+            try
+            {
+                // Checks if input is one number operation
+                if (op == "sqrt" || op == "10x" || op == "sin" || op == "cos" || op == "tan")
+                {
+                    result = calculator.DoOneNumberOperation(cleanNum1, op);
+                }
+                else
+                {
+                    // Runs if input is a two number operation - Asks for number, parses number and performs operation
+                    Console.Write("Type another number, and then press Enter: ");
+                    numInput2 = Console.ReadLine();
+                    cleanNum2 = parseStringToDouble(numInput2);
+                    result = calculator.DoTwoNumberOperation(cleanNum1, cleanNum2, op);
+                }
+                // Error checking
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+        }
+    }
+
+    // Runs when user would like new numbers 
+    if (previousCalculationInput == "" || previousCalculationInput == "n")
+    {
+        // Ask the user to type the first number. Parses number and performs operation
+        Console.Write("Type a number, and then press Enter: ");
+        numInput1 = Console.ReadLine();
+        cleanNum1 = parseStringToDouble(numInput1);
+        op = chooseOperation();
+
+        // Validate input is not null, and matches the pattern
+        if (op == null || !Regex.IsMatch(op, "[a|s|m|d|p|sqrt|10x|sin|cos|tan]"))
+        {
+            Console.WriteLine("Error: Unrecognised input.");
+        }
+        else
+        {
+            try
+            {
+                // Checks if input is one number operation
+                if (op == "sqrt" || op == "10x" || op == "sin" || op == "cos" || op == "tan")
+                {
+                    result = calculator.DoOneNumberOperation(cleanNum1, op);
+                }
+                else
+                {
+                    // Runs if input is a two number operation - Asks for number, parses number and performs operation
+                    Console.Write("Type another number, and then press Enter: ");
+                    numInput2 = Console.ReadLine();
+                    cleanNum2 = parseStringToDouble(numInput2);
+                    result = calculator.DoTwoNumberOperation(cleanNum1, cleanNum2, op);
+                }
+                // Error checking
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+        }
+    }
+
+    Console.WriteLine("------------------------\n");
+
+    // Wait for the user to respond before closing.
+    Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+    if (Console.ReadLine() == "n")
+    {
+        Console.WriteLine($"Number of calculations: {calculator.CalculationsCount}");
+        endApp = true;
+    }
+    Console.WriteLine("\n"); // Friendly linespacing.
+}
+// Finish JSON File
+calculator.Finish();
+
+// FUNCTIONS
+
+string? chooseOperation()
+{
+    // Ask the user to choose an operator.
+    Console.WriteLine("Choose an operator from the following list:");
+    Console.WriteLine("\ta - Add");
+    Console.WriteLine("\ts - Subtract");
+    Console.WriteLine("\tm - Multiply");
+    Console.WriteLine("\td - Divide");
+    Console.WriteLine("\tp - Power");
+    Console.WriteLine("\tsqrt - Square Root");
+    Console.WriteLine("\t10x - Ten Times");
+    Console.WriteLine("\tsin - Sin");
+    Console.WriteLine("\tcos - Cos");
+    Console.WriteLine("\ttan - Tan");
+    Console.Write("Your option? ");
+
+    string? choice = Console.ReadLine();
+    return choice;
+}
+
+double parseStringToDouble(string? numInput)
+{
+    double cleanNum = 0;
+    while (!double.TryParse(numInput, out cleanNum))
+    {
+        Console.Write("This is not valid input. Please enter a numeric value: ");
+        numInput = Console.ReadLine();
+    }
+    return cleanNum;
+}
---
+++ Calculator.HarryjLeigh/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,169 @@
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+        public int CalculationsCount { get; private set; } = 0;
+        public List<double> PreviousCalculations { get; private set; } = new List<double>();
+        public Calculator()
+        {
+            // Creates  and opens JSON file
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        // Closes JSON file
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+        public double DoOneNumberOperation(double num1, string op) 
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.]
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operation");
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "sqrt":
+                   result = Math.Sqrt(num1);
+                    writer.WriteValue("Square Root");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "10x":
+                    result = num1 * 10;
+                    writer.WriteValue("10x");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "sin":
+                    result = Math.Sin(num1);
+                    writer.WriteValue("Sin");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "cos":
+                    result = Math.Cos(num1);
+                    writer.WriteValue("Cos");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "tan":
+                    result = Math.Tan(num1);
+                // change below into function
+                    writer.WriteValue("Tan");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+            return result;
+        }
+
+        public double DoTwoNumberOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        writer.WriteValue("Divide");
+                        IncreaseCalculationsCount();
+                        AddToPreviousCalculations(result);
+                    }
+                    break;
+                case "p":
+                    result = Math.Pow(num1, num2);
+                    writer.WriteValue("Power");
+                    IncreaseCalculationsCount();
+                    AddToPreviousCalculations(result);
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+            return result;
+        }
+
+        private void IncreaseCalculationsCount() => CalculationsCount++;
+        public void DeletePreviousCalculations() => PreviousCalculations.Clear();
+        private void AddToPreviousCalculations(double result) => PreviousCalculations.Add(result);
+
+        public string UsePreviousCalculations()
+        {
+            string? numFromPrevious = "";
+            // Writes previous calculations to console to choose from
+            Console.WriteLine();
+            Console.Write("Previous Calculations: ");
+            foreach (double result in PreviousCalculations)
+            {
+                if (PreviousCalculations.Last() == result) Console.Write($"{result} ");
+                else Console.Write($"{result}, ");
+            }
+
+            // Checks whether choice is from previous calculations list
+            while (numFromPrevious == "" || numFromPrevious == null)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Select a number to use: ");
+                numFromPrevious = Console.ReadLine();
+
+                // check if choice is valid if not asks user again for number
+                while (!PreviousCalculations.Contains(Convert.ToDouble(numFromPrevious)))
+                {
+                    Console.WriteLine("Select a number from previous calculations: ");
+                    numFromPrevious = Console.ReadLine();
+                }
+            }
+            return numFromPrevious;
+        }
+    }
+}
