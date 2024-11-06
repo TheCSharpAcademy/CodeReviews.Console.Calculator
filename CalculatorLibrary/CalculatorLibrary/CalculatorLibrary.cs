d7f911c23aad3fbc5696f1d99a263677963f7d40
Valentina Conte McCune <titinaconte@outlook.it>
2024-11-05 21:47:06 -0500

    Finally finishing the calculator app

---
+++ CalculatorLibrary/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,138 @@
+﻿// CalculatorLibrary.cs
+using System.Diagnostics;
+using System.Reflection;
+
+// CalculatorLibrary.cs
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+
+    public class Calculator
+    {
+        // CalculatorLibrary.cs
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+        // CalculatorLibrary.cs
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+
+        // CalculatorLibrary.cs
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    writer.WriteValue("Divide");
+                    break;
+                // Return text for an incorrect option entry.
+                case "r":
+                    {
+                        result = Math.Sqrt(num1);
+                        writer.WriteValue("SquareRoot");
+                        break;
+
+                    }
+                case "t":
+                    {
+                        result = num1 * 10.00;
+                        writer.WriteValue("10x");
+                        break;
+                    }
+                case "p":
+                    {
+                        result = Math.Pow(num1, num2);
+                        writer.WriteValue("power of");
+                        break;
+                    }
+                case "c":
+                    {
+                        result = Math.Cos(num1);
+                        writer.WriteValue("cosine");
+                        break;
+                    }
+                case "sn":
+                    {
+                        result = Math.Sin(num1);
+                        writer.WriteValue("sine");
+                        break;
+                    }
+                case "ta":
+                    {
+                        result = Math.Tan(num1);
+                        writer.WriteValue("tangent");
+                        break;
+                    }
+                case "ct":
+                    {
+                        result = Math.Cos(num1) / Math.Sin(num1);
+                        writer.WriteValue("cotangent");
+                        break;
+                    }
+                case "se":
+                    {
+                        result = 1 / Math.Cos(num1);
+                        writer.WriteValue("secant");
+                        break;
+                    }
+                case "cose":
+                    {
+                        result = 1 / Math.Sin(num1);
+                        writer.WriteValue("cosecant");
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+         
+        }
+    }
+
+
---
+++ calculatorrr/calculatorrr/Program.cs
@@ -0,0 +1,353 @@
+﻿using System.Text.RegularExpressions;
+// Program.cs
+using CalculatorLibrary;
+// Program.cs
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+        // Program.cs
+        Calculator calculator = new Calculator();
+
+        List<string> pastCalc = new List<string>();
+        int timesUsed = 0;
+
+        while (!endApp)
+        {
+            // Declare variables and set to empty.
+            // Use Nullable types (with ?) to match type of System.Console.ReadLine
+            string? numInput1 = "";
+            string? numInput2 = "";
+            double result = 0;
+            double cleanNum1 = 0; 
+            double cleanNum2 = 0;
+
+
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("\tp - Power of");
+            Console.WriteLine("\tr - Square root");
+            Console.WriteLine("\tt - 10x");
+            Console.WriteLine("\tc - cosine");
+            Console.WriteLine("\tsn - sine");
+            Console.WriteLine("\tta - tangent");
+            Console.WriteLine("\tct - cotanget");
+            Console.WriteLine("\tse - secant");
+            Console.WriteLine("\tcose - cosecant");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+
+
+
+                // Validate input is not null, and matches the pattern
+            if (op == null || !Regex.IsMatch(op, "[a|s|m|d|p|r|t|c|sn|ta|ct|se|cose]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            {
+                try
+                {
+                    // Program.cs
+                    // Program.cs
+                    if ((op == "a") || (op == "s") || (op == "m") || (op == "d"))
+                    {
+                        Console.WriteLine("Do you want to use a previous result for your calculation? y or n");
+                        string sult = Console.ReadLine().ToLower();
+                        if (sult == "y")
+                        {
+                            if (pastCalc.Count == 0)
+                            {
+                                Console.WriteLine("you have no items in your history");
+                                // Ask the user to type the first number.
+                                Console.Write("Type a number, and then press Enter: ");
+                                numInput1 = Console.ReadLine();
+
+
+                                while (!double.TryParse(numInput1, out cleanNum1))
+                                {
+                                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                                    numInput1 = Console.ReadLine();
+                                }
+
+                            }
+                            else
+                            {
+                                foreach (string item in pastCalc)
+                                {
+                                    int index = pastCalc.IndexOf(item);
+                                    Console.WriteLine($"{index}: {item} ");
+                                }
+                                Console.WriteLine("Which result do you want to use?");
+                                string ? dex = Console.ReadLine();
+                                int indexCalc = int.Parse(dex);
+                                string calcToUse = pastCalc[indexCalc];
+                                int indexOfEqual = calcToUse.IndexOf("=");
+                                int lenghtCalc = calcToUse.Length;
+                                string resultClean = calcToUse.Substring(indexOfEqual+1).Trim();
+                                double resClean = double.Parse(resultClean);
+                                cleanNum1 = resClean;
+
+
+                            }
+                        }
+                        else if (sult == "n")
+                        {
+                        // Ask the user to type the first number.
+                        Console.Write("Type a number, and then press Enter: ");
+                        numInput1 = Console.ReadLine();
+
+                        
+                        while (!double.TryParse(numInput1, out cleanNum1))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            numInput1 = Console.ReadLine();
+                        }
+
+                        }
+                        else { 
+                            Console.WriteLine("Not a valid choice");
+                            // Ask the user to type the first number.
+                            Console.Write("Type a number, and then press Enter: ");
+                        numInput1 = Console.ReadLine();
+
+
+                        while (!double.TryParse(numInput1, out cleanNum1))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            numInput1 = Console.ReadLine();
+                        }
+
+                    }
+
+                    // Ask the user to type the second number.
+                    Console.Write("Type another number, and then press Enter: ");
+                        numInput2 = Console.ReadLine();
+
+                        
+                        while (!double.TryParse(numInput2, out cleanNum2))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            numInput2 = Console.ReadLine();
+                        }
+
+                    }
+                    else if (op == "p")
+                    {
+                        Console.WriteLine("Do you want to use a previous result for your calculation? y or n");
+                        string sult = Console.ReadLine().ToLower();
+                        if (sult == "y")
+                        {
+                            if (pastCalc.Count == 0)
+                            {
+                                Console.WriteLine("you have no items in your history");
+                                // Ask the user to type the first number.
+                                Console.Write("Type a number, and then press Enter: ");
+                                numInput1 = Console.ReadLine();
+
+
+                                while (!double.TryParse(numInput1, out cleanNum1))
+                                {
+                                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                                    numInput1 = Console.ReadLine();
+                                }
+
+                            }
+                            else
+                            {
+                                foreach (string item in pastCalc)
+                                {
+                                    int index = pastCalc.IndexOf(item);
+                                    Console.WriteLine($"{index}: {item} ");
+                                }
+                                Console.WriteLine("Which result do you want to use?");
+                                string? dex = Console.ReadLine();
+                                int indexCalc = int.Parse(dex);
+                                string calcToUse = pastCalc[indexCalc];
+                                int indexOfEqual = calcToUse.IndexOf("=");
+                                int lenghtCalc = calcToUse.Length;
+                                string resultClean = calcToUse.Substring(indexOfEqual+1).Trim();
+                                double resClean = double.Parse(resultClean);
+                                cleanNum1 = resClean;
+
+
+                            }
+                        }
+                        else if (sult == "n")
+                        {
+// Ask the user to type the first number.
+                            Console.Write("Type a number, and then press Enter: ");
+                        numInput1 = Console.ReadLine();
+
+                        
+                        while (!double.TryParse(numInput1, out cleanNum1))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            numInput1 = Console.ReadLine();
+                        }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not a valid choice");
+                            // Ask the user to type the first number.
+                            Console.Write("Type a number, and then press Enter: ");
+                            numInput1 = Console.ReadLine();
+
+
+                            while (!double.TryParse(numInput1, out cleanNum1))
+                            {
+                                Console.Write("This is not valid input. Please enter a numeric value: ");
+                                numInput1 = Console.ReadLine();
+                            }
+
+                        }
+
+
+
+                        // Ask the user to type the second number.
+                        Console.Write("Type your exponent number, and then press Enter: ");
+                        numInput2 = Console.ReadLine();
+
+                        
+                        while (!double.TryParse(numInput2, out cleanNum2))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            numInput2 = Console.ReadLine();
+                        }
+                    }
+                    else if ((op == "r") || (op == "t") || (op == "c") || (op == "sn") || (op == "ta") || (op == "ct") || (op == "se") || (op == "cose"))
+                    {
+                        Console.WriteLine("Do you want to use a previous result for your calculation? y or n");
+                        string sult = Console.ReadLine().ToLower();
+                        if (sult == "y")
+                        {
+                            if (pastCalc.Count == 0)
+                            {
+                                Console.WriteLine("you have no items in your history");
+                                // Ask the user to type the first number.
+                                Console.Write("Type a number, and then press Enter: ");
+                                numInput1 = Console.ReadLine();
+
+
+                                while (!double.TryParse(numInput1, out cleanNum1))
+                                {
+                                    Console.Write("This is not valid input. Please enter a numeric value: ");
+                                    numInput1 = Console.ReadLine();
+                                }
+
+                            }
+                            else
+                            {
+                                foreach (string item in pastCalc)
+                                {
+                                    int index = pastCalc.IndexOf(item);
+                                    Console.WriteLine($"{index}: {item} ");
+                                }
+                                Console.WriteLine("Which result do you want to use?");
+                                string? dex = Console.ReadLine();
+                                int indexCalc = int.Parse(dex);
+                                string calcToUse = pastCalc[indexCalc];
+                                int indexOfEqual = calcToUse.IndexOf("=");
+                                int lenghtCalc = calcToUse.Length;
+                                string resultClean = calcToUse.Substring(indexOfEqual+1).Trim();
+                                double resClean = double.Parse(resultClean);
+                                cleanNum1 = resClean;
+
+                            }
+                        }
+                        else if (sult == "n")
+                        {
+// Ask the user to type the first number.
+                            Console.Write("Type a number, and then press Enter: ");
+                        numInput1 = Console.ReadLine();
+
+                        
+                        while (!double.TryParse(numInput1, out cleanNum1))
+                        {
+                            Console.Write("This is not valid input. Please enter a numeric value: ");
+                            numInput1 = Console.ReadLine();
+                        }
+                        }
+                        else
+                        {
+                            Console.WriteLine("input not valid");
+                            // Ask the user to type the first number.
+                            Console.Write("Type a number, and then press Enter: ");
+                            numInput1 = Console.ReadLine();
+
+
+                            while (!double.TryParse(numInput1, out cleanNum1))
+                            {
+                                Console.Write("This is not valid input. Please enter a numeric value: ");
+                                numInput1 = Console.ReadLine();
+                            }
+
+                        }
+
+
+                    }
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            pastCalc.Add($" {cleanNum1} {op} {cleanNum2} = {result}");
+            timesUsed++;
+            Console.Write("Press 'n' and Enter to close the app, press 'h' to view your history, press 't' to view how many times you used the calculator or press any other key and Enter to continue: ");
+            string ? newR = Console.ReadLine().ToLower();
+            if (newR == "n") endApp = true;
+            else if ((newR == "h"))
+            {
+                foreach (var item in pastCalc)
+                {
+                    int index = pastCalc.IndexOf(item);
+                    Console.WriteLine($"{index}: {item} ");
+                }
+                Console.WriteLine("Do you want to erase all your history? y or n");
+                if (Console.ReadLine() == "y")
+                {
+                    pastCalc.Clear();
+                    continue;
+                }
+                else continue;
+
+            }
+            else if  (newR == "t") {Console.WriteLine($"you used the app  {timesUsed} times"); continue; }
+            else
+            {
+                
+                continue;
+            }
+                
+            Console.WriteLine("\n"); // Friendly linespacing.
+            // Program.cs
+            // Add call to close the JSON writer before return
+            calculator.Finish();
+            return;
+        }
+
+    }
+}
