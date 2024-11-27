bda90f502e35df79b07ff61dd435d11c26d1b905
Ryan Macdonald <ryan@ryanmacdonald.io>
2024-11-27 14:52:24 +1100

    refactored Calculator class to CalculatorLibrary + added basic logging

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,41 +1,5 @@
 ﻿using System.Text.RegularExpressions;
-
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double
-            result = double
-                .NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-
-        return result;
-    }
-}
+using CalculatorLibrary;
 
 class Program
 {
@@ -46,6 +10,8 @@ class Program
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
 
+        Calculator calculator = new Calculator();
+        
         while (!endApp)
         {
             // Declare variables and set to empty.
@@ -95,7 +61,7 @@ class Program
             {
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
