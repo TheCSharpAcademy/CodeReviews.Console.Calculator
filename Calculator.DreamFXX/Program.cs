bacc04107f035148766ea62fcfae29856ff601be
DreamFXX <filipek.husak@gmail.com>
2024-12-04 12:49:10 +0100

    cleanup?

--- Calculator.DreamFXX/Program.cs
+++ Calculator.DreamFXX/Program.cs
@@ -1,78 +1,83 @@
 ﻿using CalculatorLibrary;
 using System.Text.RegularExpressions;
 
-namespace CalculatorProgram;
-class Program
+namespace CalculatorProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        bool endApp = false;
-        Console.WriteLine("Console Calculator in C#\r");
-        Console.WriteLine("------------------------\n");
-
-        Calculator calculator = new Calculator();
-        while (!endApp)
+        static void Main(string[] args)
         {
-            string? numInput1 = "";
-            string? numInput2 = "";
-            double result = 0;
-
-            Console.Write("Type a number, and then press Enter: ");
-            numInput1 = Console.ReadLine();
-
-            double cleanNum1 = 0;
-            while (!double.TryParse(numInput1, out cleanNum1))
-            {
-                Console.Write("This is not valid input. Input must be a number. Try again and enter a numeric value: ");
-                numInput1 = Console.ReadLine();
-            }
-
-            Console.Write("Type second number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
-
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
-            {
-                Console.Write("This is not valid input. Input must be a number. Try again and enter a numeric value: ");
-                numInput2 = Console.ReadLine();
-            }
-
-            Console.WriteLine("Choose an operator from the following list:");
-            Console.WriteLine("\ta - Add");
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.Write("Your option? ");
-
-            string? op = Console.ReadLine();
-
-            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
-            {
-                Console.WriteLine("Error: Input is not valid.");
-            }
-            else
-            {
-                try
-                {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
-                    if (double.IsNaN(result))
-                    {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
-                    }
-                    else Console.WriteLine("Result: {0:0.##}\n", result);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error - Something went wrong.\n - Details: " + e.Message);
-                }
-            }
+            bool endApp = false;
+            Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
+
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+                string? numInput1 = "";
+                string? numInput2 = "";
+                double result = 0;
+
+                Console.Write("Type a number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("This is not valid input. Input must be a number. Try again and enter a numeric value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                Console.Write("Type second number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Input must be a number. Try again and enter a numeric value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+                {
+                    Console.WriteLine("Error: Input is not valid.");
+                }
+                else
+                {
+                    try
+                    {
+                        result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else Console.WriteLine("Result: {0:0.##}\n", result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error - Something went wrong.\n - Details: " + e.Message);
+                    }
+                }
+                Console.WriteLine("------------------------\n");
 
             Console.Write("Enter the n symbol to Close the app. Press any key to continue: ");
             if (Console.ReadLine() == "n") endApp = true;
 
             Console.WriteLine("\n");
-        }
+            }
+
+            calculator.Finish();
         return;
+        }
     }
+
 }
--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -5,43 +5,63 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
+        JsonWriter writer;
         public Calculator()
         {
-            StreamWriter logFile = File.CreateText("calculator.log");
-            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-            Trace.AutoFlush = true;
-            Trace.WriteLine("Starting Calculator Log");
-            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+            StreamWriter logFile = File.CreateText("calculatorLog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
         }
-
-        public static double DoOperation(double num1, double num2, string op)
+        
+        public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
             switch (op)
             {
                 case "a":
                     result = num1 + num2;
-                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Add");
                     break;
                 case "s":
                     result = num1 - num2;
-                    Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Subtract");
                     break;
                 case "m":
                     result = num1 * num2;
-                    Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    writer.WriteValue("Multiply");
                     break;
                 case "d":
                     if (num2 != 0)
                     {
                         result = num1 / num2;
-                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                        writer.WriteValue("Divide");
                     }
                     break;
                 default:
                     break;
             }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
             return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
         }
     }
 }
