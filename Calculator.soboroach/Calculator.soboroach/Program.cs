a289716affdc5e1950836f2db36422510fca6019
soboroach <app8229@knou.ac.kr>
2024-12-16 16:35:30 +0900

    calculator final

---
+++ Calculator.soboroach/Calculator.soboroach/Program.cs
@@ -0,0 +1,176 @@
+﻿// Program.cs
+using CalculatorLibrary;
+using System.Text.RegularExpressions;
+
+namespace CalculatorProgram
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            bool IsReused = false;
+            bool InsertAndReplaceCheck = false;
+
+            int count = 0;
+            int saveNumber = 0;
+            int firstTempValue = -1;
+
+
+            String Select = "";
+
+            List<double> resultNumbers = new List<double>();
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Calculator calculator = new Calculator();
+            while (!endApp)
+            {
+                saveNumber = resultNumbers.Count;
+
+                while (!Select.Equals("x"))
+                {
+                    Console.WriteLine("\tb - Calculator usage counter");
+                    Console.WriteLine("\tz - chaining calculations");
+                    Console.WriteLine("\tk - result remove");
+                    Console.WriteLine("\tx - skip");
+                    Console.Write("Your option? ");
+
+                    Select = Console.ReadLine();
+
+                    if (Select == null || !Regex.IsMatch(Select, "[b|z|x|k]"))
+                    {
+                        Console.WriteLine("Error: Unrecognized input.");
+                        continue;
+                    }
+                    else if (Select == "b")
+                    {
+                        calculator.UsageCount(count);
+                    }
+                    else if (Select == "z")
+                    {
+                        saveNumber = calculator.ChainingCalculations(Select, resultNumbers, ref IsReused, ref firstTempValue);
+                        break;
+                    }
+                    else if (Select == "k")
+                    {
+                        calculator.ResultRemove(resultNumbers);
+                    }
+                }
+
+                Select = "";
+
+
+                // Declare variables and set to empty.
+                // Use Nullable types (with ?) to match type of System.Console.ReadLine
+                string? numInput1 = "";
+                string? numInput2 = "";
+
+                double result = 0;
+
+                // Ask the user to type the first number.
+
+                if (!IsReused)
+                {
+                    Console.Write("Type a number, and then press Enter: ");
+                    numInput1 = Console.ReadLine();
+                }
+                else
+                {
+                    numInput1 = resultNumbers[saveNumber].ToString();
+                    IsReused = false;
+                    InsertAndReplaceCheck = true;
+                }
+
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Ask the user to type the second number.
+                Console.Write("Type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\tr - Square root");
+                Console.WriteLine("\te - Exponentiation");
+                Console.WriteLine("\tt - Tenfold - Only num1 is applied.");
+                Console.WriteLine("\ti - Sine - Only num1 is applied.");
+                Console.WriteLine("\tc - Cosine - Only num1 is applied.");
+                Console.WriteLine("\tg - Tangent - Only num1 is applied.");
+                Console.Write("Your option? ");
+
+                string? op = Console.ReadLine();
+
+                // Validate input is not null, and matches the pattern
+                if (op == null || !Regex.IsMatch(op, "[a|s|m|d|r|e|t|i|c|g]"))
+                {
+                    Console.WriteLine("Error: Unrecognized input.");
+                }
+                else
+                {
+                    try
+                    {
+                        result = calculator.DoOperation(cleanNum1, cleanNum2, op, count, resultNumbers, IsReused);
+
+                        if (double.IsNaN(result))
+                        {
+                            Console.WriteLine("This operation will result in a mathematical error.\n");
+                        }
+                        else if (IsReused == true)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            count++;
+
+                            if (InsertAndReplaceCheck == false)
+                            {
+                                resultNumbers.Add(result);
+                            }
+                            else
+                            {
+                                resultNumbers[saveNumber] = result;
+                                InsertAndReplaceCheck = false;
+                            }
+
+
+                            Console.WriteLine("Your result: {0:0.##}\n", result);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                    }
+                }
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            calculator.Finish();
+            return;
+        }
+    }
+}
---
+++ Calculator.soboroach/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,163 @@
+﻿// CalculatorLibrary.cs
+using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public double DoOperation(double num1, double num2, string op, int count, List<double> resultNumbers, bool IsReused)
+        {
+
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+
+            double radians = num1 * (Math.PI / 180.0);
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    writer.WriteValue("Divide");
+                    break;
+                case "r":
+                    result = Math.Sqrt(num1 * num2);
+                    writer.WriteValue("Square root");
+                    break;
+                case "e":
+                    result = Math.Pow(num1, num2);
+                    writer.WriteValue("Exponentiation");
+                    break;
+                case "t":
+                    result = num1 * 10;
+                    writer.WriteValue("Tenfold");
+                    break;
+                case "i":
+                    result = Math.Sin(radians);
+                    writer.WriteValue("Sine");
+                    break;
+                case "c":
+                    result = Math.Cos(radians);
+                    writer.WriteValue("Cosine");
+                    break;
+                case "g":
+                    result = Math.Tan(radians);
+                    writer.WriteValue("Tangent");
+                    break;
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
+            return result;
+        }
+
+        public int ChainingCalculations(String select, List<double> resultNumbers, ref bool IsReused, ref int firstTempValue)
+        {
+            if (resultNumbers.Count == 0)
+            {
+                Console.WriteLine("No results found. Returning to the beginning.");
+            }
+            else
+            {
+                int numbering = 1;
+                foreach (double saveNumber in resultNumbers)
+                {
+                    Console.WriteLine(numbering + ". {0:0.##}", saveNumber);
+                    numbering++;
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Here is the recent results list. The higher the number, the more recent the result. If you want to calculate with these numbers, please press the number next to it or 'n'.");
+                    if (int.TryParse(Console.ReadLine(), out firstTempValue))
+                    {
+                        firstTempValue -= 1; // 입력이 유효하면 1을 뺍니다.
+                        IsReused = true;
+                        return firstTempValue; // 변환이 성공하면 루프를 종료합니다.
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a number.");
+                    }
+                }
+            }
+            return -1; // 기본 반환값 추가
+        }
+
+        public void ResultRemove(List<double> resultNumbers)
+        {
+            if (resultNumbers.Count == 0)
+            {
+                Console.WriteLine("No results found. Returning to the beginning.");
+                return;
+            }
+            else
+            {
+                int numbering = 1;
+                foreach (double saveNumber in resultNumbers)
+                {
+                    Console.WriteLine(numbering + ". {0:0.##}", saveNumber);
+                    numbering++;
+                }
+            }
+            Console.WriteLine("Please enter the number to delete.");
+
+            resultNumbers.RemoveAt(int.Parse(Console.ReadLine()) - 1);
+        }
+
+        public void UsageCount(int count)
+        {
+            Console.WriteLine("usage counter: " + count);
+            if (count != 0)
+            {
+                count--;
+            }
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+
+    }
+}
