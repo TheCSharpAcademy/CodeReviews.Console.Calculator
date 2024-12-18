dacad1e7f806ab4acf42cd25d161ecec476525ee
Okiemute Agbatutu <kite_tutu@yahoo.com>
2024-12-18 09:17:16 +0100

    Adjusted the user menu to accomodate more functions

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,188 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+class Program
+
+{
+    private static double[] getGeneralArguments()
+    {
+        string? numInput1 = "";
+        string? numInput2 = "";
+
+        // Ask the user to type the first number.
+        Console.Write("Type a number, and " +
+            "then press Enter: ");
+        numInput1 = Console.ReadLine();
+
+        double cleanNum1 = 0;
+        while (!double.TryParse(numInput1, out cleanNum1))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput1 = Console.ReadLine();
+        }
+
+        // Ask the user to type the second number.
+        Console.Write("Type another number, and then press Enter: ");
+        numInput2 = Console.ReadLine();
+
+        double cleanNum2 = 0;
+        while (!double.TryParse(numInput2, out cleanNum2))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput2 = Console.ReadLine();
+        }
+        return [cleanNum1, cleanNum2];
+    }
+
+    private static string[] getTrigFunctionArguments()
+    {
+        string? numInput1 = "";
+        string? numInput2 = "";
+
+        // Ask the user to type the first number.
+        Console.Write("Type either sin, cos or tan, and then press Enter: ");
+        numInput1 = Console.ReadLine();
+
+        //double cleanNum1 = 0;
+        while (numInput1 == "" || numInput1 == null)
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput1 = Console.ReadLine();
+        }
+
+        // Ask the user to type the second number.
+        Console.Write("Type another number, and then press Enter: ");
+        numInput2 = Console.ReadLine();
+
+        //double cleanNum2 = 0;
+        while (numInput2 == "" || numInput2 == null)
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput2 = Console.ReadLine();
+        }
+        return [numInput1, numInput2];
+    }
+
+    private static double getSquareRootArgument()
+    {
+        string? numInput1 = "";
+
+        // Ask the user to type the first number.
+        Console.Write("Type a number, and then press Enter: ");
+        numInput1 = Console.ReadLine();
+
+        double cleanNum1 = 0;
+        while (!double.TryParse(numInput1, out cleanNum1))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput1 = Console.ReadLine();
+        }
+
+        return cleanNum1;
+    }
+
+    private static double getTenToPowerArgument()
+    {
+        string? numInput1 = "";
+
+        // Ask the user to type the first number.
+        Console.Write("Type a number, and then press Enter: ");
+        numInput1 = Console.ReadLine();
+
+        double cleanNum1 = 0;
+        while (!double.TryParse(numInput1, out cleanNum1))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput1 = Console.ReadLine();
+        }
+
+        return cleanNum1;
+    }
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        Calculator calculator = new Calculator();
+
+        while (!endApp)
+        {
+            double result = 0;
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.WriteLine("\tr - Square root");
+            Console.WriteLine("\tp - Power");
+            Console.WriteLine("\tx - 10th Power");
+            Console.WriteLine("\tt - Trigonometric Function");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+
+            // Validate input is not null, and matches the pattern
+            if (op == null || !Regex.IsMatch(op, "[a|s|m|d|r|p|x|t]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            {
+                try
+                {
+                    calculator.setNumberOfTimes();
+                    switch (op)
+                    {
+                        case "a":
+                        case "s":
+                        case "m":
+                        case "d":
+                        case "p":
+                            double[] entriesArray = getGeneralArguments();
+                            result = calculator.DoOperation(op, entriesArray[0], entriesArray[1]);
+                            break;
+                        case "r":
+                            result = calculator.DoOperation(op, getSquareRootArgument());
+                            break;
+                        case "x":
+                            result = calculator.DoOperation(op, getTenToPowerArgument());
+                            break;
+                        case "t":
+                            string[] entriesArray2 = getTrigFunctionArguments();
+                            double v = Convert.ToDouble(entriesArray2[1]);
+                            string w = entriesArray2[0];
+                            result = calculator.DoOperation(op, functionName: w, angle: v);
+
+                            break;
+                        // Return text for an incorrect option entry.
+                        default:
+                            break;
+                    }
+
+                    //result = calculator.DoOperation(op);
+                    Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred.\n - Details: " + e.Message);
+                }
+            }
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+
+        // Add call to close the JSON writer before return
+        calculator.Finish();
+
+        return;
+    }
+}
---
+++ Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,144 @@
+﻿// CalculatorLibrary.cs
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Multiply(double a, double b) => a * b;
+        public double Divide(double a, double b)
+        {
+            if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+        public double SquareRoot(double a)
+        {
+            if (a < 0) throw new ArgumentException("Cannot calculate square root of a negative number.");
+            return Math.Sqrt(a);
+        }
+        public double Power(double baseValue, double exponent) => Math.Pow(baseValue, exponent);
+        public double TenToThePower(double a) => Math.Pow(10, a);
+        public double TrigonometricFunction(string function, double angle)
+        {
+            if (function == null)
+            {
+                throw new ArgumentNullException("Null Function Argument passed");
+            }
+            else
+            {
+                switch (function.ToLower())
+                {
+
+                    case "sin": return Math.Sin(angle);
+                    case "cos": return Math.Cos(angle);
+                    case "tan": return Math.Tan(angle);
+                    default: throw new ArgumentException("Invalid trigonometric function.");
+
+
+                }
+            }
+        }
+        public static int NumberOfTimes { get; set; } = 0;
+
+        public int getNumberOfTimes()
+        {
+            return NumberOfTimes;
+        }
+
+        public void setNumberOfTimes()
+        {
+            NumberOfTimes++;
+        }
+
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+
+        public double DoOperation(string op, [Optional] double num1, [Optional] double num2, [Optional] string functionName, [Optional] double angle)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(Convert.ToString(num1));
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(Convert.ToString(num2));
+            writer.WritePropertyName("Operand3");
+            writer.WriteValue(functionName);
+            writer.WritePropertyName("Operand4");
+            writer.WriteValue(Convert.ToString(angle));
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = Add(num1, num2);
+                    writer.WriteValue("Add");
+                    break;
+                case "s":
+                    result = Subtract(num1, num2);
+                    writer.WriteValue("Subtract");
+                    break;
+                case "m":
+                    result = Multiply(num1, num2);
+                    writer.WriteValue("Multiply");
+                    break;
+                case "d":
+                    result = Divide(num1, num2);
+                    writer.WriteValue("Divide");
+                    break;
+                case "r":
+                    result = SquareRoot(num1);
+                    writer.WriteValue("Square Root");
+                    break;
+                case "p":
+                    result = Power(num1, num2);
+                    writer.WriteValue("Power");
+                    break;
+                case "x":
+                    result = TenToThePower(num1);
+                    writer.WriteValue("Ten To The Power");
+                    break;
+                case "t":
+                    result = TrigonometricFunction(functionName, angle);
+                    writer.WriteValue("Trigonometric Fuction");
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WritePropertyName("Number Of Times");
+            writer.WriteValue(Calculator.NumberOfTimes);
+            writer.WriteEndObject();
+
+
+            return result;
+        }
+    }
+}
