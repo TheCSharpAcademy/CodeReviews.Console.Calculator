ecc385aef7e0f211120c8c608e2c307636c8778e
pkrzysiek <pkrzysiek24@gmail.com>
2025-01-21 05:58:52 +0100

    Refactor calc library

--- pkrzysiekk.Calculator/CalculatorLibrary/CalculatorMenu.cs
+++ pkrzysiekk.Calculator/CalculatorLibrary/CalculatorMenu.cs
@@ -6,8 +6,8 @@ namespace CalculatorLibrary
     public class CalculatorMenu
     {
         bool endApp;
-        double cleanNum1 = 0;
-        double cleanNum2 = 0;
+        double cleanNum1;
+        double cleanNum2;
         public void ShowMenu()
         {
             // Display title as the C# console calculator app.
