501bf87e91c67b5e37671495259aaad20e4167b3
Daniel <Daniel@DESKTOP-EICR6RV>
2025-01-29 11:45:26 +0100

    initial commit

---
+++ Calculator.BrozDa/Calculator.BrozDa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.BrozDa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
