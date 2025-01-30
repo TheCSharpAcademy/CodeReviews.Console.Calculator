5fc41dc4de580a3eb68ccb98271a8525ca679f62
victor cesario <victor.cesario@hotmail.com>
2025-01-30 19:31:59 -0300

    finished part 2

--- Program.cs
+++ Program.cs
@@ -1,37 +1,5 @@
 ﻿using System.Text.RegularExpressions;
-
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
+using CalculatorLibrary;
 
 class Program
 {
@@ -41,6 +9,8 @@ class Program
         // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
+
+        Calculator calculator = new Calculator();
 
         while (!endApp)
         {
@@ -91,7 +61,7 @@ class Program
             {
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -111,6 +81,9 @@ class Program
 
             Console.WriteLine("\n"); // Friendly linespacing.
         }
+
+        // Add call to close the JSON writer before return
+        calculator.Finish();
         return;
     }
 }
