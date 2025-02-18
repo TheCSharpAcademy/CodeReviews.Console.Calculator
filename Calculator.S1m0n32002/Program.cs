189e131c95b2a73aff8fcd64bccf34254a4de956
S1m0n32002 <39035729+S1m0n32002@users.noreply.github.com>
2025-02-18 11:22:06 +0100

    Before revision

--- Calculator.S1m0n32002/Program.cs
+++ Calculator.S1m0n32002/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Declare variables and then initialize to zero.
+double num1 = 0; double num2 = 0;
+
+// Display title as the C# console calculator app.
+Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("------------------------\n");
+
+// Ask the user to type the first number.
+Console.WriteLine("Type a number, and then press Enter");
+while (true)
+{
+    var input = Console.ReadLine();
+    if (double.TryParse(input, out num1))
+        break;
+    Console.WriteLine($"\"{input}\" is not a valid number, insert a number and then press Enter");
+}
+
+// Ask the user to type the second number.
+Console.WriteLine("Type another number, and then press Enter");
+while (true)
+{
+    var input = Console.ReadLine();
+    if (double.TryParse(input, out num2))
+        break;
+    Console.WriteLine($"\"{input}\" is not a valid number, insert a number and then press Enter");
+}
+
+// Ask the user to choose an option.
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
+
+// Use a switch statement to do the math.
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        // Ask the user to enter a non-zero divisor until they do so.
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+// Wait for the user to respond before closing.
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
