8c0d8d40d6cb6ba877405ab32f60531a05a2646e
dan-burger <burger.dan4@gmail.com>
2025-03-04 10:32:30 -0500

    completed Calculator project

---
+++ dan-burger.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        JsonWriter writer;
+        public int timesUsed { get; set; }
+        public List<string> calculationsList { get; private set; } = new List<string>();
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+            timesUsed = 0;
+        }
+
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+            string operation = "";
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    writer.WriteValue("Add");
+                    operation = "+";
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    writer.WriteValue("Subtract");
+                    operation = "-";
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    writer.WriteValue("Multiply");
+                    operation = "*";
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    operation = "/";
+                    writer.WriteValue("Divide");
+                    break;
+                case "r":
+                    result = Math.Sqrt(num1);
+                    writer.WriteValue("Square Root");
+                    operation = "√";
+                    break;
+                case "p":
+                    result = Math.Pow(num1, num2);
+                    writer.WriteValue("Taking The Power");
+                    operation = "^";
+                    break;
+                case "t":
+                    result = Math.Pow(num1, 10);
+                    writer.WriteValue("10x");
+                    operation = "10^";
+                    break;
+                default:
+                    break;
+            }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+            timesUsed++;
+
+            calculationsList.Add($"{num1} {operation} {num2} = {result}");
+            return result;
+        }
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
+        }
+
+        public void ShowList()
+        {
+            foreach (var item in calculationsList)
+            {
+                Console.WriteLine("\nPrevious Calculations:");
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+        }
+        public void ClearList()
+        {
+            calculationsList.Clear();
+        }
+    }
+}
