e45b59662ab8103a6943edba5f664dfcc12ffec1
Dalton Soto <daltonsoto@live.com>
2025-03-10 14:34:07 -0400

    finished project

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+namespace CalculatorProgram;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        int timesUsed = 0;
+        List<string> pastCalculations = new List<string>();
+
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+        Calculator calculator = new Calculator();
+
+        while (!endApp)
+        {
+            // Declare variables and set to empty.
+            // Use Nullable types (with ?) to match type of System.Console.ReadLine
+            string? numInput1 = "";
+            string? numInput2 = "";
+            double result = 0;
+
+            // Ask the user to type the first number.
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            // Ask the user to type the second number.
+            Console.Write("Type another number, and then press Enter: ");
+            numInput2 = Console.ReadLine();
+
+            double cleanNum2 = 0;
+            while (!double.TryParse(numInput2, out cleanNum2))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                numInput2 = Console.ReadLine();
+            }
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\t+ - Add");
+            Console.WriteLine("\t- - Subtract");
+            Console.WriteLine("\t* - Multiply");
+            Console.WriteLine("\t/ - Divide");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+
+            // Validate input is not null, and matches the pattern
+            if (op == null || !Regex.IsMatch(op, "[+\\-*/]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            {
+                try
+                {
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your result: {0:0.##}\n", result);
+                        string storeCalculation = $"{numInput1} {op} {numInput2} = {result}";
+                        pastCalculations.Add(storeCalculation);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            ++timesUsed;
+            Console.WriteLine($"This calculator has been used {timesUsed} times.\n");
+
+            Console.WriteLine("Pasted Calculations:");
+            foreach (var i in pastCalculations)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.Write("\nPress 'n' and Enter to close the app, Press 'd' to delete your past calculations, or press any other key and Enter to continue: ");
+
+            string? lastResult = Console.ReadLine();
+            if (lastResult == "n")
+            {
+                endApp = true;
+            } else if (lastResult == "d")
+            {
+                pastCalculations.Clear();
+            }
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+        calculator.Finish();
+
+        return;
+    }
+}
+
