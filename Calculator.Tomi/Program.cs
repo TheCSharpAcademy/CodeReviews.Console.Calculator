cf9db6d02e3d34a173fdab7f949da4805af9c3af
tomi <remioluwatomicephas@gmail.com>
2025-03-12 22:02:08 +0100

    feat: implemented the calculator entry - program.cs

--- Calculator.Tomi/Program.cs
+++ Calculator.Tomi/Program.cs
@@ -18,8 +18,52 @@ class Program
 
         while (!endApp)
         {
-            return;
+            double result = 0;
+
+            string op = Prompts.PromptForOperation();
+            List<double> operands = [];
+
+            bool reUseResult = Prompts.AskToReUseResult(calculatorEngine.CanReuseResult);
+
+            int requiredOperandCount = op == "sq" || op == "sqr" ? 1 : 2;
+
+            if (reUseResult)
+            {
+                if (requiredOperandCount > 1) operands = Prompts.PromptForOperands(1);
+            }
+            else
+            {
+                operands = Prompts.PromptForOperands(requiredOperandCount);
+            }
+
+            if (calculatorEngine.CanReuseResult && reUseResult) operands.Insert(0, calculatorEngine.PrevResult);
+
+            try
+            {
+                result = calculatorEngine.DoOperation(operands, op);
+
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
         }
+
+        calculatorEngine.Finish();
+        return;
 
     }
 }
