b08ea0b5944a42ac63440a3d3fd65ab244243f04
S1m0n32002 <39035729+S1m0n32002@users.noreply.github.com>
2025-03-13 16:08:38 +0100

    Moved a bunch of methods that ended up inside CalculatorStart()

--- Calculator.S1m0n32002/Calculator.cs
+++ Calculator.S1m0n32002/Calculator.cs
@@ -70,101 +70,104 @@ namespace Calculator.S1m0n32002
                     }
                 }
             }
+        }
 
-            static void WriteTitle()
+        /// <summary>
+        /// Writes the title on top of the console
+        /// <\summary>
+        static void WriteTitle()
+        {
+            Console.Clear();
+
+            AnsiConsole.Write(new Rule("[white]Console Calculator in C#[/]").RuleStyle(Style.Parse("yellow")));
+            AnsiConsole.WriteLine();
+        }
+
+        /// <summary>
+        /// Prompt the user to choose an operation
+        /// </summary>
+        /// <returns></returns>
+        static Operations? PromptOperationChoice()
+        {
+            var prompt = new SelectionPrompt<string>()
             {
-                Console.Clear();
-
-                AnsiConsole.Write(new Rule("[white]Console Calculator in C#[/]").RuleStyle(Style.Parse("yellow")));
-                AnsiConsole.WriteLine();
+                Title = "Choose an operation"
             }
+            .AddChoices([.. strOperations.Keys, "[yellow]Exit[/]"]);
 
-            /// <summary>
-            /// Prompt the user to choose an operation
-            /// </summary>
-            /// <returns></returns>
-            static Operations? PromptOperationChoice()
+            var answer = AnsiConsole.Prompt(prompt);
+
+            if (strOperations.TryGetValue(answer, out var operation))
+                return operation;
+            else
+                return null;
+        }
+
+        static void DoCalc()
+        {
+            WriteTitle();
+
+            var answer = PromptOperationChoice();
+            if (answer == null)
+                return;
+
+            Operations operation = (Operations)answer;
+
+            var numbers = CompileOperation(operation);
+
+            try
             {
-                var prompt = new SelectionPrompt<string>()
+                var result = DoOperation(operation, [.. numbers]);
+
+                if (double.IsNaN(result))
                 {
-                    Title = "Choose an operation"
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
                 }
-                .AddChoices([.. strOperations.Keys, "[yellow]Exit[/]"]);
-
-                var answer = AnsiConsole.Prompt(prompt);
-
-                if (strOperations.TryGetValue(answer, out var operation))
-                    return operation;
-                else
-                    return null;
+                else Console.WriteLine("Your result: {0:0.00}\n", result);
+            }
+            catch (Exception e)
+            {
+                AnsiConsole.WriteLine("Oh no! An exception occurred trying to do the math.\n");
+                AnsiConsole.WriteException(e);
             }
 
-            static void DoCalc()
+            AnsiConsole.MarkupLine("Press [blue]ENTER[/] to continue");
+            Console.ReadLine();
+        }
+
+        static IEnumerable<double> CompileOperation(Operations operation)
+        {
+            List<double> numbers = [];
+
+            numbers.Add(AnsiConsole.Prompt(new TextPrompt<double>("Enter one number number:")));
+
+            /// Prompt the user for the second number if needed
+            switch (operation)
             {
-                WriteTitle();
-
-                var answer = PromptOperationChoice();
-                if (answer == null)
-                    return;
-
-                Operations operation = (Operations)answer;
-
-                var numbers = CompileOperation(operation);
-
-                try
-                {
-                    var result = DoOperation(operation, [.. numbers]);
-
-                    if (double.IsNaN(result))
+                case Operations.Divide:
+                    numbers.Add(AnsiConsole.Prompt(new TextPrompt<double>("Enter the second number")
                     {
-                        Console.WriteLine("This operation will result in a mathematical error.\n");
-                    }
-                    else Console.WriteLine("Your result: {0:0.00}\n", result);
-                }
-                catch (Exception e)
-                {
-                    AnsiConsole.WriteLine("Oh no! An exception occurred trying to do the math.\n");
-                    AnsiConsole.WriteException(e);
-                }
-
-                AnsiConsole.MarkupLine("Press [blue]ENTER[/] to continue");
-                Console.ReadLine();
-            }
-
-            static IEnumerable<double> CompileOperation(Operations operation)
-            {
-                List<double> numbers = [];
-
-                numbers.Add(AnsiConsole.Prompt(new TextPrompt<double>("Enter one number number:")));
-
-                /// Prompt the user for the second number if needed
-                switch (operation)
-                {
-                    case Operations.Divide:
-                        numbers.Add(AnsiConsole.Prompt(new TextPrompt<double>("Enter the second number")
+                        Validator = (input) =>
                         {
-                            Validator = (input) =>
-                            {
-                                if (input == 0)
-                                    return ValidationResult.Error("Cannot divide by zero");
-                                return ValidationResult.Success();
-                            }
-                        }));
-                        break;
+                            if (input == 0)
+                                return ValidationResult.Error("Cannot divide by zero");
+                            return ValidationResult.Success();
+                        }
+                    }));
+                    break;
 
-                    case Operations.Sum:
-                    case Operations.Subtract:
-                    case Operations.Multiply:
-                    case Operations.Power:
-                        numbers.Add(AnsiConsole.Prompt(new TextPrompt<double>("Enter the second number")));
-                        break;
+                case Operations.Sum:
+                case Operations.Subtract:
+                case Operations.Multiply:
+                case Operations.Power:
+                    numbers.Add(AnsiConsole.Prompt(new TextPrompt<double>("Enter the second number")));
+                    break;
 
-                    default:
-                        break;
-                }
-
-                return numbers;
+                default:
+                    break;
             }
+
+            return numbers;
         }
     }
 }
