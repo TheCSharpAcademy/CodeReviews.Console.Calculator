3a89b7b0aa43fa8c50928dd4383ac10555ce1cdf
4ECHatu <49622353+Hatu-ey@users.noreply.github.com>
2025-03-28 11:35:19 +0100

    finished ms project

---
+++ Calculator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+bool endApp = false;
+
+Console.WriteLine("Console calculator");
+Console.WriteLine("------------------------\n");
+
+Calculator calculator = new Calculator();
+
+while (!endApp)
+{
+    string? numInput1 = "";
+    string? numInput2 = "";
+    double result = 0;
+
+    Console.Write("Type a number, and then press Enter: ");
+    numInput1 = Console.ReadLine();
+
+    double cleanNum1 = 0;
+    while (!double.TryParse(numInput1, out cleanNum1))
+    {
+        Console.Write("This is not valid input. Please enter a numeric value: ");
+        numInput1 = Console.ReadLine();
+    }
+
+    Console.Write("Type another number, and then press Enter: ");
+    numInput2 = Console.ReadLine();
+
+    double cleanNum2 = 0;
+    while (!double.TryParse(numInput2, out cleanNum2))
+    {
+        Console.Write("This is not valid input. Please enter a numeric value: ");
+        numInput2 = Console.ReadLine();
+    }
+
+    Console.WriteLine("Choose an operator from the following list:");
+    Console.WriteLine("\ta - Add");
+    Console.WriteLine("\ts - Subtract");
+    Console.WriteLine("\tm - Multiply");
+    Console.WriteLine("\td - Divide");
+    Console.Write("Your option? ");
+
+
+    string? op = Console.ReadLine();
+
+    if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+    {
+        Console.WriteLine("Error: Unrecognized input.");
+    }
+    else
+    {
+        try
+        {
+            result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+            if (double.IsNaN(result))
+            {
+                Console.WriteLine("The Operation will result in Mathematical error\n");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {result:0.##}\n");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error occured!" + e);
+        }
+    }
+
+    Console.WriteLine("------------------------\n");
+
+    // Wait for the user to respond before closing.
+    Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+    if (Console.ReadLine() == "n") endApp = true;
+
+    Console.WriteLine("\n"); // Friendly linespacing.
+
+}
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public Calculator() {
+            StreamWriter logFile = File.CreateText("Calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Starting Calculator log");
+            Trace.WriteLine(String.Format("Started {0}", DateTime.Now.ToString()));
+        }
+        public double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    Trace.WriteLine(String.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    Trace.WriteLine(String.Format("{0} - {1} = {2}", num1, num2, result));
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    Trace.WriteLine(String.Format("{0} * {1} = {2}", num1, num2, result));
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                    }
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
