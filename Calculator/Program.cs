588f95e81f6e3e9f44f936dfe932e3e702fbf791
Diogo Carvalho <dmrccarvalho@gmail.com>
2025-04-03 17:24:52 +0100

    Calculator project finished
    Calculator project finished

---
+++ Calculator/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        Calculator calculator = new Calculator();
+        string? userOption = "";
+
+        while (!endApp)
+        {
+            switch (userOption)
+            {
+                case "cl":
+                    calculator.clearHistory();
+                    System.Console.WriteLine("History cleared.");
+                    break;
+                default:
+                    userCalculation(calculator);
+                    break;
+            }
+
+            Console.WriteLine("------------------------\n");
+
+            // Wait for the user to respond before closing.
+            Console.Write("Press 'n' and Enter to close the app, 'cl' to clear the result history, or press any other key and Enter to continue: ");
+            userOption = Console.ReadLine();
+            if (userOption == "n") endApp = true;
+
+            Console.WriteLine("\n"); // Friendly linespacing.
+        }
+
+        // Add call to close the JSON writer before return
+        calculator.Finish();
+
+        return;
+    }
+
+    static void userCalculation(Calculator calculator)
+    {
+        // Declare variables and set to empty.
+        double result = 0;
+
+        // Ask the user to choose an option.
+        Console.WriteLine("Choose an option from the following list:");
+        Console.WriteLine("\ta - Add");
+        Console.WriteLine("\ts - Subtract");
+        Console.WriteLine("\tm - Multiply");
+        Console.WriteLine("\td - Divide");
+        Console.WriteLine("\tp - Power");
+        Console.WriteLine("\tq - Square Root");
+        Console.WriteLine("\tt - 10x");
+        Console.WriteLine("\ti - Sin");
+        Console.WriteLine("\to - Cos");
+        Console.WriteLine("\tn - Tan");
+        Console.Write("Your option? ");
+
+        string? op = Console.ReadLine();
+
+        // Validate input is not null, and matches and pattern
+        if (op == null || !Regex.IsMatch(op, "[a|s|m|d|p|q|t|i|o|n]"))
+        {
+            Console.WriteLine("Error: Unrecognized input.");
+        }
+        else
+        {
+            // Ask the user to type the first number.
+            double cleanNum1 = retrieveNumberFromUserInput(calculator, true);
+
+            double cleanNum2 = 0;
+            if (Regex.IsMatch(op, "[a|s|m|d|p]"))
+            {
+                // Ask the user to type the second number.
+                cleanNum2 = retrieveNumberFromUserInput(calculator, false);
+            }
+
+            try
+            {
+                if (Regex.IsMatch(op, "[a|s|m|d|p]"))
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                else
+                    result = calculator.DoSingleInputOperation(cleanNum1, op);
+                if (double.IsNaN(result))
+                {
+                    Console.WriteLine("This operation will result in a mathematical error.\n");
+                }
+                else Console.WriteLine("Your result: {0:0.##}\n", result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + ex.Message);
+            }
+        }
+    }
+
+    /** 
+    * Returns true if the function is able to retrieve a value from the history,
+    * otherwise it returns false (e.g. when the history is empty)
+    */
+    private static bool selectResultFromHistory(Calculator calculator, out double previousResult)
+    {
+        List<double> resultHistory = calculator.getResultHistory();
+
+
+        if (resultHistory.Count == 0)
+        {
+            System.Console.WriteLine("The history is empty!");
+
+            previousResult = 0;
+            return false;
+        }
+
+        for (int i = 0; i < resultHistory.Count; i++)
+        {
+            System.Console.WriteLine($"({i + 1}): {resultHistory[i]}");
+        }
+
+        System.Console.WriteLine("Which result to load?");
+        string? userInput = Console.ReadLine();
+        int historySelection;
+        while (!int.TryParse(userInput, out historySelection))
+        {
+            System.Console.WriteLine("This is not a valid input. Please enter a numberic value between 1 and {0}:", resultHistory.Count);
+            userInput = Console.ReadLine();
+        }
+
+        previousResult = resultHistory[historySelection - 1];
+
+        return true;
+    }
+
+    private static double retrieveNumberFromUserInput(Calculator calculator, bool isFirstInput)
+    {
+        // Use Nullable types (with ?) to match type of System.Console.ReadLine
+        string? numInput = "";
+
+        Console.WriteLine("Type {0} number or write 'h' (to see result history), and then press Enter", (isFirstInput ? "a" : "another"));
+        numInput = Console.ReadLine();
+
+        double cleanNum = 0;
+        bool retrievedFromHistory = false;
+        if (numInput == "h")
+        {
+            retrievedFromHistory = selectResultFromHistory(calculator, out cleanNum);
+        }
+
+        while (!retrievedFromHistory && !double.TryParse(numInput, out cleanNum))
+        {
+            System.Console.WriteLine("This is not a valid input. Please enter a numberic value:");
+            numInput = Console.ReadLine();
+        }
+
+        return cleanNum;
+    }
+}
+
+
---
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+
+namespace CalculatorLibrary;
+public class Calculator
+{
+    JsonWriter writer;
+    int numUsages;
+    List<double> resultsHistory;
+
+    public Calculator()
+    {
+        StreamWriter logFile = File.CreateText("calculatorlog.json");
+        logFile.AutoFlush = true;
+        writer = new JsonTextWriter(logFile);
+        writer.Formatting = Formatting.Indented;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operations");
+        writer.WriteStartArray();
+
+        // Initialize variables
+        numUsages = 0;
+        resultsHistory = new List<double>();
+    }
+
+    public double DoOperation(double num1, double num2, string op)
+    {
+        numUsages++;
+
+        double result = Double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operand1");
+        writer.WriteValue(num1);
+        writer.WritePropertyName("Operand2");
+        writer.WriteValue(num2);
+        writer.WritePropertyName("Operation");
+
+        // Use a switch statement to do the math.
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                resultsHistory.Add(result);
+                writer.WriteValue("Add");
+                break;
+            case "s":
+                result = num1 - num2;
+                resultsHistory.Add(result);
+                writer.WriteValue("Subtract");
+                break;
+            case "m":
+                result = num1 * num2;
+                resultsHistory.Add(result);
+                writer.WriteValue("Multiply");
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    resultsHistory.Add(result);
+                }
+                writer.WriteValue("Divide");
+                break;
+            // Return text for an incorrect option entry.
+            case "p":
+                result = Math.Pow(num1, num2);
+                resultsHistory.Add(result);
+                writer.WriteValue("Power");
+                break;
+            default:
+                break;
+        }
+
+        writer.WritePropertyName("Result");
+        writer.WriteValue(result);
+        writer.WriteEndObject();
+
+        return result;
+    }
+
+    public double DoSingleInputOperation(double num, string op)
+    {
+        numUsages++;
+
+        double result = Double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operand");
+        writer.WriteValue(num);
+        writer.WritePropertyName("Operation");
+
+        // Use a switch statement to do the math.
+        switch (op)
+        {
+            case "q":
+                result = Math.Sqrt(num);
+                resultsHistory.Add(result);
+                writer.WriteValue("Square Root");
+                break;
+            case "t":
+                result = num * 10;
+                resultsHistory.Add(result);
+                writer.WriteValue("10x");
+                break;
+            case "i":
+                result = Math.Sin(num);
+                resultsHistory.Add(result);
+                writer.WriteValue("Sin");
+                break;
+            case "o":
+                result = Math.Cos(num);
+                resultsHistory.Add(result);
+                writer.WriteValue("Cos");
+                break;
+            case "n":
+                result = Math.Tan(num);
+                resultsHistory.Add(result);
+                writer.WriteValue("Tan");
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                break;
+        }
+
+        writer.WritePropertyName("Result");
+        writer.WriteValue(result);
+        writer.WriteEndObject();
+
+        return result;
+    }
+
+    public void Finish()
+    {
+        writer.WriteEndArray();
+
+        writer.WritePropertyName("Number of usages");
+        writer.WriteValue(numUsages);
+
+        writer.WritePropertyName("History");
+        writer.WriteStartArray();
+        foreach (double res in resultsHistory)
+        {
+            writer.WriteValue(res);
+        }
+        writer.WriteEndArray();
+
+
+        writer.WriteEndObject();
+        writer.Close();
+    }
+
+    public void clearHistory()
+    {
+        resultsHistory = new List<double>();
+    }
+
+    public List<double> getResultHistory()
+    {
+        return resultsHistory;
+    }
+}
