d9298372b7b254048f5552639141b98ff5274010
4ECHatu <49622353+Hatu-ey@users.noreply.github.com>
2025-04-04 11:11:05 +0200

    Challenge Requirements Finish

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,41 +10,29 @@ Calculator calculator = new Calculator();
 
 while (!endApp)
 {
-    string? numInput1 = "";
-    string? numInput2 = "";
     double result = 0;
 
-    Console.Write("Type a number, and then press Enter: ");
-    numInput1 = Console.ReadLine();
-
-    double cleanNum1 = 0;
-    while (!double.TryParse(numInput1, out cleanNum1))
-    {
-        Console.Write("This is not valid input. Please enter a numeric value: ");
-        numInput1 = Console.ReadLine();
-    }
-
-    Console.Write("Type another number, and then press Enter: ");
-    numInput2 = Console.ReadLine();
-
-    double cleanNum2 = 0;
-    while (!double.TryParse(numInput2, out cleanNum2))
-    {
-        Console.Write("This is not valid input. Please enter a numeric value: ");
-        numInput2 = Console.ReadLine();
-    }
+    double cleanNum1 = HandleUserInput();
+    double cleanNum2 = HandleUserInput();
 
     Console.WriteLine("Choose an operator from the following list:");
     Console.WriteLine("\ta - Add");
     Console.WriteLine("\ts - Subtract");
     Console.WriteLine("\tm - Multiply");
     Console.WriteLine("\td - Divide");
+    Console.WriteLine("\tr - Square Root");
+    Console.WriteLine("\tp - Power");
+    Console.WriteLine("\tt - 10x");
+    Console.WriteLine("\tsin - sin");
+    Console.WriteLine("\tcos - cos");
+    Console.WriteLine("\ttan - tan");
+
     Console.Write("Your option? ");
 
 
     string? op = Console.ReadLine();
 
-    if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+    if (op == null || !Regex.IsMatch(op, "[a|s|m|d|r|p|t|sin|cos|tan]"))
     {
         Console.WriteLine("Error: Unrecognized input.");
     }
@@ -76,4 +64,41 @@ while (!endApp)
 
     Console.WriteLine("\n"); // Friendly linespacing.
 
+}
+
+double HandleUserInput()
+{
+    double cleanNum = double.NaN;
+    string? input;
+
+    if (calculator.HasHistory())
+    {
+        Console.WriteLine("Would you like to use Previous Answer? Enter H  or press any other key and Enter to continue: ");
+        if (Console.ReadLine()?.ToLower().Trim() == "h")
+        {
+            calculator.DisplayHistory();
+            int index;
+            do
+            {
+                Console.WriteLine("Please enter a index within correct range: ");
+                int.TryParse(Console.ReadLine(), out index);
+                cleanNum = calculator.GetResultFromHistory(index);
+            }
+            while (double.IsNaN(cleanNum));
+
+            return cleanNum;
+        }
+
+    }
+
+    Console.Write("Type a number, and then press Enter: ");
+    input = Console.ReadLine();
+
+    while (!double.TryParse(input, out cleanNum))
+    {
+        Console.Write("This is not valid input. Please enter a numeric value: ");
+        input = Console.ReadLine();
+    }
+
+    return cleanNum;
 }
--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,10 +1,12 @@
 ﻿using System.Diagnostics;
-
 namespace CalculatorLibrary
 {
     public class Calculator
     {
-        public Calculator() {
+        private static int CalculatorUsedCount = 0;
+        private static List<(double num1, double num2, string op, double result)> CalculationHistory = new();
+        public Calculator()
+        {
             StreamWriter logFile = File.CreateText("Calculator.log");
             Trace.Listeners.Add(new TextWriterTraceListener(logFile));
             Trace.AutoFlush = true;
@@ -13,8 +15,8 @@ namespace CalculatorLibrary
         }
         public double DoOperation(double num1, double num2, string op)
         {
-            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
 
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
             // Use a switch statement to do the math.
             switch (op)
             {
@@ -32,17 +34,90 @@ namespace CalculatorLibrary
                     break;
                 case "d":
                     // Ask the user to enter a non-zero divisor.
-                    if (num2 != 0)
+                    if (num2 == 0)
                     {
-                        result = num1 / num2;
-                        Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                        throw new DivideByZeroException("You can't divide by zero!");
                     }
+
+                    result = num1 / num2;
+                    Trace.WriteLine(String.Format("{0} / {1} = {2}", num1, num2, result));
+                    break;
+                case "r":
+                    if (num1 < 0)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(num1), num1, "You can't sqrt negative number! (i mean... you can but not with this basic calculator)");
+                    }
+                    num2 = double.NaN;
+                    result = Math.Sqrt(num1);
+                    Trace.WriteLine(String.Format("SQRT{0} = {1}", num1, result));
+                    break;
+                case "p":
+                    result = Math.Pow(num1, num2);
+                    Trace.WriteLine(String.Format("{0} Raised to the power of {1} = {2}", num1, num2, result));
+                    break;
+                case "t":
+                    num2 = double.NaN;
+                    result = num1 * 10;
+                    Trace.WriteLine(String.Format("{0} x10 = {1}", num1, result));
+                    break;
+                case "sin":
+                    num2 = double.NaN;
+                    result = Math.Sin(num1);
+                    Trace.WriteLine(String.Format("{0} Sin = {1}", num1, result));
+                    break;
+                case "cos":
+                    num2 = double.NaN;
+                    result = Math.Cos(num1);
+                    Trace.WriteLine(String.Format("{0} Cos = {1}", num1, result));
+                    break;
+                case "tan":
+                    num2 = double.NaN;
+                    result = Math.Tan(num1);
+                    Trace.WriteLine(String.Format("{0} Tan = {1}", num1, result));
                     break;
                 // Return text for an incorrect option entry.
                 default:
                     break;
             }
+
+            if (!double.IsNaN(result))
+            {
+                CalculationHistory.Add((num1, num2, op, result));
+            }
+            Trace.Write($"Calc used no. {++CalculatorUsedCount}");
             return result;
         }
+
+        public void ClearHistory()
+        {
+            CalculationHistory.Clear();
+        }
+
+        public double GetResultFromHistory(int index)
+        {
+            double result = double.NaN;
+
+            if (index >= 0 && index < CalculationHistory.Count)
+            {
+                result = CalculationHistory[index].result;
+            }
+
+            return result;
+        }
+
+        public bool HasHistory()
+        {
+            return CalculationHistory.Count > 0;
+        }
+
+        public void DisplayHistory()
+        {
+            for (int i = 0; i < CalculationHistory.Count; i++)
+            {
+                (double num1, double num2, string op, double result) calculation = CalculationHistory[i];
+                Console.WriteLine($"index {i}: {calculation.num1} {calculation.op} {(!double.IsNaN(calculation.num2) ? calculation.num2.ToString() : "")} equals {calculation.result}");
+            }
+        }
+
     }
 }
