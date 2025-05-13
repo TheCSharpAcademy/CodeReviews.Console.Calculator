d198c7b7d18d7674c4a652811e55742eedb8d65e
FaniriniainaR <faniriniainarakotomahenina@gmail.com>
2025-05-13 15:48:59 +0300

    Add initial implementation of Calculator and CalculatorLibrary projects

---
+++ Calculator.FaniriniainaR/Calculator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
+
+
+class Program
+{
+    static double GetValidNumber(string prompt)
+    {
+        double result;
+        string input;
+
+        Console.Write(prompt);
+        input = Console.ReadLine();
+
+        while (!double.TryParse(input, out result))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            input = Console.ReadLine();
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        Calculator calculator = new Calculator();
+        while (!endApp)
+        {
+            
+            double result = 0;
+
+            double cleanNum1 = GetValidNumber("Type a number, and then press Enter: ");
+            double cleanNum2 = GetValidNumber("Type another number, and then press Enter: ");
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\t1 - Add");
+            Console.WriteLine("\t2 - Subtract");
+            Console.WriteLine("\t3 - Multiply");
+            Console.WriteLine("\t4 - Divide");
+            Console.Write("Your option? ");
+
+            string? op = Console.ReadLine();
+
+            // Validate input is not null, and matches the pattern
+            if (op == null || !Regex.IsMatch(op, "[1|2|3|4]"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+            }
+            else
+            {
+                try
+                {
+                    result = calculator.Operation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+            Console.WriteLine("------------------------\n");
+
+            
+            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n");
+        }
+        return;
+    }
+}
---
+++ Calculator.FaniriniainaR/CalculatorLibrary/CalculatorLibrary.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Trace.WriteLine("Starting Calculator Log");
+            Trace.WriteLine(string.Format("Started {0}", System.DateTime.Now.ToString()));
+        }
+
+        public double Operation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.  
+
+            // Use a switch statement to do the math.  
+            switch (op)
+            {
+                case "1":
+                    result = num1 + num2;
+                    Trace.WriteLine(string.Format("{0} + {1} = {2}", num1, num2, result));
+                    break;
+                case "2":
+                    result = num1 - num2;
+                    Trace.WriteLine(string.Format("{0} - {1} = {2}", num1, num2, result));
+                    break;
+                case "3":
+                    result = num1 * num2;
+                    Trace.WriteLine(string.Format("{0} * {1} = {2}", num1, num2, result));
+                    break;
+                case "4":
+                    // Ask the user to enter a non-zero divisor.  
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Trace.WriteLine(string.Format("{0} / {1} = {2}", num1, num2, result));
+                    }
+                    break;
+                // Return text for an incorrect option entry.  
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
