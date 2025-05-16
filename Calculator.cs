5c18fade4d1b11a1071727c6f25bcae04e2f69e3
Gerimantas Pukys <g.pukys@gmail.com>
2025-05-16 17:11:11 +0300

    initial microsoft implementation

---
+++ Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpAcademy.Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
