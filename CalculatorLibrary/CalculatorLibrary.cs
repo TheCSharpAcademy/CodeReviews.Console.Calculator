53bacff234cd1101d0f755200693f43d6399cda8
coolorvi <lizavetaaaaaaaaa@yandex.ru>
2025-06-25 21:32:30 +0300

    End fix

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -25,7 +25,7 @@ namespace CalculatorLibrary
         }
 
         public List<ExpressionsHistory> History { get; set; } = new();
-
+        public int CountExpressions { get; set; } = 0;
 
         public double DoOperation(double num1, double num2, string op)
         {
