806d37101cf8d247424820230b91671823451383
Niasua <nsuarez2410@gmail.com>
2025-07-08 21:25:28 -0300

    Allow users to use previous results to perform new calculations

--- Calculator/Menu.cs
+++ Calculator/Menu.cs
@@ -19,7 +19,7 @@ internal class Menu
             Console.WriteLine("2 - View History");
             Console.WriteLine("3 - Clear History");
             Console.WriteLine("4 - Exit");
-            Console.Write("Your choice: ");
+            Console.Write("\nYour choice: ");
             string? choice = Console.ReadLine();
 
             switch (choice)
@@ -30,24 +30,27 @@ internal class Menu
                     // Use Nullable types (with ?) to match type of System.Console.ReadLine
                     string? numInput1 = "";
                     string? numInput2 = "";
+                    double cleanNum1 = 0;
+                    double cleanNum2 = 0;
+
                     double result = 0;
 
-                    // Ask the user to type the first number.
-                    Console.Write("Type a number, and then press Enter: ");
-                    numInput1 = Console.ReadLine();
+                    Console.Write("Do you want to use previous calculations? (y/n): ");
+                    var previousCalc = Console.ReadLine();
 
-                    double cleanNum1 = 0;
-                    while (!double.TryParse(numInput1, out cleanNum1))
+                    if (previousCalc.ToLower() == "y")
                     {
-                        Console.Write("This is not valid input. Please enter a numeric value: ");
-                        numInput1 = Console.ReadLine();
+                        cleanNum1 = SetFirstNumber(calculator, ref numInput1);
+                    }
+                    else
+                    {
+                        AskFirstNumber(out numInput1, out cleanNum1);
                     }
 
                     // Ask the user to type the second number.
                     Console.Write("Type another number, and then press Enter: ");
                     numInput2 = Console.ReadLine();
 
-                    double cleanNum2 = 0;
                     while (!double.TryParse(numInput2, out cleanNum2))
                     {
                         Console.Write("This is not valid input. Please enter a numeric value: ");
@@ -55,17 +58,17 @@ internal class Menu
                     }
 
                     // Ask the user to choose an operator.
-                    Console.WriteLine("Choose an operator from the following list:");
+                    Console.WriteLine("Choose an operator from the following list:\n");
                     Console.WriteLine("\ta - Add");
                     Console.WriteLine("\ts - Subtract");
                     Console.WriteLine("\tm - Multiply");
                     Console.WriteLine("\td - Divide");
-                    Console.Write("Your option? ");
+                    Console.Write("\nYour option? ");
 
                     string? op = Console.ReadLine();
 
                     // Validate input is not null, and matches the pattern
-                    if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+                    if (op == null || !Regex.IsMatch(op, "^[asmd]$"))
                     {
                         Console.WriteLine("Error: Unrecognized input.");
                     }
@@ -78,7 +81,7 @@ internal class Menu
                             {
                                 Console.WriteLine("This operation will result in a mathematical error.\n");
                             }
-                            else Console.WriteLine("Your result: {0:0.##}\n", result);
+                            else Console.WriteLine("\nYour result: {0:0.##}\n", result);
                         }
                         catch (Exception e)
                         {
@@ -88,13 +91,15 @@ internal class Menu
                     Console.WriteLine("------------------------\n");
 
                     // Wait for the user to respond before closing.
-                    Console.Write("Press 'n' and Enter to leave the calculation, or press any other key and Enter to continue: ");
+                    Console.Write("Press any key to continue...");
                     if (Console.ReadLine() == "n") continue;
 
                     Console.WriteLine("\n"); // Friendly linespacing.
                     break;
                 case "2":
                     calculator.ShowHistory();
+                    Console.Write("\nPress any key to return to menu...");
+                    Console.ReadKey();
                     break;
                 case "3":
                     calculator.ClearHistory();
@@ -113,6 +118,56 @@ internal class Menu
         // Add call to close the JSON writer before return
         calculator.Finish();
         return;
+    }
+
+    private static double SetFirstNumber(Calculator calculator, ref string? numInput1)
+    {
+        double cleanNum1;
+        if (!calculator.HasHistory())
+        {
+            Console.WriteLine("There aren't previous calculations");
+            AskFirstNumber(out numInput1, out cleanNum1);
+        }
+        else
+        {
+            calculator.ShowHistory();
+            Console.WriteLine("\nSelect the calculation by its ID: ");
+            var inputId = Console.ReadLine();
+            int id;
+
+            while (!int.TryParse(inputId, out id))
+            {
+                Console.Write("This is not valid input. Please enter a numeric value: ");
+                inputId = Console.ReadLine();
+            }
+
+            var calculation = calculator.GetCalculationById(id);
+            if (calculation == null)
+            {
+                Console.WriteLine("No calculation found with that ID.");
+                AskFirstNumber(out numInput1, out cleanNum1);
+            }
+            else
+            {
+                cleanNum1 = calculation.Result;
+                Console.WriteLine($"\nYou selected: {cleanNum1}");
+            }
+        }
+
+        return cleanNum1;
+    }
+
+    private static void AskFirstNumber(out string? numInput1, out double cleanNum1)
+    {
+        // Ask the user to type the first number.
+        Console.Write("Type a number, and then press Enter: ");
+        numInput1 = Console.ReadLine();
+
+        while (!double.TryParse(numInput1, out cleanNum1))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+            numInput1 = Console.ReadLine();
+        }
     }
 }
 
--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -2,137 +2,146 @@
 using System.Diagnostics;
 using Newtonsoft.Json;
 
-namespace CalculatorLibrary
+namespace CalculatorLibrary;
+public class Calculator
 {
-    public class Calculator
+    JsonWriter writer;
+    private static int calculatorUses;
+    private List<Calculation> calculations = new();
+    private int nextId = 1;
+
+    public Calculator()
     {
-        JsonWriter writer;
-        private static int calculatorUses;
-        private List<Calculation> calculations = new(); 
-
-        public Calculator()
-        {
-            StreamWriter logFile = File.CreateText("calculatorlog.json");
-            logFile.AutoFlush = true;
-            writer = new JsonTextWriter(logFile);
-            writer.Formatting = Formatting.Indented;
-            writer.WriteStartObject();
-            writer.WritePropertyName("Operations");
-            writer.WriteStartArray();
-        }
-
-        public double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-            string operation = "";
-            writer.WriteStartObject();
-            writer.WritePropertyName("Operand1");
-            writer.WriteValue(num1);
-            writer.WritePropertyName("Operand2");
-            writer.WriteValue(num2);
-            writer.WritePropertyName("Operation");
-            // Use a switch statement to do the math.
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    writer.WriteValue("Add");
-                    calculatorUses++;
-                    operation = "+";
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    writer.WriteValue("Subtract");
-                    calculatorUses++;
-                    operation = "-";
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    writer.WriteValue("Multiply");
-                    calculatorUses++;
-                    operation = "*";
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor.
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                        writer.WriteValue("Divide");
-                        calculatorUses++;
-                        operation = "/";
-                    }
-                    else
-                    {
-                        writer.WriteValue("Divide by zero attempt");
-                    }
-                    break;
-                // Return text for an incorrect option entry.
-                default:
-                    break;
-            }
-            writer.WritePropertyName("Result");
-            writer.WriteValue(result);
-            writer.WriteEndObject();
-
-            Calculation calculation = new(num1, num2, operation, result);
-            calculations.Add(calculation);
-            return result;
-        }
-        public void Finish()
-        {
-            writer.WriteEndArray();
-            writer.WritePropertyName("Calculator uses");
-            writer.WriteValue(calculatorUses);
-            writer.WriteEndObject();
-            writer.Close();
-        }
-
-        public void ShowHistory()
-        {
-            Console.Clear();
-            if (calculations.Count == 0)
-            {
-                Console.WriteLine("No calculations yet.");
-            }
-            else
-            {
-                int i = 1;
-                Console.WriteLine("Calculation History:\n");
-                foreach (var calc in calculations)
-                {
-                    Console.WriteLine($"{i}.\t({calc})");
-                    i++;
-                }
-                Console.Write("\nPress any key to go back...");
-                Console.ReadLine();
-            }
-        }
-
-        public void ClearHistory()
-        {
-            calculations.Clear();
-        }
-
+        StreamWriter logFile = File.CreateText("calculatorlog.json");
+        logFile.AutoFlush = true;
+        writer = new JsonTextWriter(logFile);
+        writer.Formatting = Formatting.Indented;
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operations");
+        writer.WriteStartArray();
     }
 
-    public class Calculation
+    public double DoOperation(double num1, double num2, string op)
     {
-        public double Operand1 { get; set; }
-        public double Operand2 { get; set; }
-        public string Operation { get; set; }
-        public double Result { get; set; }
-
-        public Calculation(double operand1, double operand2, string operation, double result)
+        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        string operation = "";
+        writer.WriteStartObject();
+        writer.WritePropertyName("Operand1");
+        writer.WriteValue(num1);
+        writer.WritePropertyName("Operand2");
+        writer.WriteValue(num2);
+        writer.WritePropertyName("Operation");
+        // Use a switch statement to do the math.
+        switch (op)
         {
-            Operand1 = operand1;
-            Operand2 = operand2;
-            Result = result;
-            Operation = operation;
+            case "a":
+                result = num1 + num2;
+                writer.WriteValue("Add");
+                calculatorUses++;
+                operation = "+";
+                break;
+            case "s":
+                result = num1 - num2;
+                writer.WriteValue("Subtract");
+                calculatorUses++;
+                operation = "-";
+                break;
+            case "m":
+                result = num1 * num2;
+                writer.WriteValue("Multiply");
+                calculatorUses++;
+                operation = "*";
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    writer.WriteValue("Divide");
+                    calculatorUses++;
+                    operation = "/";
+                }
+                else
+                {
+                    writer.WriteValue("Divide by zero attempt");
+                }
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                break;
         }
+        writer.WritePropertyName("Result");
+        writer.WriteValue(result);
+        writer.WriteEndObject();
 
-        public override string ToString()
+        Calculation calculation = new(nextId, num1, num2, operation, result);
+        nextId++;
+        calculations.Add(calculation);
+        return result;
+    }
+    public void Finish()
+    {
+        writer.WriteEndArray();
+        writer.WritePropertyName("Calculator uses");
+        writer.WriteValue(calculatorUses);
+        writer.WriteEndObject();
+        writer.Close();
+    }
+
+    public void ShowHistory()
+    {
+        Console.Clear();
+        if (calculations.Count == 0)
         {
-            return $"{Operand1} {Operation} {Operand2} = {Result} ";
+            Console.WriteLine("No calculations yet.");
         }
+        else
+        {
+            Console.WriteLine("Calculation History:\n");
+            foreach (var calc in calculations)
+            {
+                Console.WriteLine($"[{calc.Id}].\t({calc})");
+            }
+        }
+    }
+
+    public Calculation GetCalculationById(int id)
+    {
+        return calculations.FirstOrDefault(c => c.Id == id);
+    }
+
+    public bool HasHistory()
+    {
+        return (calculations.Count > 0); 
+    }
+
+    public void ClearHistory()
+    {
+        calculations.Clear();
+    }
+
+}
+
+public class Calculation
+{
+    public int Id { get; set; }
+    public double Operand1 { get; set; }
+    public double Operand2 { get; set; }
+    public string Operation { get; set; }
+    public double Result { get; set; }
+
+    public Calculation(int id, double operand1, double operand2, string operation, double result)
+    {
+        Id = id;
+        Operand1 = operand1;
+        Operand2 = operand2;
+        Result = result;
+        Operation = operation;
+    }
+
+    public override string ToString()
+    {
+        return $"{Operand1} {Operation} {Operand2} = {Result} ";
     }
 }
+
